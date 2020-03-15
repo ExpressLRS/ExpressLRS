88a9e22df2634ab3c3e62173048d9ddb76accf11
DaBit <dabit@icecoldcomputing.com>
2020-03-15 11:37:31 +0100

    By default all the regulatory domain #defines are commented out. This forces users to select the proper one for their region.

--- src/src/common.h
+++ src/src/common.h
@@ -1,9 +1,11 @@
 #pragma once
 
-// define frequnecy band of operation
+// define exacly one frequency band of operation here
+
 //#define Regulatory_Domain_AU_915
-#define Regulatory_Domain_EU_868
-// #define Regulatory_Domain_AU_433
+//#define Regulatory_Domain_EU_868
+//#define Regulatory_Domain_AU_433
+//#define Regulatory_Domain_EU_433
 
 #include "FHSS.h"
 #include "LoRaRadioLib.h"
