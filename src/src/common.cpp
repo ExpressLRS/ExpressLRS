e3ac54d1705bd072116a808528f147563cc70456
cruwaller <cruwaller@gmail.com>
2020-03-27 08:52:49 +0200

    build: read user specified defines from file
    User can define specific defines in src/user_defines.txt file
    to avoid unnecessary file modifications. This also helps to
    keep specific regulatory domain and UID in place.

--- src/src/common.cpp
+++ src/src/common.cpp
@@ -25,9 +25,13 @@ int8_t ExpressLRS_prevPower = 0;
 connectionState_e connectionState = disconnected;
 connectionState_e connectionStatePrev = disconnected;
 
+#ifndef MY_UID
 //uint8_t UID[6] = {48, 174, 164, 200, 100, 50};
 //uint8_t UID[6] = {180, 230, 45, 152, 126, 65}; //sandro unique ID
 uint8_t UID[6] = {180, 230, 45, 152, 125, 173}; // Wez's unique ID
+#else
+uint8_t UID[6] = {MY_UID};
+#endif
 
 uint8_t CRCCaesarCipher = UID[4];
 uint8_t DeviceAddr = UID[5] & 0b111111; // temporarily based on mac until listen before assigning method merged
