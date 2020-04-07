3ee2922153440a051372b75c51925cb355fa8ff3
wvarty <wezley.varty@gmail.com>
2020-04-07 23:28:01 +1000

    Fix ELRS MSP opcode values

--- src/lib/MSP/msptypes.h
+++ src/lib/MSP/msptypes.h
@@ -3,9 +3,9 @@
 #define MSP_ELRS_FUNC 17784     // ['E','x']
 
 // ELRS specific opcodes
-#define MSP_ELRS_RF_MODE    0x01
-#define MSP_ELRS_TX_PWR     0x02
-#define MSP_ELRS_TLM_RATE   0x03
+#define MSP_ELRS_RF_MODE    0x00
+#define MSP_ELRS_TX_PWR     0x01
+#define MSP_ELRS_TLM_RATE   0x02
 
 #define CHECK_PACKET_PARSING() \
   if (packet.readError) {\
