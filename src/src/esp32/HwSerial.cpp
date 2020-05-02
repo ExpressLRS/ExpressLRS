e952825a4121368f652482c28e9c80b52b1a727e
cruwaller <cruwaller@gmail.com>
2020-05-02 11:19:22 +0300

    esp32: hwserial half duplex cleanup

--- src/src/esp32/HwSerial.cpp
+++ src/src/esp32/HwSerial.cpp
@@ -7,6 +7,17 @@
 #error "Not supported serial!"
 #endif
 
+#if CRSF_SERIAL_NBR == 0
+#define UART_RXD_IDX (U0RXD_IN_IDX)
+#define UART_TXD_IDX (U0TXD_OUT_IDX)
+#elif CRSF_SERIAL_NBR == 1
+#define UART_RXD_IDX (U1RXD_IN_IDX)
+#define UART_TXD_IDX (U1TXD_OUT_IDX)
+#elif CRSF_SERIAL_NBR == 2
+#define UART_RXD_IDX (U2RXD_IN_IDX)
+#define UART_TXD_IDX (U2TXD_OUT_IDX)
+#endif
+
 /********************************************************************************
  *                                   PUBLIC
  ********************************************************************************/
@@ -27,23 +38,22 @@ void HwSerial::enable_receiver(void)
 {
     yield();
     HardwareSerial::flush(); // wait until write ends
-    ESP_ERROR_CHECK(gpio_set_direction((gpio_num_t)GPIO_PIN_RCSIGNAL_RX, GPIO_MODE_INPUT));
-    //ESP_ERROR_CHECK(gpio_set_pull_mode((gpio_num_t)GPIO_PIN_RCSIGNAL_RX, GPIO_FLOATING));
-    //ESP_ERROR_CHECK(gpio_set_pull_mode(port->config.rx, port->config.inverted ? GPIO_PULLDOWN_ONLY : GPIO_PULLUP_ONLY));
-    gpio_matrix_in((gpio_num_t)GPIO_PIN_RCSIGNAL_RX, U1RXD_IN_IDX, true);
+    /* Detach TX pin */
+    gpio_matrix_out((gpio_num_t)-1, UART_TXD_IDX, true, false);
+    /* Attach RX pin */
+    gpio_set_direction((gpio_num_t)GPIO_PIN_RCSIGNAL_RX, GPIO_MODE_INPUT);
+    gpio_matrix_in((gpio_num_t)GPIO_PIN_RCSIGNAL_RX, UART_RXD_IDX, true);
     yield();
 }
 
 void HwSerial::enable_transmitter(void)
 {
     delayMicroseconds(20);
-    gpio_matrix_in((gpio_num_t)-1, U1RXD_IN_IDX, false);
-    ESP_ERROR_CHECK(gpio_set_pull_mode((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, GPIO_FLOATING));
-    ESP_ERROR_CHECK(gpio_set_pull_mode((gpio_num_t)GPIO_PIN_RCSIGNAL_RX, GPIO_FLOATING));
-    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, 0));
-    ESP_ERROR_CHECK(gpio_set_direction((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, GPIO_MODE_OUTPUT));
-    gpio_matrix_out((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, U1TXD_OUT_IDX, true, false);
-
-    //vTaskDelay(1 / portTICK_PERIOD_MS);
+    /* Detach RX pin */
+    gpio_matrix_in((gpio_num_t)-1, UART_RXD_IDX, false);
+    /* Attach TX pin */
+    gpio_set_level((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, 0);
+    gpio_set_direction((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, GPIO_MODE_OUTPUT);
+    gpio_matrix_out((gpio_num_t)GPIO_PIN_RCSIGNAL_TX, UART_TXD_IDX, true, false);
     yield();
 }
