235a0448793770e3df8a8b72b482a69323cea27a
cruwaller <cruwaller@gmail.com>
2020-05-12 12:03:39 +0300

    lua: some changes for NV14

--- src/lua/ExLRS.lua
+++ src/lua/ExLRS.lua
@@ -1,17 +1,13 @@
 --[[
-        ChgId
-
-      DanielGeA
+  Change ExpressLRS parameters
 
   License https://www.gnu.org/licenses/gpl-3.0.en.html
 
-  Ported from erskyTx. Thanks to MikeB
-
   Lua script for radios X7, X9, X-lite and Horus with openTx 2.2 or higher
 
-  Change Frsky sensor Id
-
+  Original author: AlessandroAU.
 ]] --
+
 local version = 'v0.1'
 local gotFirstResp = false
 
@@ -21,31 +17,34 @@ local AirRate = {
     selected = 99,
     list = {'200 Hz', '100 Hz', '50 Hz'},
     values = {0x00, 0x01, 0x02},
-    allowed = 3
+    max_allowed = 3
 }
+
 local TLMinterval = {
     editable = true,
     name = 'TLM Ratio',
     selected = 99,
     list = {'Off', '1:128', '1:64', '1:32', '1:16', '1:8', '1:4', '1:2', 'Default'},
     values = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0xFF},
-    allowed = 9
+    max_allowed = 9
 }
+
 local MaxPower = {
     editable = true,
     name = 'Power',
     selected = 99,
     list = {'10 mW', '25 mW', '50 mW', '100 mW', '250 mW', '500 mW', '1000 mW', '2000 mW'},
     values = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07},
-    allowed = 8
+    max_allowed = 8
 }
+
 local RFfreq = {
     editable = false,
     name = 'RF Freq',
     selected = 99,
     list = {'915 MHz', '868 MHz', '433 MHz'},
     values = {0x00, 0x01, 0x02},
-    allowed = 3
+    max_allowed = 3
 }
 
 local selection = {
@@ -53,7 +52,7 @@ local selection = {
     modify = false,
     -- Note: list indexes must match to param handling in tx_main!
     list = {AirRate, TLMinterval, MaxPower, RFfreq},
-    allowed = 4
+    max_allowed = 4
 }
 
 -- returns flags to pass to lcd.drawText for inverted and flashing text
@@ -72,35 +71,39 @@ local supportedRadios =
 {
     ["128x64"]  =
     {
-        highRes         = false,
+        --highRes         = false,
         textSize        = SMLSIZE,
         xOffset         = 60,
         yOffset         = 10,
-        hasTitle        = true,
+        topOffset       = 1,
+        leftOffset      = 1,
     },
     ["212x64"]  =
     {
-        highRes         = false,
+        --highRes         = false,
         textSize        = SMLSIZE,
         xOffset         = 60,
         yOffset         = 10,
-        hasTitle        = true,
+        topOffset       = 1,
+        leftOffset      = 1,
     },
     ["480x272"] =
     {
-        highRes         = true,
+        --highRes         = true,
         textSize        = 0,
         xOffset         = 100,
         yOffset         = 20,
-        hasTitle        = false,
+        topOffset       = 1,
+        leftOffset      = 1,
     },
     ["320x480"] =
     {
-        highRes         = true,
+        --highRes         = true,
         textSize        = 0,
         xOffset         = 60,
-        yOffset         = 20,
-        hasTitle        = false,
+        yOffset         = 25,
+        topOffset       = 5,
+        leftOffset      = 5,
     },
 }
 
@@ -110,18 +113,19 @@ local radio_data = assert(supportedRadios[radio_resolution], radio_resolution.."
 -- redraw the screen
 local function refreshLCD()
 
-    local yOffset = 1
+    local yOffset = radio_data.topOffset;
+    local lOffset = radio_data.leftOffset;
 
     lcd.clear()
-    lcd.drawText(1, yOffset, 'ExpressLRS CFG ' .. version, INVERS)
-    yOffset = yOffset + radio_data.yOffset + 4
+    lcd.drawText(lOffset, yOffset, 'ExpressLRS CFG ' .. version, INVERS)
+    yOffset = 5 + radio_data.yOffset
 
     for idx,item in pairs(selection.list) do
         local value = '?'
-        if item.selected <= #item.list and item.selected <= item.allowed then
+        if item.selected <= #item.list and item.selected <= item.max_allowed then
             value = item.list[item.selected]
         end
-        lcd.drawText(1, yOffset, item.name, radio_data.textSize)
+        lcd.drawText(lOffset, yOffset, item.name, radio_data.textSize)
         lcd.drawText(radio_data.xOffset, yOffset, value, getFlags(idx) + radio_data.textSize)
         yOffset = yOffset + radio_data.yOffset
     end
@@ -144,7 +148,7 @@ local function increase(_selection)
         item = item.list[item.selected]
     end
 
-    if item.selected < #item.list and item.selected < item.allowed then
+    if item.selected < #item.list and item.selected < item.max_allowed then
         item.selected = item.selected + 1
         --playTone(2000, 50, 0)
     end
@@ -194,7 +198,7 @@ local function processResp()
                     MaxPower.selected = data[5] + 1
                 end
                 if data[6] < #MaxPower.list then
-                    MaxPower.allowed = data[6] + 1 -- limit power list
+                    MaxPower.max_allowed = data[6] + 1 -- limit power list
                 end
                 if data[7] ~= 0xff and data[7] < #RFfreq.list then
                     RFfreq.selected = data[7] + 1
@@ -233,6 +237,9 @@ local function run_func(event)
         crossfireTelemetryPush(0x2D, {0xEE, 0xEA, 0x00, 0x00}) -- ping until we get a resp
     end
 
+    local type = selection.selected
+    local item = selection.list[type]
+
     -- now process key events
     if event == EVT_ROT_LEFT or
        event == EVT_MINUS_BREAK or
@@ -247,8 +254,6 @@ local function run_func(event)
     elseif event == EVT_ENTER_BREAK then
         if selection.modify then
             -- update module when edit ready
-            local type = selection.selected
-            local item = selection.list[type]
             local value = 0
             if item.selected <= #item.values then
                 value = item.values[item.selected]
@@ -257,7 +262,7 @@ local function run_func(event)
             end
             crossfireTelemetryPush(0x2D, {0xEE, 0xEA, type, value})
             selection.modify = false
-        elseif selection.list[selection.selected].editable and item.selected <= #item.values then
+        elseif item.editable and item.selected <= #item.values then
             -- allow modification only if not readonly and values received from module
             selection.modify = true
         end
