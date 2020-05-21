48486aa5262300b5f8632acd6b994f0425384b9b
cruwaller <cruwaller@gmail.com>
2020-05-22 00:03:58 +0300

    Bootloader source codes

---
+++ src/bootloader/src/README.md
@@ -0,0 +1,17 @@
+# Bootloader
+ Supports Express LRS application code update using XMODEM protocol.
+
+ This bootloader contains support for
+
+ - FrSky R9MM / R9 MINI receivers
+ - FrSky R9M transmitter (OLD)
+ - RHF76-052 LoRa module
+
+ Not tested yet:
+
+ - RAK811 LoRa tracker module
+ - RAK4200 LoRa module
+
+ This version forked from nice [STM32-bootloader](https://github.com/ferenc-nemeth/stm32-bootloader) project.
+
+ Big thanks to original author @ferenc-nemeth !
---
+++ src/bootloader/src/Src/uart.h
@@ -0,0 +1,38 @@
+/**
+ * @file    uart.h
+ * @author  Ferenc Nemeth
+ * @date    21 Dec 2018
+ * @brief   This module is a layer between the HAL UART functions and my Xmodem protocol.
+ *
+ *          Copyright (c) 2018 Ferenc Nemeth - https://github.com/ferenc-nemeth
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdint.h>
+
+/* Timeout for HAL. */
+#define UART_TIMEOUT ((uint16_t)1000u)
+
+/* Status report for the functions. */
+typedef enum
+{
+  UART_OK = 0x00u,   /**< The action was successful. */
+  UART_ERROR = 0xFFu /**< Generic error. */
+} uart_status;
+
+uart_status uart_receive(uint8_t *data, uint16_t length);
+uart_status uart_receive_timeout(uint8_t *data, uint16_t length, uint16_t timeout);
+uart_status uart_transmit_str(uint8_t *data);
+uart_status uart_transmit_ch(uint8_t data);
+uart_status uart_transmit_bytes(uint8_t *data, uint32_t len);
+
+void uart_init(void);
+
+#ifdef DEBUG_UART
+void debug_init(void);
+void debug_send(uint8_t data);
+#endif
+
+#endif /* UART_H_ */
