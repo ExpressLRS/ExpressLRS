6838ae654d41be55b46460266ee8cc165c7a98e2
cruwaller <cruwaller@gmail.com>
2020-06-10 01:19:08 +0300

    ESP RX: Wifi manager included to enable AP connection

--- src/src/esp82xx/ESP8266_WebUpdate.cpp
+++ src/src/esp82xx/ESP8266_WebUpdate.cpp
@@ -1,34 +1,80 @@
 #include "debug_elrs.h"
 
+#define USE_WIFI_MANAGER 1
+
+//#define STATIC_IP_AP     "192.168.4.1"
+//#define STATIC_IP_CLIENT "192.168.1.50"
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
 
-#define STASSID "ExpressLRS RX"
-#define STAPSK "expresslrs"
+#if USE_WIFI_MANAGER
+#include <WiFiManager.h>
+#endif /* USE_WIFI_MANAGER */
 
-const char *host = "esp8266-webupdate";
-const char *ssid = STASSID;
-const char *password = STAPSK;
+#define STASSID "ExpressLRS RX AP"
+#define STAPSK "expresslrs"
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
 void BeginWebUpdate(void)
 {
+    const char *host = "elrs_rx";
 
     DEBUG_PRINTLN("Begin Webupdater");
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, password);
 
-    MDNS.begin(host);
+    wifi_station_set_hostname(host);
+
+    IPAddress addr;
+
+#if USE_WIFI_MANAGER
+    WiFiManager wifiManager;
+    //WiFiManagerParameter header("<p>Express LRS ESP82xx RX</p>");
+    //wifiManager.addParameter(&header);
+
+#ifdef STATIC_IP_AP
+    /* Static portal IP (default: 192.168.4.1) */
+    addr.fromString(STATIC_IP_CLIENT);
+    wifiManager.setAPStaticIPConfig(addr,
+                                    IPAddress(192,168,4,1),
+                                    IPAddress(255,255,255,0));
+#endif /* STATIC_IP_AP */
+#ifdef STATIC_IP_CLIENT
+    /* Static client IP */
+    addr.fromString(STATIC_IP_CLIENT);
+    wifiManager.setSTAStaticIPConfig(addr,
+                                     IPAddress(192,168,1,1),
+                                     IPAddress(255,255,255,0));
+#endif /* STATIC_IP_CLIENT */
+
+    wifiManager.setConfigPortalTimeout(60);
+    //wifiManager.autoConnect(STASSID, STAPSK);
+    if (wifiManager.autoConnect(STASSID)) // start unsecure portal AP
+    {
+        addr = WiFi.localIP();
+    }
+    else
+#endif /* USE_WIFI_MANAGER */
+    {
+        // No wifi found, start AP
+        WiFi.mode(WIFI_AP);
+        WiFi.softAP(STASSID, STAPSK);
+        addr = WiFi.softAPIP();
+    }
+
+    if (MDNS.begin(host, addr))
+    {
+        MDNS.addService("http", "tcp", 80);
+        MDNS.update();
+    }
 
     httpUpdater.setup(&httpServer);
     httpServer.begin();
 
-    MDNS.addService("http", "tcp", 80);
     DEBUG_PRINTF("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
 }
 
