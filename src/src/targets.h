54951ff693e8ed416d91a8afb7558ab2593b5c63
juhtipuhti <63055161+juhtipuhti@users.noreply.github.com>
2020-06-15 08:51:12 +0300

    Rak4200 added to targets.h and platformio.ini (#1)
    * Update targets.h
    * platformio env for Rak4200

--- src/src/targets.h
+++ src/src/targets.h
@@ -154,6 +154,44 @@
 #define GPIO_PIN_TX_ENABLE -1
 #endif // TARGET_RHF76_052
 
+#ifdef TARGET_RAK4200
+
+
+#define GPIO_PIN_NSS         PA4
+#define GPIO_PIN_DIO0        PB10
+#define GPIO_PIN_DIO1        PB2
+#define GPIO_PIN_DIO2        PB0  // not used at the moment
+#define GPIO_PIN_DIO3        PB1  // not used at the moment
+#define GPIO_PIN_MOSI        PA7
+#define GPIO_PIN_MISO        PA6
+#define GPIO_PIN_SCK         PA5
+#define GPIO_PIN_RST         PB11
+/* PA1 and PA2 is used to selct HIGH or LOW RF band! */
+/*
+    LOW 0 : HIGH 0 = OFF
+    LOW 1 : HIGH 0 = LOW BAND (433)
+    LOW 0 : HIGH 1 = HIGH BAND (868 / 915)
+*/
+#define GPIO_SELECT_RFIO_HIGH PA2
+#define GPIO_SELECT_RFIO_LOW  PA1
+//#define GPIO_PIN_RX_ENABLE   -1
+//#define GPIO_PIN_TX_ENABLE   -1
+#define BUFFER_OE            -1
+// RAK4200 USART1: TX=PA9, RX=PA10 
+#define GPIO_PIN_RCSIGNAL_RX PA10  // USART1, PIN23
+#define GPIO_PIN_RCSIGNAL_TX PA9  // USART1, PIN22
+#define GPIO_PIN_LED         PA12  // RAK Pin6/UART1_DE
+#define GPIO_PIN_LED_GREEN   -1   //
+#define GPIO_PIN_BUTTON      -1   // Note: pullup!
+// USART2: TX=PA2, RX=PA3 
+//#define GPIO_PIN_DEBUG_RX    PA3 // USART2, PIN??
+//#define GPIO_PIN_DEBUG_TX    PA2 // USART2, PIN??
+
+#define GPIO_PIN_RX_ENABLE -1
+#define GPIO_PIN_TX_ENABLE -1
+#endif // TARGET_RAK4200
+
+
 /******************************************************************************************/
 /*                                        R9 CONFIGS                                      */
 /******************************************************************************************/
