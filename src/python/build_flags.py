ad237646992973dcf88c567066f50dd2d6a7faf0
cruwaller <cruwaller@gmail.com>
2020-06-18 00:17:49 +0300

    Custom PHRASE to be used as a UID

--- src/python/build_flags.py
+++ src/python/build_flags.py
@@ -1,20 +1,26 @@
 Import("env")
 import os
 import fhss_random
+import hashlib
 
 def parse_flags(path):
     build_flags = env['BUILD_FLAGS']
     try:
         with open(path, "r") as _f:
             for line in _f:
-                defines = " ".join(line.split())
-                defines = defines.replace(" = ", "=").split()
-                for define in defines:
-                    define = define.strip()
-                    if define.startswith("-D"):
-                        if "MY_UID" in define and len(define.split(",")) != 6:
-                            raise Exception("UID must be 6 bytes long")
-                        build_flags.append(define)
+                define = line.strip()
+                if define.startswith("-D"):
+                    if "MY_UID" in define and len(define.split(",")) != 6:
+                        raise Exception("UID must be 6 bytes long")
+                    elif "MY_PHRASE" in define:
+                        define = define.split("=")[1]
+                        define = define.replace('"', '').replace("'", "")
+                        key = define.replace("-D", "")
+                        if len(define) < 8:
+                            raise Exception("MY_PHRASE must be at least 8 characters long")
+                        md5 = hashlib.md5(key.encode()).digest()
+                        define = "-DMY_UID=" + ",".join(["0x%02X"%r for r in md5[:6]])
+                    build_flags.append(define)
     except IOError:
         print("File '%s' does not exist" % path)
 
