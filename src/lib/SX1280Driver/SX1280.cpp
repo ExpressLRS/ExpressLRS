a57a3b581989d6d873fa134e90857af127a7445b
AlessandroAU <alessandroaus@gmail.com>
2020-06-22 04:18:41 +1000

    fix ifdef

--- src/lib/SX1280Driver/SX1280.cpp
+++ src/lib/SX1280Driver/SX1280.cpp
@@ -72,16 +72,13 @@ void SX1280Driver::Begin()
     this->SetFrequency(this->currFreq); //Step 3: Set Freq
     this->SetFIFOaddr(0x00, 0x00);      //Step 4: Config FIFO addr
     
-    #ifdef TARGET_TX_EXPRESSLRS_SX1280_V1
-        this->SetOutputPower(13); //13dbm is max power for SX1280
+    #if defined(TARGET_TX_EXPRESSLRS_SX1280_V1) || defined(TARGET_RX_ESP32_SX1280_V1)
+        this->SetOutputPower(13); //13dbm is max power for bare SX1280
     #endif
-    #ifdef TARGET_TX_EXPRESSLRS_E28_V1
+    #ifdef TARGET_TX_ESP32_E28_SX1280_V1
         this->SetOutputPower(0); //0dbm is max power (pre-PA) for E28
     #endif
-    #ifdef TARGET_RX_EXPRESSLRS_SX1280_V1
-        this->SetOutputPower(13);
-    #endif
-
+    
     this->SetDioIrqParams(SX1280_IRQ_RADIO_ALL, SX1280_IRQ_TX_DONE | SX1280_IRQ_RX_DONE, SX1280_IRQ_RADIO_NONE, SX1280_IRQ_RADIO_NONE); // set DIO1 to trigger on TxDone and RxDone, disable other DIOs
 }
 
