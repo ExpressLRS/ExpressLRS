e5d829ae6c6d6de9598eb6e088c3828226f55487
Jye <JyeSmith@users.noreply.github.com>
2020-07-12 20:50:30 +1000

    example

--- src/python/build_flags.py
+++ src/python/build_flags.py
@@ -1,6 +1,8 @@
 Import("env")
 import os
 import hashlib
+import fnmatch
+import time
 
 def parse_flags(path):
     build_flags = env['BUILD_FLAGS']
@@ -19,3 +21,13 @@ def parse_flags(path):
 parse_flags("user_defines.txt")
 
 print("build flags: %s" % env['BUILD_FLAGS'])
+
+if not fnmatch.filter(env['BUILD_FLAGS'], '-DRegulatory_Domain*'):
+    print("\033[47;31m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n")
+    print("\033[47;31m !!! ExpressLRS Warning Below !!! \n")
+    print("\033[47;31m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n")
+    print("\033[47;30m Please uncomment a Regulatory_Domain in user_defines.txt \n")
+    print("\033[47;31m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print("\033[30m")
+    time.sleep(3)
+    raise Exception("!!! ExpressLRS Warning !!!")
