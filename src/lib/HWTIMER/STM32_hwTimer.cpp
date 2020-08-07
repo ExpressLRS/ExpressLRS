9f6356d5fca4172e4296249fab71448b80f1b932
Alessandro <AlessandroAUS@gmail.com>
2020-08-08 01:55:44 +1000

    update to 8.0.0 ststm platform

--- src/lib/HWTIMER/STM32_hwTimer.cpp
+++ src/lib/HWTIMER/STM32_hwTimer.cpp
@@ -74,11 +74,8 @@ void hwTimer::phaseShift(int32_t newPhaseShift)
         hwTimer::PhaseShift = newPhaseShift;
     }
 }
-#ifdef TARGET_R9MX_RX
+
 void hwTimer::callback(void)
-#else
-void hwTimer::callback(HardwareTimer *)
-#endif
 {
     if (hwTimer::TickTock)
     {
