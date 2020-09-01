345fe0158d497a75ff5c0252e4f11e932f69e2ff
AlessandroAU <alessandroaus@gmail.com>
2020-09-02 01:48:21 +1000

    adss 25hz 2.4ghz RFmode option

--- src/src/common.cpp
+++ src/src/common.cpp
@@ -31,13 +31,15 @@ expresslrs_mod_settings_s ExpressLRS_AirRateConfig[RATE_MAX] = {
     //{0, RATE_500HZ, SX1280_LORA_BW_0800, SX1280_LORA_SF5, SX1280_LORA_CR_4_5, 2000, TLM_RATIO_1_128, 2, 12}, // needs more work/debugging and it's not even supported in OTX yet 
     {0, RATE_250HZ, SX1280_LORA_BW_0800, SX1280_LORA_SF6, SX1280_LORA_CR_LI_4_7, 4000, TLM_RATIO_1_64, 2, 14},
     {1, RATE_150HZ, SX1280_LORA_BW_0800, SX1280_LORA_SF7, SX1280_LORA_CR_LI_4_7, 6666, TLM_RATIO_1_32, 2, 12},
-    {2, RATE_50HZ, SX1280_LORA_BW_0800, SX1280_LORA_SF9, SX1280_LORA_CR_4_5, 20000, TLM_RATIO_NO_TLM, 2, 12}};
+    {2, RATE_50HZ, SX1280_LORA_BW_0800, SX1280_LORA_SF9, SX1280_LORA_CR_4_5, 20000, TLM_RATIO_NO_TLM, 2, 12},
+    {3, RATE_25HZ, SX1280_LORA_BW_0800, SX1280_LORA_SF10, SX1280_LORA_CR_4_5, 40000, TLM_RATIO_NO_TLM, 2, 12}};
 
 expresslrs_rf_pref_params_s ExpressLRS_AirRateRFperf[RATE_MAX] = {
     //{0, RATE_500HZ, -112, 4380, 3500, 1000, 2000, 5000}, // ~ 3 sync packets
-    {0, RATE_250HZ, -112, 4380, 3500, 1000, 2000, 5000}, // ~ 3 sync packets
-    {1, RATE_150HZ, -117, 8770, 3500, 4000, 2000, 5000},
-    {2, RATE_50HZ, -120, 17540, 3500, 6000, 2000, 5000}}; // this means always send sync on ch[0] as soon as we can
+    {0, RATE_250HZ, -108, 4380, 3500, 1000, 2000, 5000}, // ~ 3 sync packets
+    {1, RATE_150HZ, -112, 8770, 3500, 4000, 2000, 5000},
+    {2, RATE_50HZ, -117, 17540, 3500, 6000, 2000, 5000}, // this means always send sync on ch[0] as soon as we can
+    {2, RATE_25HZ, -120, 36886, 3500, 12000, 2000, 5000}};
 
 #endif
 
