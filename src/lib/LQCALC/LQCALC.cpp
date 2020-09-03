43aea39dfe27523b1421b1a871b90bb9ca5cb763
AlessandroAU <alessandroaus@gmail.com>
2020-09-03 19:55:25 +1000

    makes linkquality calculation into its own lib, also fixed downlink LQ calc (for TX) (#159)
    * make linkquality calculation into a lib, also fix downlink LQ (for TX)
    
    * remove unused instance pointer

---
+++ src/lib/LQCALC/LQCALC.cpp
@@ -0,0 +1,43 @@
+#include "LQCALC.h"
+
+void ICACHE_RAM_ATTR LQCALC::init()
+{
+    this->init(100);
+}
+
+void ICACHE_RAM_ATTR LQCALC::init(uint8_t depth)
+{
+    this->LQcalcDepth = depth;
+}
+
+void ICACHE_RAM_ATTR LQCALC::inc()
+{
+    this->LQArrayCounter++;
+    this->LQArrayIndex = this->LQArrayCounter % this->LQcalcDepth;
+    this->LQArray[this->LQArrayIndex] = 0;
+}
+
+void ICACHE_RAM_ATTR LQCALC::add()
+{
+    this->LQArray[LQArrayIndex] = 1;
+}
+
+uint8_t ICACHE_RAM_ATTR LQCALC::getLQ()
+{
+    this->LQ = 0;
+
+    for (int i = 0; i < this->LQcalcDepth; i++)
+    {
+        this->LQ += this->LQArray[i];
+    }
+
+    return (this->LQ) * (100 / this->LQcalcDepth);
+}
+
+void ICACHE_RAM_ATTR LQCALC::reset()
+{
+    for (int i = 0; i < this->LQcalcDepth; i++)
+    {
+        this->LQArray[i] = 0;
+    }
+}
