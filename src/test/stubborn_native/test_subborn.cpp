9efc007eb9450a69c13fd022a27ce9bdd11c88e5
Johannes Kasberger <schugabe@gmx.at>
2020-10-27 21:21:05 +0100

    refactor tests for stubborn link

--- src/test/stubborn_native/test_subborn.cpp
+++ src/test/stubborn_native/test_subborn.cpp
@@ -1,145 +1,169 @@
 #include <cstdint>
-#include <stubborn_link.h>
+#include <stubborn_sender.h>
+#include <stubborn_receiver.h>
 #include <unity.h>
 #include <iostream>
 #include <bitset>
 
-StubbornLink link;
+StubbornSender sender;
+StubbornReceiver receiver;
 
 void test_stubborn_link_sends_data(void)
 {
     uint8_t batterySequence[] = {0xEC,10, 0x08,0,0,0,0,0,0,0,0,109};
-    link.ResetState();
-    link.SetDataToTransmit(sizeof(batterySequence), batterySequence);
+    sender.ResetState();
+    sender.SetDataToTransmit(sizeof(batterySequence), batterySequence, 1);
     uint8_t *data;
     uint8_t maxLength;
     uint8_t packageIndex;
+    bool confirmValue = true;
 
     for(int i = 0; i < sizeof(batterySequence); i++)
     {
-        link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+        sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
         TEST_ASSERT_EQUAL(1, maxLength);
         TEST_ASSERT_EQUAL(i + 1, packageIndex);
         TEST_ASSERT_EQUAL(batterySequence[i], *data);
-        link.ConfirmCurrentPayload();
+        sender.ConfirmCurrentPayload(confirmValue);
+        confirmValue = !confirmValue;
     }
-    link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+
+    sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
     TEST_ASSERT_EQUAL(0, packageIndex);
-    TEST_ASSERT_EQUAL(true, link.IsActive());
-    link.ConfirmCurrentPayload();
-    TEST_ASSERT_EQUAL(false, link.IsActive());
+    TEST_ASSERT_EQUAL(true, sender.IsActive());
+    sender.ConfirmCurrentPayload(confirmValue);
+    TEST_ASSERT_EQUAL(false, sender.IsActive());
 }
 
 void test_stubborn_link_sends_data_even_bytes_per_call(void)
 {
     uint8_t batterySequence[] = {0xEC,10, 0x08,0,0,0,0,0,0,0,0,109};
-    link.ResetState();
-    link.SetBytesPerCall(2);
-    link.SetDataToTransmit(sizeof(batterySequence), batterySequence);
+    sender.ResetState();
+    sender.SetDataToTransmit(sizeof(batterySequence), batterySequence, 2);
     uint8_t *data;
     uint8_t maxLength;
     uint8_t packageIndex;
+    bool confirmValue = true;
 
     for(int i = 0; i < sizeof(batterySequence) / 2; i++)
     {
-        link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+        sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
         TEST_ASSERT_EQUAL(2, maxLength);
         TEST_ASSERT_EQUAL(batterySequence[i*2], data[0]);
         TEST_ASSERT_EQUAL(batterySequence[i*2+1], data[1]);
-        link.ConfirmCurrentPayload();
+        sender.ConfirmCurrentPayload(confirmValue);
+        confirmValue = !confirmValue;
     }
-    link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+    sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
     TEST_ASSERT_EQUAL(0, maxLength);
 }
 
 void test_stubborn_link_sends_data_odd_bytes_per_call(void)
 {
     uint8_t batterySequence[] = {0xEC,11, 0x08,0,0,0,0,0,0,0,109};
-    link.ResetState();
-    link.SetBytesPerCall(3);
-    link.SetDataToTransmit(sizeof(batterySequence), batterySequence);
+    sender.ResetState();
+    sender.SetDataToTransmit(sizeof(batterySequence), batterySequence, 3);
     uint8_t *data;
     uint8_t maxLength;
     uint8_t packageIndex;
+    bool confirmValue = true;
 
     for(int i = 0; i < sizeof(batterySequence) / 3; i++)
     {
-        link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+        sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
         TEST_ASSERT_EQUAL(3, maxLength);
         TEST_ASSERT_EQUAL(batterySequence[i*3], data[0]);
         TEST_ASSERT_EQUAL(batterySequence[i*3+1], data[1]);
         TEST_ASSERT_EQUAL(batterySequence[i*3+2], data[2]);
-        link.ConfirmCurrentPayload();
+        sender.ConfirmCurrentPayload(confirmValue);
+        confirmValue = !confirmValue;
     }
-    link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+    sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
     TEST_ASSERT_EQUAL(2, maxLength);
     TEST_ASSERT_EQUAL(batterySequence[9], data[0]);
     TEST_ASSERT_EQUAL(batterySequence[10], data[1]);
 
-    link.ConfirmCurrentPayload();
-    link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+    sender.ConfirmCurrentPayload(confirmValue);
+    sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
     TEST_ASSERT_EQUAL(0, maxLength);
 }
 
 void test_stubborn_link_sends_data_larger_frame_size(void)
 {
     uint8_t batterySequence[] = {0xEC,10, 0x08,0,0,0,0,0,0,0,0,109};
-    link.ResetState();
-    link.SetBytesPerCall(13);
-    link.SetDataToTransmit(sizeof(batterySequence), batterySequence);
+    sender.ResetState();
+    sender.SetDataToTransmit(sizeof(batterySequence), batterySequence, 13);
     uint8_t *data;
     uint8_t maxLength;
     uint8_t packageIndex;
+    bool confirmValue = true;
 
-    link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+    sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
     TEST_ASSERT_EQUAL(12, maxLength);
     for(int i = 0; i < sizeof(batterySequence); i++)
     {
         TEST_ASSERT_EQUAL(batterySequence[i], data[i]);
     }
 
-    link.ConfirmCurrentPayload();
-    link.GetCurrentPayload(&packageIndex, &maxLength, &data);
+    sender.ConfirmCurrentPayload(confirmValue);
+    confirmValue = !confirmValue;
+    sender.GetCurrentPayload(&packageIndex, &maxLength, &data);
     TEST_ASSERT_EQUAL(0, maxLength);
 }
 
 void test_stubborn_link_receives_data(void)
 {
-    uint8_t batterySequence[] = {0xEC,10, 0x08,0,0,0,0,0,0,0,0,109};
+    volatile uint8_t batterySequence[] = {0xEC,10, 0x08,0,0,0,0,0,0,0,0,109};
     uint8_t buffer[100];
-    link.ResetState();
-    link.SetDataToReceive(sizeof(buffer), buffer);
+    receiver.ResetState();
+    receiver.SetDataToReceive(sizeof(buffer), buffer, 1);
 
     for(int i = 0; i < sizeof(batterySequence); i++)
     {
-        bool result = link.ReceiveData(i+1, batterySequence[i]);
+        bool result = receiver.ReceiveData(i+1, batterySequence + i);
         TEST_ASSERT_EQUAL(true, result);
     }
-    TEST_ASSERT_EQUAL(true, link.ReceiveData(0, 0));
-    TEST_ASSERT_EQUAL(sizeof(batterySequence), link.GetReceivedData());
+    TEST_ASSERT_EQUAL(true, receiver.ReceiveData(0, 0));
 
     for(int i = 0; i < sizeof(batterySequence); i++)
     {
         TEST_ASSERT_EQUAL(batterySequence[i], buffer[i]);
     }
 
-    link.Unlock();
-    TEST_ASSERT_EQUAL(0, link.GetReceivedData());
+    receiver.Unlock();
+}
+
+void test_stubborn_link_receives_data_with_multiple_bytes(void)
+{
+    volatile uint8_t batterySequence[] = {0xEC,10, 0x08,0,0,0,0,0,0,0,0,109,0,0,0};
+    uint8_t buffer[100];
+    receiver.ResetState();
+    receiver.SetDataToReceive(sizeof(buffer), buffer, 5);
+
+    for(int i = 0; i < sizeof(batterySequence) / 3; i++)
+    {
+        bool result = receiver.ReceiveData(i+1, batterySequence + (i * 5));
+        TEST_ASSERT_EQUAL(true, result);
+    }
+    TEST_ASSERT_EQUAL(true, receiver.ReceiveData(0, 0));
+
+    for(int i = 0; i < sizeof(batterySequence); i++)
+    {
+        TEST_ASSERT_EQUAL(batterySequence[i], buffer[i]);
+    }
+
+    receiver.Unlock();
 }
 
 int main(int argc, char **argv)
 {
-    uint8_t TelemetryStatus = 1;
-    uint8_t currentSwitches = 1;
-
-    uint8_t result = (TelemetryStatus << 7) + ((currentSwitches & 0b11) << 5);
-    std::cout << "result = " << std::bitset<8>(result)  << std::endl;
     UNITY_BEGIN();
     RUN_TEST(test_stubborn_link_sends_data);
     RUN_TEST(test_stubborn_link_sends_data_even_bytes_per_call);
     RUN_TEST(test_stubborn_link_sends_data_odd_bytes_per_call);
     RUN_TEST(test_stubborn_link_sends_data_larger_frame_size);
     RUN_TEST(test_stubborn_link_receives_data);
+    RUN_TEST(test_stubborn_link_receives_data_with_multiple_bytes);
     UNITY_END();
 
     return 0;
