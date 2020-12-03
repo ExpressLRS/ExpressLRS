a71b84c69cb8809ee4b32646c489ae5c59acf822
Logan Rawlins <maveric@gotskill.net>
2020-12-03 12:17:21 -0700

    fixed seriallist nonetype and added sleep for older bootloader

--- src/python/serials_find.py
+++ src/python/serials_find.py
@@ -52,7 +52,7 @@ def serial_ports():
             if "permission denied" in str(error).lower():
                 raise Exception("You don't have persmission to use serial port!")
             pass
-    result.reverse()
+        result.reverse()
     return result
 
 def get_serial_port(debug=True):
