001571b8e31ff5322eabf0cb06ae670d4dade73f
Cru Waller <24904289+cruwaller@users.noreply.github.com>
2021-01-04 02:40:28 +0200

    ESP Backpack logger improvements (#252)
    * ESPbackpack: STK500 support added
    
    * Logger bin renamed to avoid mistakes
    
    * Sync wait timeout fix
    
    * Logger: support for STM32 flash offset + STK500 fixes
    
    * WIFI upload to allow user specific upload_port
    
    * ESPbackpack enhancements
    
    * STK500 sync fail info improved
    * STM32 filename check before flashing
    
    * ESPbackpack to check firmware filenames
    
    Firmware filenames are checked and user noted if filenames
    does not match to expected.

--- src/python/upload_via_esp8266_backpack.py
+++ src/python/upload_via_esp8266_backpack.py
@@ -1,19 +1,30 @@
-import subprocess
+import subprocess, os
+import opentx
 
 def on_upload(source, target, env):
+    # Make sure the firmware.elrs is generated
+    opentx.gen_elrs(source, target, env)
     firmware_path = str(source[0])
+    bin_path = os.path.dirname(firmware_path)
+    elrs_bin_target = os.path.join(bin_path, 'firmware.elrs')
+
     cmd = ["curl", "-v", "--max-time", "60",
            "--retry", "2", "--retry-delay", "1",
-           "-F", "data=@%s" % (firmware_path,)]
-    try:
-        print(" ** UPLOADING TO: http://elrs_tx/upload")
-        subprocess.check_call(cmd + ["http://elrs_tx/upload"])
-        return
-    except subprocess.CalledProcessError:
-        print("FAILED!")
+           "-F", "data=@%s" % (elrs_bin_target,)]
 
-    print(" ** UPLOADING TO: http://elrs_tx.local/upload")
-    try:
-        subprocess.check_call(cmd + ["http://elrs_tx.local/upload"])
-    except subprocess.CalledProcessError:
-        raise Exception("WiFI upload FAILED!")
+    upload_addr = ['elrs_tx', 'elrs_tx.local']
+
+    upload_port = env.get('UPLOAD_PORT', None)
+    if upload_port is not None:
+        upload_addr = [upload_port]
+
+    for addr in upload_addr:
+        addr = "http://%s/upload" % addr
+        print(" ** UPLOADING TO: %s" % addr)
+        try:
+            subprocess.check_call(cmd + [addr])
+            return
+        except subprocess.CalledProcessError:
+            print("FAILED!")
+
+    raise Exception("WiFI upload FAILED!")
