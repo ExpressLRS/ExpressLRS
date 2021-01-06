afc7009e8500649359e7b174fee75e69bc6d6e14
Jonathan <greenbigfrog@gmail.com>
2021-01-06 16:29:23 +0100

    Fix bf passthrough flashing for DIY RX (#275)
    * fix typo
    
    * fix esptool part of BF_Passthrough flashing for rx900
    
    * fix bf init passthrough
    
    * Make use of env to set baud rate
    
    * fix upload_cmd for 2400 RX

--- src/python/build_env_setup.py
+++ src/python/build_env_setup.py
@@ -9,7 +9,7 @@ stm = env.get('PIOPLATFORM', '') in ['ststm32']
 # don't overwrite if custom command defined
 if stm and "$UPLOADER $UPLOADERFLAGS" in env.get('UPLOADCMD', '$UPLOADER $UPLOADERFLAGS'):
     target_name = env['PIOENV'].upper()
-    print("STM ENv: '%s'" % target_name)
+    print("STM ENV: '%s'" % target_name)
     if "_R9M_" in target_name:
         env.AddPostAction("buildprog", [opentx.gen_elrs])
         if "WIFI" in target_name:
