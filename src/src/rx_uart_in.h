9481a853d4428d131d731a0cfae44ce12ccd0d31
Alessandro <AlessandroAUS@gmail.com>
2021-01-09 16:48:38 +1000

    update

--- src/src/rx_uart_in.h
+++ src/src/rx_uart_in.h
@@ -1,7 +1,20 @@
 #include "CRSF.h"
 #include "targets.h"
 
+//#define DEBUG_RX_UART_IN
+
+#if defined(TARGET_RX_GHOST_ATTO_V1) /* !TARGET_RX_GHOST_ATTO_V1 */
+    #define CRSF_RX_SERIAL CrsfRxSerial
+    HardwareSerial CrsfRxSerial(USART1, HALF_DUPLEX_ENABLED);
+#elif defined(TARGET_R9SLIMPLUS_RX) /* !TARGET_R9SLIMPLUS_RX */
+    #define CRSF_RX_SERIAL CrsfRxSerial
+    HardwareSerial CrsfRxSerial(USART3);
+#else
+    #define CRSF_RX_SERIAL Serial
+#endif
+
 extern CRSF crsf;
+extern GENERIC_CRC8 crsf_crc;
 
 uint8_t UARTinPacketPtr;
 uint8_t UARTinPacketLen;
@@ -16,7 +29,9 @@ void RX_UARTinProcessPacket()
     if (UARTinBuffer[2] == CRSF_FRAMETYPE_COMMAND)
     {
 #ifdef PLATFORM_STM32
+        #ifdef DEBUG_RX_UART_IN
         Serial.println("Got CMD Packet");
+        #endif
         if (UARTinBuffer[3] == 0x62 && UARTinBuffer[4] == 0x6c)
         {
             delay(100);
@@ -27,22 +42,26 @@ void RX_UARTinProcessPacket()
 #endif
     }
 
-    if (UARTinBuffer[2] == CRSF_FRAMETYPE_BATTERY_SENSOR)
+    switch (UARTinBuffer[2])
     {
+    case CRSF_FRAMETYPE_BATTERY_SENSOR:
         crsf.TLMbattSensor.voltage = (UARTinBuffer[3] << 8) + UARTinBuffer[4];
         crsf.TLMbattSensor.current = (UARTinBuffer[5] << 8) + UARTinBuffer[6];
         crsf.TLMbattSensor.capacity = (UARTinBuffer[7] << 16) + (UARTinBuffer[8] << 8) + UARTinBuffer[9];
         crsf.TLMbattSensor.remaining = UARTinBuffer[9];
-    }
-
-    if (UARTinBuffer[2] == CRSF_FRAMETYPE_GPS)
-    {
+        break;
+        
+    case CRSF_FRAMETYPE_GPS:
         crsf.TLMGPSsensor.latitude = (UARTinBuffer[3] << 24) + (UARTinBuffer[4] << 16) + (UARTinBuffer[5] << 8) + (UARTinBuffer[6] << 0);
         crsf.TLMGPSsensor.longitude = (UARTinBuffer[7] << 24) + (UARTinBuffer[8] << 16) + (UARTinBuffer[9] << 8) + (UARTinBuffer[10] << 0);
         crsf.TLMGPSsensor.speed = (UARTinBuffer[11] << 8) + (UARTinBuffer[12] << 0);
         crsf.TLMGPSsensor.headng = (UARTinBuffer[13] << 8) + (UARTinBuffer[14] << 0);
         crsf.TLMGPSsensor.alt = (UARTinBuffer[15] << 8) + (UARTinBuffer[16] << 0);
         crsf.TLMGPSsensor.sats = (UARTinBuffer[17]);
+        break;
+
+    default:
+        break;
     }
 }
 
@@ -96,7 +115,7 @@ void RX_UARTinHandle()
 
             if (UARTinPacketPtr == UARTinPacketLen + 2) // plus 2 because the packlen is referenced from the start of the 'type' flag, IE there are an extra 2 bytes.
             {
-                char CalculatedCRC = CalcCRC((uint8_t *)UARTinBuffer + 2, UARTinPacketPtr - 3);
+                uint8_t CalculatedCRC = crsf_crc.calc((uint8_t *)UARTinBuffer + 2, UARTinPacketPtr - 3);
 
                 if (CalculatedCRC == inChar)
                 {
@@ -111,7 +130,9 @@ void RX_UARTinHandle()
                     UARTinPacketPtr = 0;
                     UARTinPacketLen = 0;
                     UARTframeActive = false;
+                    #ifdef DEBUG_RX_UART_IN
                     Serial.println("UART in CRC failure");
+                    #endif
                     while (Serial.available())
                     {
                         Serial.read(); // empty the read buffer
