f41bb0e0f46e8ffb9a06ab40c35aed10a37b580a
Cru Waller <24904289+cruwaller@users.noreply.github.com>
2021-02-10 09:15:15 +0200

    Feature STM32 simulated EEPROM support (#343)
    * generic: .vscode cache dir removed from repo
    
    * feature: STM32 simulated EEPROM

--- src/lib/EEPROM/elrs_eeprom.h
+++ src/lib/EEPROM/elrs_eeprom.h
@@ -1,13 +1,6 @@
 #pragma once
 
-#include <Arduino.h>
-#include "../../src/targets.h"
-#include <Wire.h>
-#ifdef PLATFORM_STM32
-    #include <extEEPROM.h>
-#else
-    #include <EEPROM.h>
-#endif
+#include <stdint.h>
 
 #define RESERVED_EEPROM_SIZE 1024
 
@@ -15,23 +8,23 @@ class ELRS_EEPROM
 {
 public:
     void Begin();
-    uint8_t ReadByte(const unsigned long address);
-    void WriteByte(const unsigned long address, const uint8_t value);
+    uint8_t ReadByte(const uint32_t address);
+    void WriteByte(const uint32_t address, const uint8_t value);
     void Commit();
-    
+
     // The extEEPROM lib that we use for STM doesn't have the get and put templates
     // These templates need to be reimplemented here
-    template <typename T> void Get(int addr, T &value)
+    template <typename T> void Get(uint32_t addr, T &value)
     {
-        byte* p = (byte*)(void*)&value;
-        byte  i = sizeof(value);
+        uint8_t* p = (uint8_t*)(void*)&value;
+        uint8_t  i = sizeof(value);
         while(i--)  *p++ = ReadByte(addr++);
     };
 
-    template <typename T> const void Put(int addr, const T &value)
+    template <typename T> const void Put(uint32_t addr, const T &value)
     {
-        const byte* p = (const byte*)(const void*)&value;
-        byte        i = sizeof(value);
+        const uint8_t* p = (const uint8_t*)(const void*)&value;
+        uint8_t        i = sizeof(value);
         while(i--)  WriteByte(addr++, *p++);
     };
 };
