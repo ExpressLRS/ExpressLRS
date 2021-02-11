31d712c8d94e8c01272945628734e56c5d595793
Jahnkeanater <jahnkeanater@aol.com>
2021-02-10 19:21:10 -0600

    Update tx_main.cpp
    Moved R9M Lite Pro initialization to POWERMGNT.cpp

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -541,13 +541,6 @@ void setup()
     pinMode(GPIO_PIN_LED_GREEN, OUTPUT);
     pinMode(GPIO_PIN_LED_RED, OUTPUT);
     digitalWrite(GPIO_PIN_LED_GREEN, HIGH);
-  
-#if defined(TARGET_R9M_LITE_PRO_TX)
-//#define HAL_DAC_MODULE_ENABLED //this may need to be enabled in the Arduino stm32h7xx_hal_conf.h file
-pinMode(GPIO_PIN_RFamp_APC1, OUTPUT); //I'm sure this does not belong here
-pinMode(GPIO_PIN_RFamp_APC2, OUTPUT); //I'm sure this does not belong here
-analogWriteResolution(12); //I'm sure this does not belong here
-#endif
 
 #ifdef USE_ESP8266_BACKPACK
     HardwareSerial(USART1);
