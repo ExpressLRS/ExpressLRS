1a88ab81036e92f2d78827e5bd8562a84767bb72
Brandon Geraci <brandon.geraci@gmail.com>
2021-02-13 18:15:03 -0600

    Revert "Revert "Feature STM32 simulated EEPROM support (#343)""
    This reverts commit bd78235d2fba6d20d65371965bd38fcbb770a439.

--- src/lib/EEPROM/elrs_eeprom.cpp
+++ src/lib/EEPROM/elrs_eeprom.cpp
@@ -1,32 +1,43 @@
 #include "elrs_eeprom.h"
+#include "../../src/targets.h"
 #include <Arduino.h>
 
-#if defined(TARGET_R9M_TX) || defined(TARGET_R9M_LITE_TX) || defined(TARGET_R9M_LITE_PRO_TX)
-    extEEPROM EEPROM(kbits_2, 1, 1, 0x51);
-#elif defined(TARGET_R9M_RX)
-    extEEPROM EEPROM(kbits_2, 1, 1, 0x50);
-#elif defined(PLATFORM_ESP32) || defined(PLATFORM_ESP8266)
+#if defined(PLATFORM_STM32) && TARGET_USE_EEPROM
+    #if !defined(TARGET_EEPROM_ADDR)
+        #define TARGET_EEPROM_ADDR 0x51
+        #warning "!! Using default EEPROM address (0x51) !!"
+    #endif
+
+    #include <Wire.h>
+    #include <extEEPROM.h>
+    extEEPROM EEPROM(kbits_2, 1, 1, TARGET_EEPROM_ADDR);
 #else
-#define NO_EEPROM 1
+    #include <EEPROM.h>
 #endif
 
 void
 ELRS_EEPROM::Begin()
 {
-#if !NO_EEPROM
-#ifdef PLATFORM_STM32
-    Wire.setSDA(GPIO_PIN_SDA); // set is needed or it wont work :/
-    Wire.setSCL(GPIO_PIN_SCL);
-    Wire.begin();
-    EEPROM.begin();
-#else
+#if defined(PLATFORM_STM32)
+    #if TARGET_USE_EEPROM && \
+            defined(GPIO_PIN_SDA) && (GPIO_PIN_SDA != UNDEF_PIN) && \
+            defined(GPIO_PIN_SCL) && (GPIO_PIN_SCL != UNDEF_PIN)
+        /* Initialize I2C */
+        Wire.setSDA(GPIO_PIN_SDA);
+        Wire.setSCL(GPIO_PIN_SCL);
+        Wire.begin();
+        /* Initialize EEPROM */
+        EEPROM.begin(extEEPROM::twiClock100kHz, &Wire);
+    #else // !TARGET_USE_EEPROM
+        EEPROM.begin();
+    #endif // TARGET_USE_EEPROM
+#else /* !PLATFORM_STM32 */
     EEPROM.begin(RESERVED_EEPROM_SIZE);
-#endif
-#endif /* NO_EEPROM */
+#endif /* PLATFORM_STM32 */
 }
 
 uint8_t
-ELRS_EEPROM::ReadByte(const unsigned long address)
+ELRS_EEPROM::ReadByte(const uint32_t address)
 {
     if (address >= RESERVED_EEPROM_SIZE)
     {
@@ -34,15 +45,11 @@ ELRS_EEPROM::ReadByte(const unsigned long address)
         Serial.println("ERROR! EEPROM address is out of bounds");
         return 0;
     }
-#if !NO_EEPROM
     return EEPROM.read(address);
-#else /* NO_EEPROM */
-    return 0;
-#endif /* NO_EEPROM */
 }
 
 void
-ELRS_EEPROM::WriteByte(const unsigned long address, const uint8_t value)
+ELRS_EEPROM::WriteByte(const uint32_t address, const uint8_t value)
 {
     if (address >= RESERVED_EEPROM_SIZE)
     {
@@ -50,9 +57,7 @@ ELRS_EEPROM::WriteByte(const unsigned long address, const uint8_t value)
         Serial.println("ERROR! EEPROM address is out of bounds");
         return;
     }
-#if !NO_EEPROM
     EEPROM.write(address, value);
-#endif /* NO_EEPROM */
 }
 
 void
