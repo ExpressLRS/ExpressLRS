b2e0175bc8b86842b243439568efae2fc07416d2
SpencerGraffunder <spencergraffunder@cedarville.edu>
2021-03-30 04:54:51 -0500

    6km readme (#412)
    * Added support for mac os (Darwin) (#365)
    
    * Removed os checks in runpython.py and added addition print statment to stlink.py
    
    * Fix print statement in stlink.py
    
    * Updating print statement in bootloader stlink.py
    
    * Updating stlink to check for dawin (Mac OS)
    
    * add 6km 500Hz record to readme
    
    * change my name
    
    Co-authored-by: Brandon Geraci <brandon.geraci@gmail.com>
    Co-authored-by: Spencer Graffunder <asymmetriccube@gmail.com>

--- src/bootloader/src/python/stlink.py
+++ src/bootloader/src/python/stlink.py
@@ -44,11 +44,18 @@ def get_commands(env, firmware):
         if bootloader is not None:
             BL_CMD = [TOOL, "write", bootloader, hex(flash_start)]
         APP_CMD = [TOOL, "--reset", "write", firmware, hex(app_start)]
+    elif "darwin" in platform_name:
+        TOOL = os.path.join(
+            env_dir,
+            "tool-stm32duino", "stlink", "st-flash")
+        if bootloader is not None:
+            BL_CMD = [TOOL, "write", bootloader, hex(flash_start)]
+        APP_CMD = [TOOL, "--reset", "write", firmware, hex(app_start)]
     elif "os x" in platform_name:
         print("OS X not supported at the moment\n")
         raise OSError
     else:
-        print("Unknown operating system...\n")
+        print("Operating system: "+ platform_name +  " is not supported.\n")
         raise OSError
 
     return " ".join(BL_CMD), " ".join(APP_CMD)
