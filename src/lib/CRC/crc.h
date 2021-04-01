eceb2b0858f1ca138786b9596ca0d3bb1b835fbc
Jye <14170229+JyeSmith@users.noreply.github.com>
2021-04-01 16:24:46 +1000

    adds GENERIC_CRC13 and parity lib (#418)
    * adds GENERIC_CRC13 and parity lib

--- src/lib/CRC/crc.h
+++ src/lib/CRC/crc.h
@@ -15,3 +15,18 @@ public:
     uint8_t calc(uint8_t *data, uint8_t len);
     uint8_t calc(volatile uint8_t *data, uint8_t len);
 };
+
+class GENERIC_CRC13
+{
+private:
+    uint16_t crc13tab[crclen];
+    uint16_t crcpoly;
+
+public:
+    GENERIC_CRC13(uint16_t poly);
+    uint16_t calc(uint8_t *data, uint8_t len, uint16_t crc);
+    uint16_t calc(volatile uint8_t *data, uint8_t len, uint16_t crc);
+};
+
+uint8_t getParity(uint8_t *data, uint8_t len);
+uint8_t getParity(volatile uint8_t *data, uint8_t len);
