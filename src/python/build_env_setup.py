2cca18ef548584f8967f5d071bbae25d23e84157
Cru Waller <24904289+cruwaller@users.noreply.github.com>
2021-04-03 19:06:45 +0300

    Feature: enable ESP82xx WIFI upload (#422)

--- src/python/build_env_setup.py
+++ src/python/build_env_setup.py
@@ -3,20 +3,25 @@ import stlink
 import UARTupload
 import opentx
 import upload_via_esp8266_backpack
+import esp_compress
 
-stm = env.get('PIOPLATFORM', '') in ['ststm32']
+platform = env.get('PIOPLATFORM', '')
+stm = platform in ['ststm32']
+
+target_name = env['PIOENV'].upper()
+print("PLATFORM : '%s'" % platform)
+print("BUILD ENV: '%s'" % target_name)
 
 # don't overwrite if custom command defined
 if stm and "$UPLOADER $UPLOADERFLAGS" in env.get('UPLOADCMD', '$UPLOADER $UPLOADERFLAGS'):
-    target_name = env['PIOENV'].upper()
-    print("BUILD ENV: '%s'" % target_name)
     if target_name == "FRSKY_TX_R9M_VIA_STLINK_OLD_BOOTLOADER_DEPRECATED":
         print("you are using the old bootloader, please update this will be removed soon")
-        env.AddPostAction("buildprog", [opentx.gen_frsky])
+        env.AddPostAction("buildprog", opentx.gen_frsky)
     elif "APLHA_900_TX" in target_name:
         env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
     elif "_R9M_" in target_name or "ES915TX" in target_name:
-        env.AddPostAction("buildprog", [opentx.gen_elrs])
+        env.AddPostAction("buildprog", opentx.gen_elrs)
+        env.AddPreAction("upload", opentx.gen_elrs)
         if "WIFI" in target_name:
             env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
         else:
@@ -25,3 +30,14 @@ if stm and "$UPLOADER $UPLOADERFLAGS" in env.get('UPLOADCMD', '$UPLOADER $UPLOAD
         env.Replace(UPLOADCMD=stlink.on_upload)
     elif "_BETAFLIGHTPASSTHROUGH" in target_name:
         env.Replace(UPLOADCMD=UARTupload.on_upload)
+elif platform in ['espressif8266']:
+    env.AddPostAction("buildprog", esp_compress.compressFirmware)
+    env.AddPreAction("${BUILD_DIR}/spiffs.bin",
+                     [esp_compress.compress_files])
+    env.AddPreAction("${BUILD_DIR}/${ESP8266_FS_IMAGE_NAME}.bin",
+                     [esp_compress.compress_files])
+    env.AddPostAction("${BUILD_DIR}/${ESP8266_FS_IMAGE_NAME}.bin",
+                     [esp_compress.compress_fs_bin])
+    if "_WIFI" in target_name:
+        env.Replace(UPLOAD_PROTOCOL="custom")
+        env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
