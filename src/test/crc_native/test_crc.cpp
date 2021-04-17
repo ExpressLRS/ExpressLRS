ae8cb5df8a318cddf90e2fede248e09e2edb1b3f
Paul Kendall <pkendall64@gmail.com>
2021-04-18 09:32:49 +1200

    Add a CRC8 test as well

--- src/test/crc_native/test_crc.cpp
+++ src/test/crc_native/test_crc.cpp
@@ -4,7 +4,7 @@
 #include <crc.h>
 
 
-void test_crc(void)
+void test_crc13(void)
 {
     uint8_t bytes[] = {45, 46, 47, 48, 49, 50, 51};
 
@@ -17,10 +17,25 @@ void test_crc(void)
     TEST_ASSERT_EQUAL((int)(crc & 0x1FFF), c);
 }
 
+void test_crc8(void)
+{
+    // Size of a CRSF
+    uint8_t bytes[] = {45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55};
+
+    uCRC_t ccrc = uCRC_t("CRC8", 8, 0x107, 0, false, false, 0);
+    uint64_t crc = ccrc.get_raw_crc(bytes, 7, 0);
+
+    GENERIC_CRC8 ecrc = GENERIC_CRC8(0x07);
+    uint16_t c = ecrc.calc(bytes, 7);
+
+    TEST_ASSERT_EQUAL((int)(crc & 0xFF), c);
+}
+
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_crc);
+    RUN_TEST(test_crc13);
+    RUN_TEST(test_crc8);
     UNITY_END();
 
     return 0;
