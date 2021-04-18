624a6e37f258699f4b12994afdbaddfd8f40127e
Paul Kendall <pkendall64@gmail.com>
2021-04-18 23:25:08 +1200

    Extend CRC test to use random data (#487)
    * Extend CRC tests to use randomised test data
    
    * Add 6-bit flip test
    
    * Added extra test to check that 5 changed bits are detected as an error
    
    * Changes to tests to use CRC poly from common.h
    
    And also only use 50bits of data as in the OTA packet
    Provide a define 'BIG_TEST' which will perform a million iterations vs 1000.
    
    * Fixes as per review
    
    * Increase number of big iterations to 10 million
    
    Add "test" to print percentage of false positives for 6, 8 and 10 bit errors

--- src/test/crc_native/test_crc.cpp
+++ src/test/crc_native/test_crc.cpp
@@ -2,40 +2,158 @@
 #include <unity.h>
 #include "ucrc_t.h"
 #include <crc.h>
+#include "common.h"
 
+#ifdef BIG_TEST
+#define NUM_ITERATIONS 10000000
+#else
+#define NUM_ITERATIONS 1000
+#endif
 
-void test_crc13(void)
+static char *genMsg(uint8_t bytes[], int len) {
+    static char buf[80];
+    char hex[4];
+    sprintf(buf, "bytes ");
+    for (int i = 0; i < len; i++)
+    {
+        sprintf(hex, "%02x ", bytes[i]);
+        strcat(buf, hex);
+    }
+    return buf;
+}
+
+void test_crc13_implementation_compatibility(void)
 {
-    uint8_t bytes[] = {45, 46, 47, 48, 49, 50, 51};
+    uint8_t bytes[7];
+    for (int i = 0; i < sizeof(bytes); i++)
+        bytes[i] = random() % 255;
+    bytes[0] &= 0b11;
 
-    uCRC_t ccrc = uCRC_t("CRC13", 13, 0x3d2f, 0, false, false, 0);
+    uCRC_t ccrc = uCRC_t("CRC13", 13, ELRS_CRC13_POLY, 0, false, false, 0);
     uint64_t crc = ccrc.get_raw_crc(bytes, 7, 0);
 
-    GENERIC_CRC13 ecrc = GENERIC_CRC13(0x1d2f);
+    GENERIC_CRC13 ecrc = GENERIC_CRC13(ELRS_CRC13_POLY);
     uint16_t c = ecrc.calc(bytes, 7, 0);
 
-    TEST_ASSERT_EQUAL((int)(crc & 0x1FFF), c);
+    TEST_ASSERT_EQUAL_MESSAGE((int)(crc & 0x1FFF), c, genMsg(bytes, sizeof(bytes)));
+}
+
+void test_crc13_flip_n(int flip)
+{
+    int false_positive = 0;
+    GENERIC_CRC13 ccrc = GENERIC_CRC13(ELRS_CRC13_POLY);
+
+    for (int x=0 ; x<NUM_ITERATIONS ; x++) {
+        uint8_t bytes[7];
+        for (int i = 0; i < sizeof(bytes); i++)
+            bytes[i] = random() % 255;
+        bytes[0] &= 0b11;
+
+        uint16_t c = ccrc.calc(bytes, 7, 0);
+
+        // Flip 'flip' random bits
+        for (int i=0 ; i<flip ; i++) {
+            int pos = random() % 50;
+            if (pos > 1)
+                pos += 6;
+            bytes[pos / 8] ^= 1 << (pos % 8);
+        }
+
+        uint16_t e = ccrc.calc(bytes, 7, 0);
+        if (c == e)
+        {
+            //fprintf(stderr, "False +ve %s\n", genMsg(bytes, sizeof(bytes)));
+            false_positive++;
+        }
+    }
+    printf("%d out of %d false positives, %f%%\n", false_positive, NUM_ITERATIONS, false_positive*100.0/NUM_ITERATIONS);
+}
+
+void test_crc13_flip5(void)
+{
+    GENERIC_CRC13 ccrc = GENERIC_CRC13(ELRS_CRC13_POLY);
+
+    for (int x = 0; x < NUM_ITERATIONS; x++)
+    {
+        uint8_t bytes[7];
+        for (int i = 0; i < sizeof(bytes); i++)
+            bytes[i] = random() % 255;
+        bytes[0] &= 0b11;
+
+        uint16_t c = ccrc.calc(bytes, 7, 0);
+
+        // Flip 4 random bits
+        for (int i = 0; i < 4; i++)
+        {
+            int pos = random() % 50;
+            if (pos > 1) 
+                pos += 6;
+            bytes[pos / 8] ^= 1 << (pos % 8);
+        }
+
+        // Flip all the bits one after the other
+        for (int i = 0; i < 50; i++)
+        {
+            // flip bit i and test
+            int pos = i;
+            if (pos > 1)
+                pos += 6;
+            bytes[pos / 8] ^= 1 << (pos % 8);
+
+            uint16_t e = ccrc.calc(bytes, 7, 0);
+            if (c == e)
+            {
+                fprintf(stderr, "False +ve %s\n", genMsg(bytes, sizeof(bytes)));
+            }
+
+            // flip bit i back again
+            bytes[pos / 8] ^= 1 << (pos % 8);
+        }
+    }
 }
 
 void test_crc8(void)
 {
-    // Size of a CRSF
-    uint8_t bytes[] = {45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55};
+    // Size of a CRSF packet
+    uint8_t bytes[11];
+    for (int i = 0; i < sizeof(bytes); i++)
+        bytes[i] = random() % 255;
 
-    uCRC_t ccrc = uCRC_t("CRC8", 8, 0x107, 0, false, false, 0);
+    uCRC_t ccrc = uCRC_t("CRC8", 8, ELRS_CRC_POLY, 0, false, false, 0);
     uint64_t crc = ccrc.get_raw_crc(bytes, 7, 0);
 
-    GENERIC_CRC8 ecrc = GENERIC_CRC8(0x07);
+    GENERIC_CRC8 ecrc = GENERIC_CRC8(ELRS_CRC_POLY);
     uint16_t c = ecrc.calc(bytes, 7);
 
-    TEST_ASSERT_EQUAL((int)(crc & 0xFF), c);
+    TEST_ASSERT_EQUAL_MESSAGE((int)(crc & 0xFF), c, genMsg(bytes, sizeof(bytes)));
+}
+
+void test_crc13_flip_6()
+{
+    test_crc13_flip_n(6);
+}
+
+void test_crc13_flip_8()
+{
+    test_crc13_flip_n(8);
+}
+
+void test_crc13_flip_10()
+{
+    test_crc13_flip_n(10);
 }
 
 int main(int argc, char **argv)
 {
+    srandom(micros());
+
     UNITY_BEGIN();
-    RUN_TEST(test_crc13);
+    RUN_TEST(test_crc13_implementation_compatibility);
+    RUN_TEST(test_crc13_flip5);
     RUN_TEST(test_crc8);
+    RUN_TEST(test_crc13_flip_6);
+    RUN_TEST(test_crc13_flip_8);
+    RUN_TEST(test_crc13_flip_10);
     UNITY_END();
 
     return 0;
