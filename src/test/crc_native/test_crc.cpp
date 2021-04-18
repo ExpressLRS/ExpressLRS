1f69848b35a088df70fb2895ad4871ddb0a44d42
Paul Kendall <pkendall64@gmail.com>
2021-04-18 14:10:58 +1200

    Add 6-bit flip test

--- src/test/crc_native/test_crc.cpp
+++ src/test/crc_native/test_crc.cpp
@@ -22,7 +22,36 @@ void test_crc13(void)
         sprintf(hex, "%02x ", bytes[i]);
         strcat(buf, hex);
     }
-    TEST_ASSERT_EQUAL_INT_MESSAGE((int)(crc & 0x1FFF), c, buf);
+    TEST_ASSERT_EQUAL_MESSAGE((int)(crc & 0x1FFF), c, buf);
+}
+
+void test_crc13_flip6(void)
+{
+    uint8_t bytes[7];
+    for (int i = 0; i < sizeof(bytes); i++)
+        bytes[i] = random() % 255;
+
+    GENERIC_CRC13 ccrc = GENERIC_CRC13(0x1d2f);
+    uint16_t c = ccrc.calc(bytes, 7, 0);
+
+    // Flip 6 random bits
+    for (int i=0 ; i<6 ; i++) {
+        int pos = random() % 52;
+        bytes[pos/8] ^= 1 << (pos % 8);
+    }
+
+    GENERIC_CRC13 ecrc = GENERIC_CRC13(0x1d2f);
+    uint16_t e = ecrc.calc(bytes, 7, 0);
+
+    char buf[80];
+    char hex[4];
+    sprintf(buf, "bytes ");
+    for (int i = 0; i < sizeof(bytes); i++)
+    {
+        sprintf(hex, "%02x ", bytes[i]);
+        strcat(buf, hex);
+    }
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(c, e, buf);
 }
 
 void test_crc8(void)
@@ -46,13 +75,14 @@ void test_crc8(void)
         sprintf(hex, "%02x ", bytes[i]);
         strcat(buf, hex);
     }
-    TEST_ASSERT_EQUAL_INT_MESSAGE((int)(crc & 0xFF), c, buf);
+    TEST_ASSERT_EQUAL_MESSAGE((int)(crc & 0xFF), c, buf);
 }
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
     RUN_TEST(test_crc13);
+    RUN_TEST(test_crc13_flip6);
     RUN_TEST(test_crc8);
     UNITY_END();
 
