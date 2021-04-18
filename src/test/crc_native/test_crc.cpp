755fbab8541a9a4d9c59adc179af0ab91f1e55a8
Paul Kendall <pkendall64@gmail.com>
2021-04-18 13:50:21 +1200

    Extend CRC tests to use randomised test data

--- src/test/crc_native/test_crc.cpp
+++ src/test/crc_native/test_crc.cpp
@@ -3,10 +3,11 @@
 #include "ucrc_t.h"
 #include <crc.h>
 
-
 void test_crc13(void)
 {
-    uint8_t bytes[] = {45, 46, 47, 48, 49, 50, 51};
+    uint8_t bytes[7];
+    for (int i = 0; i < sizeof(bytes); i++)
+        bytes[i] = random() % 255;
 
     uCRC_t ccrc = uCRC_t("CRC13", 13, 0x3d2f, 0, false, false, 0);
     uint64_t crc = ccrc.get_raw_crc(bytes, 7, 0);
@@ -14,13 +15,22 @@ void test_crc13(void)
     GENERIC_CRC13 ecrc = GENERIC_CRC13(0x1d2f);
     uint16_t c = ecrc.calc(bytes, 7, 0);
 
-    TEST_ASSERT_EQUAL((int)(crc & 0x1FFF), c);
+    char buf[80];
+    char hex[4];
+    sprintf(buf, "bytes ");
+    for (int i=0 ; i<sizeof(bytes) ; i++) {
+        sprintf(hex, "%02x ", bytes[i]);
+        strcat(buf, hex);
+    }
+    TEST_ASSERT_EQUAL_INT_MESSAGE((int)(crc & 0x1FFF), c, buf);
 }
 
 void test_crc8(void)
 {
-    // Size of a CRSF
-    uint8_t bytes[] = {45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55};
+    // Size of a CRSF packet
+    uint8_t bytes[11];
+    for (int i = 0; i < sizeof(bytes); i++)
+        bytes[i] = random() % 255;
 
     uCRC_t ccrc = uCRC_t("CRC8", 8, 0x107, 0, false, false, 0);
     uint64_t crc = ccrc.get_raw_crc(bytes, 7, 0);
@@ -28,7 +38,15 @@ void test_crc8(void)
     GENERIC_CRC8 ecrc = GENERIC_CRC8(0x07);
     uint16_t c = ecrc.calc(bytes, 7);
 
-    TEST_ASSERT_EQUAL((int)(crc & 0xFF), c);
+    char buf[80];
+    char hex[4];
+    sprintf(buf, "bytes ");
+    for (int i = 0; i < sizeof(bytes); i++)
+    {
+        sprintf(hex, "%02x ", bytes[i]);
+        strcat(buf, hex);
+    }
+    TEST_ASSERT_EQUAL_INT_MESSAGE((int)(crc & 0xFF), c, buf);
 }
 
 int main(int argc, char **argv)
