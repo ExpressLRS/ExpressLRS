2f6e7305ff42f05cb092ae009ec280b50070a6db
Alessandro <AlessandroAUS@gmail.com>
2021-04-20 14:17:43 +1000

    Update CRSF.cpp
    according to the opentx guys this delay here isn't needed anyway

--- src/lib/CRSF/CRSF.cpp
+++ src/lib/CRSF/CRSF.cpp
@@ -632,7 +632,7 @@ void ICACHE_RAM_ATTR CRSF::handleUARTin()
                 {
                     if (ProcessPacket())
                     {
-                        delayMicroseconds(50);
+                        //delayMicroseconds(50);
                         handleUARTout();
                     }
                 }
