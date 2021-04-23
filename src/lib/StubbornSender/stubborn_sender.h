0722174b4fc2618f14ef55a8a2041f85d6f48e63
Bryan Mayland <bmayland@capnbry.net>
2021-04-23 12:23:49 -0400

    Dynamically adjust StubbonSender timeout based on rate The timeout was set too low by mistake when merging MSP, so 1:64 and 1:128 rates were timing out before the ack came. Fixes #509

--- src/lib/StubbornSender/stubborn_sender.h
+++ src/lib/StubbornSender/stubborn_sender.h
@@ -2,7 +2,8 @@
 
 #include <cstdint>
 
-#define WAIT_FOR_RESYNC 100
+// The number of times to resend the same package index before going to RESYNC
+#define SSENDER_MAX_MISSED_PACKETS 20
 
 typedef enum {
     SENDER_IDLE = 0,
@@ -17,19 +18,21 @@ class StubbornSender
 public:
     StubbornSender(uint8_t maxPackageIndex);
     void ResetState();
+    void UpdateTelemetryRate(uint16_t airRate, uint8_t tlmRatio, uint8_t tlmBurst);
     void SetDataToTransmit(uint8_t lengthToTransmit, uint8_t* dataToTransmit, uint8_t bytesPerCall);
     void GetCurrentPayload(uint8_t *packageIndex, uint8_t *count, uint8_t **currentData);
     void ConfirmCurrentPayload(bool telemetryConfirmValue);
     bool IsActive();
 private:
     uint8_t *data;
-    volatile uint8_t length;
-    volatile uint8_t bytesPerCall;
-    volatile uint8_t currentOffset;
-    volatile uint8_t currentPackage;
-    volatile bool waitUntilTelemetryConfirm;
-    volatile bool resetState;
-    volatile uint16_t waitCount;
-    volatile uint8_t maxPackageIndex;
-    volatile stubborn_sender_state_s senderState;
+    uint8_t length;
+    uint8_t bytesPerCall;
+    uint8_t currentOffset;
+    uint8_t currentPackage;
+    bool waitUntilTelemetryConfirm;
+    bool resetState;
+    uint16_t waitCount;
+    uint16_t maxWaitCount;
+    uint8_t maxPackageIndex;
+    stubborn_sender_state_s senderState;
 };
