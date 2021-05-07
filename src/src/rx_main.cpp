9b8da552cd3410a916975cfe910aaa57986eef75
Paul Kendall <pkendall64@gmail.com>
2021-05-07 14:24:21 +1200

    Enable target_name check on buttonless passthrough
    - Extend the reboot command on RX to print current target_name before rebooting into flash mode
    - Extend the BFInitPassthrough script to accept the PIOENV as target and compare against RX

--- src/src/rx_main.cpp
+++ src/src/rx_main.cpp
@@ -1343,6 +1343,8 @@ struct bootloader {
 
 void reset_into_bootloader(void)
 {
+    CRSF_TX_SERIAL.println((const char *)&target_name[4]);
+    CRSF_TX_SERIAL.flush();
 #if defined(PLATFORM_STM32)
     delay(100);
     Serial.println("Jumping to Bootloader...");
@@ -1361,6 +1363,7 @@ void reset_into_bootloader(void)
 
     HAL_NVIC_SystemReset();
 #elif defined(PLATFORM_ESP8266)
+    delay(100);
     ESP.rebootIntoUartDownloadMode();
 #endif
 }
