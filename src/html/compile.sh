a91a8365eb375ba69b87642c05f40aaaa658c0d3
Paul Kendall <pkendall64@gmail.com>
2021-05-13 10:03:26 +1200

    Compile HTML as pre-action for cpp fiiles
    Preprocess the HTML to include the tag or branch and short commit hash

--- src/html/compile.sh
+++ src/html/compile.sh
@@ -3,7 +3,15 @@
 # Make sure you install css-html-js-minify
 # pip install css-html-js-minify
 #
+cd "$(dirname "$0")"
+
 css-html-js-minify --quiet .
+
+#!/bin/sh
+
+GITBRANCH=`git describe --tags --exact-match 2> /dev/null || git symbolic-ref -q --short HEAD`
+GITHASH=`git rev-parse --short HEAD`
+VERSION="$GITBRANCH ($GITHASH)"
 
 echo "static const char PROGMEM FLAG[] = {" > ../src/flag_svg.h
 cat flag.svg | gzip -9n | xxd -i >> ../src/flag_svg.h
@@ -14,7 +22,7 @@ cat main.min.css | gzip -9n | xxd -i >> ../src/main_css.h
 echo "};" >> ../src/main_css.h
 
 echo "static const char PROGMEM INDEX_HTML[] = {" > ../src/ESP32_WebContent.h
-cat tx_index.html | gzip -9n | xxd -i >> ../src/ESP32_WebContent.h
+cat tx_index.html | sed "s/@VERSION@/$VERSION/" | gzip -9n | xxd -i >> ../src/ESP32_WebContent.h
 echo "};" >> ../src/ESP32_WebContent.h
 echo "" >> ../src/ESP32_WebContent.h
 echo "static const char PROGMEM SCAN_JS[] = {" >> ../src/ESP32_WebContent.h
@@ -22,7 +30,7 @@ cat scan.min.js | gzip -9n | xxd -i >> ../src/ESP32_WebContent.h
 echo "};" >> ../src/ESP32_WebContent.h
 
 echo "static const char PROGMEM INDEX_HTML[] = {" > ../src/ESP8266_WebContent.h
-cat rx_index.html | gzip -9n | xxd -i >> ../src/ESP8266_WebContent.h
+cat rx_index.html | sed "s/@VERSION@/$VERSION/" | gzip -9n | xxd -i >> ../src/ESP8266_WebContent.h
 echo "};" >> ../src/ESP8266_WebContent.h
 echo "" >> ../src/ESP8266_WebContent.h
 echo "static const char PROGMEM SCAN_JS[] = {" >> ../src/ESP8266_WebContent.h
