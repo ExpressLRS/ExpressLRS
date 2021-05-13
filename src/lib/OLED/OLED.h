1d29bcbdfe39c4ce09fab543b2a8d4d28aa61cf1
Brandon Geraci <brandon.geraci@gmail.com>
2021-05-12 23:50:13 -0500

    Removing bad OLED Functions

--- src/lib/OLED/OLED.h
+++ src/lib/OLED/OLED.h
@@ -13,9 +13,6 @@ private:
 
 public:
     static void displayLogo();
-    static void updatePower(PowerLevels_e rate); 
-    static void updateRate(expresslrs_RFrates_e rate);
-    static void updateTelmRatio(expresslrs_tlm_ratio_e rate);
     static void updateScreen(PowerLevels_e power, expresslrs_RFrates_e rate, expresslrs_tlm_ratio_e ratio);
 
 };
