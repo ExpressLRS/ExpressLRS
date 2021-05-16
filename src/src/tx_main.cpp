9bc2220330ddbc509853c26e1f6de2935d20740e
phobos- <pawel.m.stefanski@gmail.com>
2021-05-16 14:30:06 +0200

    Allow tx to return to original rate, mod params, iq inversion after bind

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -1007,6 +1007,7 @@ void ExitBindingMode()
 
   InBindingMode = false;
   MspSender.ResetState();
+  SetRFLinkRate(config.GetRate()); //return to original rate
 
   Serial.println("Exiting binding mode");
 }
