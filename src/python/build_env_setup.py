b41232cc71410f6d3c5daa16715b8b3c7e4aea3e
Cru Waller <24904289+cruwaller@users.noreply.github.com>
2021-06-18 19:31:26 +0300

    Fix: STM32 build env setup cleanup to avoid mismatch with target names (#689)
    * Fix: STM32 build env setup cleanup to avoid mismatch with target names
    
    * scripts: comments added into build_env_setup.py

--- src/python/build_env_setup.py
+++ src/python/build_env_setup.py
@@ -17,27 +17,40 @@ if stm and "$UPLOADER $UPLOADERFLAGS" in env.get('UPLOADCMD', '$UPLOADER $UPLOAD
     if target_name == "FRSKY_TX_R9M_VIA_STLINK_OLD_BOOTLOADER_DEPRECATED":
         print("you are using the old bootloader, please update this will be removed soon")
         env.AddPostAction("buildprog", opentx.gen_frsky)
-    elif "APLHA_900_TX" in target_name:
-        env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
-    elif "_R9M_" in target_name or "ES915TX" in target_name or "GHOST_2400_TX" in target_name or \
-         "NAMIMNORC_VOYAGER" in target_name or "NAMIMNORC_FLASH" in target_name:
+
+    # Check whether the target is using WIFI (backpack logger) upload
+    elif "_WIFI" in target_name:
+        # Generate 'firmware.elrs' file to be used for uploading (prio to bin file).
+        # This enables support for OTA updates with the "overlay double" R9M module
+        # bootloader. Bin file upload also requires the correct FLASH_OFFSET value
+        # and that can be ignored when '.elrs' file is used.
         env.AddPostAction("buildprog", opentx.gen_elrs)
         env.AddPreAction("upload", opentx.gen_elrs)
-        if "WIFI" in target_name:
-            env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
-        else:
-            env.Replace(UPLOADCMD=stlink.on_upload)
-    elif "_STLINK" in target_name:
-        env.Replace(UPLOADCMD=stlink.on_upload)
+        env.Replace(UPLOADCMD=upload_via_esp8266_backpack.on_upload)
+
+    # Check whether the target is using FC passthrough upload (receivers)
     elif "_BETAFLIGHTPASSTHROUGH" in target_name:
         env.Replace(UPLOADCMD=UARTupload.on_upload)
+
+    # Check whether the target is using DFU upload
     elif "_DFU" in target_name:
         board = env.BoardConfig()
         # PIO's ststm32 forces stm32f103 to upload via maple_upload,
-        # but we really actually truly want dfu-util
+        # but we really actually truly want dfu-util.
         env.Replace(UPLOADER="dfu-util", UPLOADERFLAGS=["-d", "0483:df11",
             "-s", "%s:leave" % board.get("upload.offset_address", "0x08001000"),
             "-D"], UPLOADCMD='$UPLOADER $UPLOADERFLAGS "${SOURCE.get_abspath()}"')
+
+    # Default to ST-Link uploading
+    # Note: this target is also used to build 'firmware.elrs' binary
+    #       for handset flashing
+    else:
+        if "_TX_" in target_name:
+            # Generate 'firmware.elrs' file for TX targets only
+            env.AddPostAction("buildprog", opentx.gen_elrs)
+            env.AddPreAction("upload", opentx.gen_elrs)
+        env.Replace(UPLOADCMD=stlink.on_upload)
+
 elif platform in ['espressif8266']:
     env.AddPostAction("buildprog", esp_compress.compressFirmware)
     env.AddPreAction("${BUILD_DIR}/spiffs.bin",
