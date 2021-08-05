7fc8c26e76a51b342655b71886a605f37f3f2ffb
Sunjun Kim <SunjunKim@users.noreply.github.com>
2021-08-06 01:34:07 +0900

    Trailing whitespaces cleanup

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -141,7 +141,7 @@ void DynamicPower_Update()
 {
   #ifdef USE_DYNAMIC_POWER
 
-  // =============  DYNAMIC_POWER_BOOST: Switch-triggered power boost up ==============  
+  // =============  DYNAMIC_POWER_BOOST: Switch-triggered power boost up ==============
   #ifdef DYNAMIC_POWER_BOOST
     // if a user selected to disable dynamic power (ch16)
     if(CRSF_to_BIT(crsf.ChannelDataIn[DYNAMIC_POWER_BOOST])) {
@@ -150,14 +150,14 @@ void DynamicPower_Update()
       return;
     }
   #endif  // DYNAMIC_POWER_BOOST
-  
+
   // if telemetry is not arrived, quick return.
   if (!dynamic_power_updated)
     return;
   dynamic_power_updated = false;
 
-  // =============  LQ-based power boost up ==============  
-  // Quick boost up of power when detected any emergency LQ drops. 
+  // =============  LQ-based power boost up ==============
+  // Quick boost up of power when detected any emergency LQ drops.
   // It should be useful for bando or sudden lost of LoS cases.
   int32_t lq_current = crsf.LinkStatistics.uplink_Link_quality;
   int32_t lq_diff = (dynamic_power_avg_lq>>16) - lq_current;
@@ -170,12 +170,12 @@ void DynamicPower_Update()
       dynamic_power_rssi_n = 0;
   }
   // Moving average calculation, multiplied by 2^16 for avoiding (costly) floating point operation, while maintaining some fraction parts.
-  dynamic_power_avg_lq = ((int32_t)(DYNAMIC_POWER_MOVING_AVG_K - 1) * dynamic_power_avg_lq + (lq_current<<16)) / DYNAMIC_POWER_MOVING_AVG_K;   
+  dynamic_power_avg_lq = ((int32_t)(DYNAMIC_POWER_MOVING_AVG_K - 1) * dynamic_power_avg_lq + (lq_current<<16)) / DYNAMIC_POWER_MOVING_AVG_K;
 
   // =============  RSSI-based power adjustment ==============
   // It is working slowly, suitable for a general long-range flights.
-  
-  // Get the RSSI from the selected antenna.  
+
+  // Get the RSSI from the selected antenna.
   int8_t rssi = (crsf.LinkStatistics.active_antenna == 0)? crsf.LinkStatistics.uplink_RSSI_1: crsf.LinkStatistics.uplink_RSSI_2;
 
   dynamic_power_rssi_sum += rssi;
@@ -568,7 +568,7 @@ void HandleUpdateParameter()
       config.SetRate(enumRatetoIndex((expresslrs_RFrates_e)crsf.ParameterUpdateData[1]));
     #if defined(HAS_OLED)
       OLED.updateScreen(OLED.getPowerString((PowerLevels_e)POWERMGNT.currPower()),
-                        OLED.getRateString((expresslrs_RFrates_e)crsf.ParameterUpdateData[1]), 
+                        OLED.getRateString((expresslrs_RFrates_e)crsf.ParameterUpdateData[1]),
                         OLED.getTLMRatioString((expresslrs_tlm_ratio_e)(ExpressLRS_currAirRate_Modparams->TLMinterval)), commitStr);
     #endif
     }
@@ -582,7 +582,7 @@ void HandleUpdateParameter()
       config.SetTlm((expresslrs_tlm_ratio_e)crsf.ParameterUpdateData[1]);
     #if defined(HAS_OLED)
       OLED.updateScreen(OLED.getPowerString((PowerLevels_e)POWERMGNT.currPower()),
-                        OLED.getRateString((expresslrs_RFrates_e)ExpressLRS_currAirRate_Modparams->enum_rate), 
+                        OLED.getRateString((expresslrs_RFrates_e)ExpressLRS_currAirRate_Modparams->enum_rate),
                         OLED.getTLMRatioString((expresslrs_tlm_ratio_e)crsf.ParameterUpdateData[1]), commitStr);
     #endif
     }
@@ -594,10 +594,10 @@ void HandleUpdateParameter()
       PowerLevels_e newPower = (PowerLevels_e)crsf.ParameterUpdateData[1];
       Serial.println(newPower, DEC);
       config.SetPower(newPower < MaxPower ? newPower : MaxPower);
-      
+
       #if defined(HAS_OLED)
        OLED.updateScreen(OLED.getPowerString((PowerLevels_e)crsf.ParameterUpdateData[1]),
-                         OLED.getRateString((expresslrs_RFrates_e)ExpressLRS_currAirRate_Modparams->enum_rate), 
+                         OLED.getRateString((expresslrs_RFrates_e)ExpressLRS_currAirRate_Modparams->enum_rate),
                          OLED.getTLMRatioString((expresslrs_tlm_ratio_e)ExpressLRS_currAirRate_Modparams->TLMinterval), commitStr);
       #endif
     }
@@ -849,7 +849,7 @@ void setup()
   SetRFLinkRate(config.GetRate());
   ExpressLRS_currAirRate_Modparams->TLMinterval = (expresslrs_tlm_ratio_e)config.GetTlm();
   POWERMGNT.setPower((PowerLevels_e)config.GetPower());
-  
+
 
   hwTimer.init();
   //hwTimer.resume();  //uncomment to automatically start the RX timer and leave it running
