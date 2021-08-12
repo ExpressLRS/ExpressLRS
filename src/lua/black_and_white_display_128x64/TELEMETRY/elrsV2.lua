5cd1071d957409c760989245d3d6bba6435d66e5
StonedDawg <68074253+StonedDawg@users.noreply.github.com>
2021-08-12 23:23:38 +0700

    add back lua for V1.0.x (#778)
    * add readme + old lua
    
    * add image
    
    * update readme
    
    * update text

--- src/lua/black_and_white_display_128x64/TELEMETRY/elrsV2.lua
+++ src/lua/black_and_white_display_128x64/TELEMETRY/elrsV2.lua
@@ -1,3 +1,19 @@
+---- #########################################################################
+---- #                                                                       #
+---- # Copyright (C) OpenTX                                                  #
+-----#                                                                       #
+---- # License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html               #
+---- #                                                                       #
+---- # This program is free software; you can redistribute it and/or modify  #
+---- # it under the terms of the GNU General Public License version 2 as     #
+---- # published by the Free Software Foundation.                            #
+---- #                                                                       #
+---- # This program is distributed in the hope that it will be useful        #
+---- # but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+---- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+---- # GNU General Public License for more details.                          #
+---- #                                                                       #
+---- #########################################################################
 local deviceId = 0
 local deviceName = ""
 local lineIndex = 0
