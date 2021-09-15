6130278cc66d9106237198e1e78d3ebbcfe16929
Bryan Mayland <bmayland@capnbry.net>
2021-09-15 17:42:06 -0400

    TelemBurst sending one extra packet per burst

--- src/src/rx_main.cpp
+++ src/src/rx_main.cpp
@@ -287,7 +287,9 @@ bool ICACHE_RAM_ATTR HandleSendTelemetryResponse()
         case ELRS_TELEMETRY_TYPE_LINK:
             #ifdef ENABLE_TELEMETRY
             NextTelemetryType = ELRS_TELEMETRY_TYPE_DATA;
-            telemetryBurstCount = 0;
+            // Start the count at 1 because the next will be DATA and doing +1 before checking
+            // against Max below is for some reason 10 bytes more code
+            telemetryBurstCount = 1;
             #else
             NextTelemetryType = ELRS_TELEMETRY_TYPE_LINK;
             #endif
