ba88ed3379e437e0ad97b4b86c0129ba8e436f87
AlessandroAU <alessandroaus@gmail.com>
2021-09-29 20:54:43 +1000

    Adds override for incorrect target detection (#924)
    * init
    
    * Update BFinitPassthrough.py

---
+++ src/python/inputimeout/__init__.py
@@ -0,0 +1,5 @@
+from .inputimeout import inputimeout, TimeoutOccurred   # noqa
+from .__version__ import (   # noqa
+    __version__, __author__, __author_email__, __copyright__, __license__,
+    __description__, __title__, __url__,
+)
