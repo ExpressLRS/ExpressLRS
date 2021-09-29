f07396bf3fe4be56269b4c5fba08184432672cf1
Paul Kendall <pkendall64@gmail.com>
2021-09-29 15:57:57 +1300

    Commit after each model is upgraded for NVS (#937)
    Move eeprom begin back to tx_main. Doesn’t work in config class!

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -59,6 +59,7 @@ GENERIC_CRC14 ota_crc(ELRS_CRC14_POLY);
 CRSF crsf;
 POWERMGNT POWERMGNT;
 MSP msp;
+ELRS_EEPROM eeprom;
 TxConfig config;
 
 static bool webserverPreventAutoStart = false;
@@ -972,6 +973,8 @@ void setup()
   hwTimer.callbackTock = &timerCallbackNormal;
   DBGLN("ExpressLRS TX Module Booted...");
 
+  eeprom.Begin(); // Init the eeprom
+  config.SetStorageProvider(&eeprom); // Pass pointer to the Config class for access to storage
   config.Load(); // Load the stored values from eeprom
 
   Radio.currFreq = GetInitialFreq(); //set frequency first or an error will occur!!!
