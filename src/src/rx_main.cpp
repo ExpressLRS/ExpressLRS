11eb7fe514761c5a87b131c590c840a2c46381ae
Jye <JyeSmith@users.noreply.github.com>
2021-10-04 13:27:03 +1000

    invertIQ refact

--- src/src/rx_main.cpp
+++ src/src/rx_main.cpp
@@ -245,7 +245,6 @@ void SetRFLinkRate(uint8_t index) // Set speed of RF link
 {
     expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
     expresslrs_rf_pref_params_s *const RFperf = get_elrs_RFperfParams(index);
-    bool invertIQ = UID[5] & 0x01;
 
     hwTimer.updateInterval(ModParams->interval);
     Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, GetInitialFreq(), ModParams->PreambleLen, invertIQ, ModParams->PayloadLength);
