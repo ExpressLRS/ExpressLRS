0121d38258240cc90c81483bd2122fa17283237a
Paul Kendall <pkendall64@gmail.com>
2021-10-20 09:18:49 +1300

    Fixes for buzzer

--- src/lib/BUZZER/devBuzzer.cpp
+++ src/lib/BUZZER/devBuzzer.cpp
@@ -29,87 +29,110 @@ static const uint16_t melody[][2] = {{659,300},{659,300},{523,100},{659,300},{78
 
 static uint8_t tunepos = 0;
 static bool callAfterComplete = false;
+static bool startComplete = false;
+static const uint16_t (*_tune)[2];
+static int _numTones;
 
-static int playTune(bool start, const uint16_t tune[][2], int numTones)
+static int playTune()
 {
-    if (start)
-    {
-        pinMode(GPIO_PIN_BUZZER, OUTPUT);
-        tunepos = 0;
-    }
-    if (tunepos >= numTones)
+    if (tunepos >= _numTones)
     {
         noTone(GPIO_PIN_BUZZER);
         pinMode(GPIO_PIN_BUZZER, INPUT);
         tunepos = 0;
-
+        DBGLN(">> end tune");
         return DURATION_NEVER;
     }
-    tone(GPIO_PIN_BUZZER, tune[tunepos][0], tune[tunepos][1]);
+    tone(GPIO_PIN_BUZZER, _tune[tunepos][0], _tune[tunepos][1]);
     tunepos++;
-    return tune[tunepos-1][1];
+    return _tune[tunepos-1][1];
 }
 
-static int updateBuzzer(bool start)
+static int startTune(const uint16_t tune[][2], int numTones)
 {
-    static bool startComplete = false;
+    _tune = tune;
+    _numTones = numTones;
+    pinMode(GPIO_PIN_BUZZER, OUTPUT);
+    tunepos = 0;
+    return playTune();
+}
+
+static int updateBuzzer()
+{
     if (connectionState == radioFailed)
     {
-        return playTune(start, failedTune, ARRAY_SIZE(failedTune));
+        DBGLN(">> start failed tune");
+        return startTune(failedTune, ARRAY_SIZE(failedTune));
     }
-#if !defined(DISABLE_STARTUP_BEEP)
-    else if (!startComplete)
-    {
-        int delay = playTune(start, melody, ARRAY_SIZE(melody));
-        if (delay == DURATION_NEVER)
-        {
-            startComplete = true;
-        }
-        return delay;
-    }
-#endif
     else if (connectionState == noCrossfire)
     {
-        return playTune(start, noCrossfireTune, ARRAY_SIZE(noCrossfireTune));
+        DBGLN(">> start no-xfire tune");
+        return startTune(noCrossfireTune, ARRAY_SIZE(noCrossfireTune));
     }
 #if !defined(DISABLE_STARTUP_BEEP)
-    else if (connectionState < MODE_STATES)
+    else if (connectionState == connected || connectionState == disconnected)
     {
-        return playTune(start, crossfireTune, ARRAY_SIZE(crossfireTune));
+        DBGLN(">> start conn/disconn tune");
+        return startTune(crossfireTune, ARRAY_SIZE(crossfireTune));
     }
 #endif
     return DURATION_NEVER;
 }
 
+static int start()
+{
+#if !defined(DISABLE_STARTUP_BEEP)
+    DBGLN(">> start startup tune");
+    return startTune(melody, ARRAY_SIZE(melody));
+#else
+    return DURATION_NEVER;
+#endif
+}
+
 static int event()
 {
+    static connectionState_e lastConnectionState = MODE_STATES;
     if (tunepos == 0)
     {
-        return updateBuzzer(true);
+        if (connectionState != lastConnectionState)
+        {
+            DBGLN(">> starting %d -> %d", lastConnectionState, connectionState);
+            lastConnectionState = connectionState;
+            return updateBuzzer();
+        }
+        lastConnectionState = connectionState;
+        return DURATION_NEVER;
     }
-    else
+    if(!startComplete)
     {
-        // if we are currently playing a tune then set a flag so that we can do a start after it completes
+        // if we are currently playing the startup tune then set a flag so that we trigger an update after it completes
+        DBGLN(">> call after");
         callAfterComplete = true;
-        return DURATION_IGNORE;
     }
+    return DURATION_IGNORE;
 }
 
 static int timeout()
 {
-    int duration = updateBuzzer(false);
-    if (duration == DURATION_NEVER && callAfterComplete)
+    int duration = playTune();
+    DBGLN(">> timeout %d", duration);
+    if (duration == DURATION_NEVER && !startComplete)
     {
-        // tune completed and the flag is set to start the next one
-        callAfterComplete = false;
-        duration = updateBuzzer(true);
+        startComplete = true;
+        if (callAfterComplete)
+        {
+            // tune completed and the flag is set to start the next one
+            callAfterComplete = false;
+            DBGLN(">> call after update");
+            duration = updateBuzzer();
+        }
     }
     return duration;
 }
 
 device_t Buzzer_device = {
     .initialize = initializeBuzzer,
-    .start = NULL,
+    .start = start,
     .event = event,
     .timeout = timeout
 };
