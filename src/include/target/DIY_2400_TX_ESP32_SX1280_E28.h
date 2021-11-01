a7a9dda4e56d6bbbadfa7eb5fcc16a418d951fc6
Brandon Geraci <brandon.geraci@gmail.com>
2021-11-01 18:11:12 -0500

    It is really fixed this time.. really really

--- src/include/target/DIY_2400_TX_ESP32_SX1280_E28.h
+++ src/include/target/DIY_2400_TX_ESP32_SX1280_E28.h
@@ -12,8 +12,11 @@
 #define GPIO_PIN_RST            14
 #define GPIO_PIN_RX_ENABLE      27
 #define GPIO_PIN_TX_ENABLE      26
-#define GPIO_PIN_OLED_SDA       -1
-#define GPIO_PIN_OLED_SCK       -1
+#define GPIO_PIN_OLED_CS 15
+#define GPIO_PIN_OLED_RST 16
+#define GPIO_PIN_OLED_DC 17
+#define GPIO_PIN_OLED_MOSI 32
+#define GPIO_PIN_OLED_SCK 33
 #define GPIO_PIN_RCSIGNAL_RX    13
 #define GPIO_PIN_RCSIGNAL_TX    13
 #define GPIO_PIN_LED_WS2812     15
