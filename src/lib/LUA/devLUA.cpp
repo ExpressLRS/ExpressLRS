2cc6fd452badb59c09a4c0ced03ad4235152e65c
Bryan Mayland <bmayland@capnbry.net>
2021-11-03 00:26:01 -0400

    Limit power shown in Lua to min/max power supported by TX (#1026)
    * Limit max power option provided to lua user
    
    * Compact code
    
    * More direct which file documentation
    
    * Remove power levels <MinPower

--- src/lib/LUA/devLUA.cpp
+++ src/lib/LUA/devLUA.cpp
@@ -24,6 +24,7 @@ extern SX1280Driver Radio;
 static const char thisCommit[] = {LATEST_COMMIT, 0};
 static const char thisVersion[] = {LATEST_VERSION, 0};
 static const char emptySpace[1] = {0};
+static char strPowerLevels[] = "10;25;50;100;250;500;1000;2000";
 
 static struct luaItem_selection luaAirRate = {
     {"Packet Rate", CRSF_TEXT_SELECTION},
@@ -51,7 +52,7 @@ static struct luaItem_folder luaPowerFolder = {
 static struct luaItem_selection luaPower = {
     {"Max Power", CRSF_TEXT_SELECTION},
     0, // value
-    "10;25;50;100;250;500;1000;2000",
+    strPowerLevels,
     "mW"
 };
 
@@ -199,6 +200,36 @@ extern unsigned long rebootTime;
 extern void beginWebsever();
 #endif
 
+static void luadevGeneratePowerOpts()
+{
+  // This function modifies the strPowerLevels in place and must not
+  // be called more than once!
+  char *out = strPowerLevels;
+  PowerLevels_e pwr = PWR_10mW;
+  // Count the semicolons to move `out` to point to the MINth item
+  while (pwr < MinPower)
+  {
+    while (*out++ != ';') ;
+    pwr = (PowerLevels_e)((unsigned int)pwr + 1);
+  }
+  // There is no min field, compensate by shifting the index when sending/receiving
+  // luaPower.min = (uint8_t)MinPower;
+  luaPower.options = (const char *)out;
+
+  // Continue until after than MAXth item and drop a null in the orginal
+  // string on the semicolon (not after like the previous loop)
+  while (pwr <= MaxPower)
+  {
+    // If out still points to a semicolon from the last loop move past it
+    if (*out)
+      ++out;
+    while (*out && *out != ';')
+      ++out;
+    pwr = (PowerLevels_e)((unsigned int)pwr + 1);
+  }
+  *out = '\0';
+}
+
 static void registerLuaParameters()
 {
   registerLUAParameter(&luaAirRate, [](uint8_t id, uint8_t arg){
@@ -249,17 +280,9 @@ static void registerLuaParameters()
 
   // POWER folder
   registerLUAParameter(&luaPowerFolder);
+  luadevGeneratePowerOpts();
   registerLUAParameter(&luaPower, [](uint8_t id, uint8_t arg){
-    PowerLevels_e newPower = (PowerLevels_e)arg;
-
-    if (newPower > MaxPower)
-    {
-        newPower = MaxPower;
-    } else if (newPower < MinPower)
-    {
-        newPower = MinPower;
-    }
-    config.SetPower(newPower);
+    config.SetPower((PowerLevels_e)constrain(arg + MinPower, MinPower, MaxPower));
   }, luaPowerFolder.common.id);
   registerLUAParameter(&luaDynamicPower, [](uint8_t id, uint8_t arg){
       config.SetDynamicPower(arg > 0);
@@ -382,8 +405,7 @@ static int event()
   setLuaTextSelectionValue(&luaTlmRate, config.GetTlm());
   setLuaTextSelectionValue(&luaSwitch,(uint8_t)(config.GetSwitchMode() - 1)); // -1 for missing sm1Bit
   setLuaTextSelectionValue(&luaModelMatch,(uint8_t)config.GetModelMatch());
-
-  setLuaTextSelectionValue(&luaPower, config.GetPower());
+  setLuaTextSelectionValue(&luaPower, config.GetPower() - MinPower);
 
   uint8_t dynamic = config.GetDynamicPower() ? config.GetBoostChannel() + 1 : 0;
   setLuaTextSelectionValue(&luaDynamicPower,dynamic);
--- src/lua/README.md
+++ src/lua/README.md
@@ -1,20 +1,12 @@
 
 # Which file do I need?
-
-## For TX firmware 1.0.x or earlier:
-NOTE: If you are on V1.0.x please use ELRS.lua not the elrsV2.lua inside the subfolders.
+* For transmitter on master / ExpressLRS 2.0 -> elrsV2.lua
+* For transmitter on 1.x -> ELRS.lua
 
 ### Downloading from Github:
-Copy the ELRS.lua file into the /SCRIPTS/TOOLS directory of the SD card on your handset.
+Click the file link above, find the "Raw" button near the top of that page. Right-click, Save link as.., copy the .lua file into the /SCRIPTS/TOOLS directory of the SD card on your handset.
 
 ### Downloading from Configurator:
-Use the button in the image below to download the .lua script and Copy the ELRS.lua file into the /SCRIPTS/TOOLS directory of the SD card on your handset.
+Use the button shown in the image below to download the .lua script into the /SCRIPTS/TOOLS directory of the SD card on your handset.
 ![downloadlua](https://user-images.githubusercontent.com/68074253/129203116-c1234719-3e8c-4cbf-a391-b7fb8dc0262d.png)
-
-
-## For TX firmware 2.x.x or later (e.g. if you are running the master branch):
-NOTE: The scripts in these subfolders are incompatible with ExpressLRS V1.0.x and use the new ExpressLRS Configurator. They will not work on V1.0.x
-
-Use the telemetry script (elrsV2.lua), Copy the ELRS.lua file into the /SCRIPTS/TOOLS directory of the SD card on your handset.
-
 
