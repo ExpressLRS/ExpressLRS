0a8b0291b876896a6e62b797356d952376c40e96
Brandon Geraci <brandon.geraci@gmail.com>
2021-11-02 23:20:26 -0500

    made complete new targets for micro TX variants

--- src/include/target/BETAFPV_2400_TX_MICRO.h
+++ src/include/target/BETAFPV_2400_TX_MICRO.h
@@ -1,24 +1,35 @@
-// This target extends the BETAFPV_2400_TX target
-
-#undef TX_DEVICE_NAME
 #define TX_DEVICE_NAME              "BETAFPV 2400TX Micro"
-
-// There is some special handling for this target
-#undef TARGET_TX_BETAFPV_2400_V1
-#define TARGET_TX_BETAFPV_2400_MICRO_V1
 
 // Any device features
 #define HAS_OLED
 #define HAS_OLED_I2C
 #define OLED_REVERSED
 
+// There is some special handling for this target
+#define TARGET_TX_BETAFPV_2400_MICRO_V1
+
 // GPIO pin definitions
-#undef GPIO_PIN_LED_BLUE      
-#undef GPIO_PIN_LED_GREEN     
+#define GPIO_PIN_NSS            5
+#define GPIO_PIN_BUSY           21
+#define GPIO_PIN_DIO0           -1
+#define GPIO_PIN_DIO1           4
+#define GPIO_PIN_MOSI           23
+#define GPIO_PIN_MISO           19
+#define GPIO_PIN_SCK            18
+#define GPIO_PIN_RST            14
+#define GPIO_PIN_RX_ENABLE      27
+#define GPIO_PIN_TX_ENABLE      26
+#define GPIO_PIN_RCSIGNAL_RX    13
+#define GPIO_PIN_RCSIGNAL_TX    13
 #define GPIO_PIN_FAN            17
 #define GPIO_PIN_LED_WS2812     16
 #define GPIO_PIN_OLED_RST       -1
 #define GPIO_PIN_OLED_SCK       32
 #define GPIO_PIN_OLED_SDA       22
+#define GPIO_PIN_BUTTON         25
 
+// Output Power
+#define MinPower PWR_10mW
+#define MaxPower PWR_500mW
+#define POWER_OUTPUT_VALUES {-18,-15,-13,-9,-4,3}
 
--- src/include/target/BETAFPV_900_TX_MICRO.h
+++ src/include/target/BETAFPV_900_TX_MICRO.h
@@ -1,25 +1,33 @@
-// This target extends the BETAFPV_900_TX target
-
-#undef TX_DEVICE_NAME
 #define TX_DEVICE_NAME              "BETAFPV 900TX Micro"
 
 // There is some special handling for this target
-#undef TARGET_TX_BETAFPV_900_V1
 #define TARGET_TX_BETAFPV_900_MICRO_V1
 
 // Any device features
 #define HAS_OLED
 #define HAS_OLED_I2C
 #define OLED_REVERSED
-
+  
 // GPIO pin definitions
-#undef GPIO_PIN_LED            
-#undef GPIO_PIN_LED_BLUE       
-#undef GPIO_PIN_LED_GREEN      
+#define GPIO_PIN_NSS            5
+#define GPIO_PIN_DIO0           4
+#define GPIO_PIN_DIO1           2
+#define GPIO_PIN_MOSI           23
+#define GPIO_PIN_MISO           19
+#define GPIO_PIN_SCK            18
+#define GPIO_PIN_RST            14
+#define GPIO_PIN_RX_ENABLE      27
+#define GPIO_PIN_TX_ENABLE      26
+#define GPIO_PIN_RCSIGNAL_RX    13
+#define GPIO_PIN_RCSIGNAL_TX    13 // so we don't have to solder the extra resistor, we switch rx/tx using gpio mux
 #define GPIO_PIN_FAN            17
-#define GPIO_PIN_LED            16
+#define GPIO_PIN_LED_WS2812     16
 #define GPIO_PIN_OLED_RST       -1
 #define GPIO_PIN_OLED_SCK       32
 #define GPIO_PIN_OLED_SDA       22
+#define GPIO_PIN_BUTTON         25
 
-
+// Output Power
+#define MinPower                PWR_100mW
+#define MaxPower                PWR_500mW
+#define POWER_OUTPUT_VALUES     {0,3,8}
