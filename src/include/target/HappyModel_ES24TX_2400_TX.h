7bd95469f27745938ed3846c6f62f17e2073a0de
Jye <JyeSmith@users.noreply.github.com>
2021-11-07 12:04:34 +1000

    separate commercial targets from DIY_2400_TX_ESP32_SX1280_E28

--- src/include/target/HappyModel_ES24TX_2400_TX.h
+++ src/include/target/HappyModel_ES24TX_2400_TX.h
@@ -3,9 +3,24 @@
 #define USE_TX_BACKPACK
 #define USE_SX1280_DCDC
 
+// GPIO pin definitions
+#define GPIO_PIN_NSS            5
+#define GPIO_PIN_BUSY           21
+#define GPIO_PIN_DIO1           4
+#define GPIO_PIN_MOSI           23
+#define GPIO_PIN_MISO           19
+#define GPIO_PIN_SCK            18
+#define GPIO_PIN_RST            14
+#define GPIO_PIN_RX_ENABLE      27
+#define GPIO_PIN_TX_ENABLE      26
+#define GPIO_PIN_RCSIGNAL_RX    13
+#define GPIO_PIN_RCSIGNAL_TX    13
+#define GPIO_PIN_LED_WS2812     15
+#define GPIO_PIN_FAN_EN         17
+
 // Output Power
 #define MinPower PWR_10mW
 #define MaxPower PWR_250mW
 #define POWER_OUTPUT_VALUES {-17,-13,-9,-6,-2}
 
-#include "target/DIY_2400_TX_ESP32_SX1280_E28.h"
+#define Regulatory_Domain_ISM_2400 1
