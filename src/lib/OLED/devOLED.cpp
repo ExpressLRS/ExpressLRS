2eb416a8bca43f7c2c08af143e1d01622ce4d596
Brandon Geraci <brandon.geraci@gmail.com>
2021-11-09 18:44:13 -0600

    Fixing the OLED update screen values

--- src/lib/OLED/devOLED.cpp
+++ src/lib/OLED/devOLED.cpp
@@ -1,6 +1,7 @@
 #include "targets.h"
 #include "common.h"
 #include "device.h"
+#include "config.h"
 
 #if defined(USE_OLED_SPI) || defined(USE_OLED_SPI_SMALL) || defined(USE_OLED_I2C)
 
@@ -8,6 +9,8 @@
 
 #include "OLED.h"
 class OLED OLED;
+
+extern TxConfig config;
 
 static const char thisCommit[] = {LATEST_COMMIT, 0};
 static bool startup = false;
@@ -25,9 +28,9 @@ static int event()
     {
         return DURATION_IGNORE; // we are still displaying the startup message, so don't change the timeout
     }
-    OLED.updateScreen(OLED.getPowerString((PowerLevels_e)POWERMGNT::currPower()),
-                        OLED.getRateString((expresslrs_RFrates_e)ExpressLRS_currAirRate_Modparams->enum_rate),
-                        OLED.getTLMRatioString((expresslrs_tlm_ratio_e)(ExpressLRS_currAirRate_Modparams->TLMinterval)), thisCommit);
+    OLED.updateScreen(OLED.getPowerString((PowerLevels_e)(config.GetPower())),
+                        OLED.getRateString((expresslrs_RFrates_e)(config.GetRate())),
+                        OLED.getTLMRatioString((expresslrs_tlm_ratio_e)(config.GetTlm())), thisCommit);
     return DURATION_NEVER;
 }
 
