89a218a5fdb5582fc0161bfe815e10ed66638952
Jye <JyeSmith@users.noreply.github.com>
2021-11-22 17:46:47 +1000

    iFlight 900

---
+++ src/include/target/iFlight_900_TX.h
@@ -0,0 +1,23 @@
+#define DEVICE_NAME "iFlight 900TX"
+
+// GPIO pin definitions
+#define GPIO_PIN_NSS            5
+#define GPIO_PIN_DIO0           17
+#define GPIO_PIN_DIO1           16
+#define GPIO_PIN_MOSI           23
+#define GPIO_PIN_MISO           19
+#define GPIO_PIN_SCK            18
+#define GPIO_PIN_RST            22
+#define GPIO_PIN_RX_ENABLE      21
+#define GPIO_PIN_TX_ENABLE      4
+#define GPIO_PIN_RCSIGNAL_RX    13
+#define GPIO_PIN_RCSIGNAL_TX    13 // so we don't have to solder the extra resistor, we switch rx/tx using gpio mux
+#define GPIO_PIN_LED_RED        14
+#define GPIO_PIN_LED_GREEN      27
+#define GPIO_PIN_LED_BLUE       26
+#define GPIO_PIN_BUTTON         15
+
+// Output Power
+#define MinPower                PWR_10mW
+#define MaxPower                PWR_500mW
+#define POWER_OUTPUT_VALUES     {0,3,8,9,10,11} // only test number.
