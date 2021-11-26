8aa208f7b7913cf7f40f70f559a85285c5859d77
Jye <JyeSmith@users.noreply.github.com>
2021-11-26 18:07:10 +1000

    adds led settings for iFlight low power output levels
    And a clean up

--- src/include/target/iFlight_2400_TX.h
+++ src/include/target/iFlight_2400_TX.h
@@ -1,6 +1,7 @@
 #define DEVICE_NAME "iFlight 2400TX"
 
 // Any device features
+#define TARGET_TX_IFLIGHT
 #define USE_SX1280_DCDC
 
 // GPIO pin definitions
@@ -21,8 +22,8 @@
 #define GPIO_PIN_BUTTON         15
 
 // Output Power
-#define MinPower PWR_10mW
-#define MaxPower PWR_500mW
-#define POWER_OUTPUT_VALUES {-18,-15,-11,-8,-4,3}
+#define MinPower                PWR_10mW
+#define MaxPower                PWR_500mW
+#define POWER_OUTPUT_VALUES     {-18,-15,-11,-8,-4,3}
 
 #define Regulatory_Domain_ISM_2400 1
