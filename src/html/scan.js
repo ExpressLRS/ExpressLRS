e10a2691786df48cb2c83e9522f168cc6ce10716
Sunjun Kim <SunjunKim@users.noreply.github.com>
2021-12-03 15:00:05 +0900

    bugfix

--- src/html/scan.js
+++ src/html/scan.js
@@ -272,12 +272,12 @@ function completeHandler(event) {
         var percent = 0;
         var interval = setInterval(()=>{
             percent = percent + 2;
-            _("progressBar_" + type_suffix).value = percent;
-            _("status_" + type_suffix).innerHTML = percent + "% flashed... please wait";
+            _("progressBar_").value = percent;
+            _("status_").innerHTML = percent + "% flashed... please wait";
             if (percent == 100) {
                 clearInterval(interval);
-                _("status_" + type_suffix).innerHTML = "";
-                _("progressBar_" + type_suffix).value = 0;
+                _("status_").innerHTML = "";
+                _("progressBar_").value = 0;
                 show_message();
             }
         }, 100);
