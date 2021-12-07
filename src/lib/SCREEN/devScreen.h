06f4a18456b927bd693757f92e829c6db0ccd8fd
Paul Kendall <pkendall64@gmail.com>
2021-12-08 09:16:11 +1300

    No need to include the implementation in the device header!

--- src/lib/SCREEN/devScreen.h
+++ src/lib/SCREEN/devScreen.h
@@ -1,9 +1,5 @@
 #pragma once
 
 #include "device.h"
-#if defined(USE_OLED_SPI) || defined(USE_OLED_SPI_SMALL) || defined(USE_OLED_I2C) // This code will not be used if the hardware does not have a OLED display. Maybe a better way to blacklist it in platformio.ini?
-#include "oledscreen.h"
-#endif
 
 extern device_t Screen_device;
-
