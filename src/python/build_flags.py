d8cdc9312d6f01e7d9d76e6b16009239ec17a71b
Nikhil Parikh <46944981+maybenikhil@users.noreply.github.com>
2021-12-22 11:22:52 -0800

    Adds Region to Web & Lua (#1200)
    * Adds Frequency to Lua
    
    Can be changed to more specific, but this will work with all screens
    
    * Fix Failing Checks
    
    * Works on my side
    
    * Change to have more clear naming conventions
    
    * Adds Region to Website
    
    minor refactor, pushes git_sha and git_version to environment
    
    * Remove extra space from reg domain
    
    Co-authored-by: Bryan Mayland <bmayland@capnbry.net>

--- src/python/build_flags.py
+++ src/python/build_flags.py
@@ -84,18 +84,40 @@ def condense_flags():
     build_flags = [x for x in build_flags if (x.strip() != "")] # remove any blank items
     build_flags = [escapeChars(x) for x in build_flags] # perform escaping of flags with values
 
-def get_git_sha():
-    sha = elrs_helpers.get_git_version(env)['sha']
-    return ",".join(["%s" % ord(x) for x in sha])
+def version_to_env():
+    ver = elrs_helpers.get_git_version(env)
+    env.Append(GIT_SHA = ver['sha'], GIT_VERSION= ver['version'])
 
-def get_git_version():
-    ver = elrs_helpers.get_git_version(env)['version']
-    return ",".join(["%s" % ord(char) for char in ver])
+def regulatory_domain_to_env():
+    regions = [("AU_915", "AU915"), ("EU_868", "EU868"), ("IN_866", "IN866"), ("AU_433", "AU433"), ("EU_433", "EU433"), ("FCC_915","FCC915"), ("ISM_2400", "ISM2G4"), ("EU_CE_2400", "CE2G4")]
+    retVal = "UNK"
+    if ("_2400" in target_name or \
+        '-DRADIO_2400=1' in build_flags) and \
+        '-DRegulatory_Domain_EU_CE_2400' not in build_flags:
+        retVal = "ISM2G4"
+    else:
+        for k, v in regions:
+            if fnmatch.filter(build_flags, '*-DRegulatory_Domain_'+k):
+                retVal = v
+                break
+    env.Append(REG_DOMAIN = retVal)
+
+def string_to_ascii(str):
+    return ",".join(["%s" % ord(char) for char in str])
+
+def get_git_sha():
+    return string_to_ascii(env.get('GIT_SHA'))
+
+def get_ver_and_reg():
+    return string_to_ascii(env.get('GIT_VERSION') + " " + env.get('REG_DOMAIN'))
+
 
 process_flags("user_defines.txt")
 process_flags("super_defines.txt") # allow secret super_defines to override user_defines
+version_to_env()
+regulatory_domain_to_env()
 build_flags.append("-DLATEST_COMMIT=" + get_git_sha())
-build_flags.append("-DLATEST_VERSION=" + get_git_version())
+build_flags.append("-DLATEST_VERSION=" + get_ver_and_reg()) # version and domain
 build_flags.append("-DTARGET_NAME=" + re.sub("_VIA_.*", "", target_name))
 condense_flags()
 
