7e16ed4bc4e70531703edd5065ce3e07a4871d9c
Sunjun Kim <SunjunKim@users.noreply.github.com>
2021-12-29 12:35:44 +0900

    Updating power change to Screen correctly  (#1222)
    * Update POWERMGNT.h
    
    * power change updated to related devices
    
    Added devicesTriggerEvent(); which will update the related display devices (e.g., OLED, Screen, etc.)

--- src/lib/POWERMGNT/POWERMGNT.h
+++ src/lib/POWERMGNT/POWERMGNT.h
@@ -2,6 +2,7 @@
 
 #include "targets.h"
 #include "DAC.h"
+#include "device.h"
 
 #if defined(Regulatory_Domain_AU_915) || defined(Regulatory_Domain_EU_868) || defined(Regulatory_Domain_IN_866) || defined(Regulatory_Domain_FCC_915) || defined(Regulatory_Domain_AU_433) || defined(Regulatory_Domain_EU_433)
 #include "SX127xDriver.h"
