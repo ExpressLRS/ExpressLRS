19655613fedf32a147e55162c81a3d890c18040f
Bryan Mayland <bmayland@capnbry.net>
2022-01-30 01:07:31 -0500

    HTTP upload result without JSON and uploadforce (#1371)
    * Print any error message returned by http upload
    
    * Allow force of upload during upload
    
    * Add uploadforce and uploadconfirm targets
    
    * Add uploadforce to BF passthrough too
    
    * Only add uploadforce if not native platform
    
    * Return -2 for mismatch
    
    * Never prompt Y/N if not in interactive shell
    
    * Standardize return codes across upload methods
    
    * PEP 8 extra spaces between top level

--- src/python/query_yes_no.py
+++ src/python/query_yes_no.py
@@ -1,11 +1,15 @@
 import sys
 from inputimeout import inputimeout, TimeoutOccurred
 
-def query_yes_no(question=''): #https://code.activestate.com/recipes/577058/
+def query_yes_no(question='') -> bool: #https://code.activestate.com/recipes/577058/
     """Ask a yes/no question via raw_input() and return their answer.
     "question" is a string that is presented to the user.
     The "answer" return value is True for "yes" or False for "no".
     """
+    # Always return false if not in an interactive shell
+    if not sys.stdin.isatty():
+        return False
+
     valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
 
     while True:
@@ -13,8 +17,8 @@ def query_yes_no(question=''): #https://code.activestate.com/recipes/577058/
         try:
             choice = inputimeout(prompt=question, timeout=5)
         except TimeoutOccurred:
-            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n')")
+            sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n')")
             sys.stdout.flush()
-	
+
         if choice in valid:
             return valid[choice]
--- src/python/upload_via_esp8266_backpack.py
+++ src/python/upload_via_esp8266_backpack.py
@@ -1,5 +1,38 @@
 import subprocess, os
 import opentx
+from elrs_helpers import ElrsUploadResult
+
+
+def process_http_result(output_json_file: str) -> int:
+    import json
+    # Print the HTTP result that was saved to the json file
+    # Returns: ElrsUploadResult enum
+    with open(output_json_file) as f:
+        output_json = json.load(f)
+
+    result = output_json['status']
+    msg = output_json['msg']
+
+    if result == 'ok':
+        # Update complete. Please wait for LED to resume blinking before disconnecting power.
+        msg = f'UPLOAD SUCCESS\n\033[32m{msg}\033[0m'  # green
+        # 'ok' is the only acceptable result
+        retval = ElrsUploadResult.Success
+    elif result == 'mismatch':
+        # <b>Current target:</b> LONG_ASS_NAME.<br><b>Uploaded image:</b> OTHER_NAME.<br/><br/>Flashing the wrong firmware may lock or damage your device.
+        msg = msg.replace('<br>', '\n').replace('<br/>', '\n') # convert breaks to newline
+        msg = msg.replace('<b>', '\033[34m').replace('</b>', '\033[0m') # bold to blue
+        msg = '\033[33mTARGET MISMATCH\033[0m\n' + msg # yellow warning
+        retval = ElrsUploadResult.ErrorMismatch
+    else:
+        # Not enough space.
+        msg = f'UPLOAD ERROR\n\033[31m{msg}\033[0m' # red
+        retval = ElrsUploadResult.ErrorGeneral
+
+    print()
+    print(msg, flush=True)
+    return retval
+
 
 def on_upload(source, target, env):
     isstm = env.get('PIOPLATFORM', '') in ['ststm32']
@@ -23,7 +56,7 @@ def on_upload(source, target, env):
             if "BOOTLOADER=" in flag:
                 bootloader_file = flag.split("=")[1]
                 bootloader_target = os.path.join((env.get('PROJECT_DIR')), bootloader_file)
-			
+
 
     firmware_path = str(source[0])
     bin_path = os.path.dirname(firmware_path)
@@ -33,38 +66,57 @@ def on_upload(source, target, env):
         if not os.path.exists(elrs_bin_target):
             raise Exception("No valid binary found!")
 
+    bin_upload_output = os.path.splitext(elrs_bin_target)[0] + '-output.json'
+    if os.path.exists(bin_upload_output):
+        os.remove(bin_upload_output)
+
     cmd = ["curl", "--max-time", "60",
            "--retry", "2", "--retry-delay", "1",
-           "-F", "data=@%s" % (elrs_bin_target,)]
+           "-o", "%s" % (bin_upload_output)]
 
-    if  bootloader_target is not None and isstm:
+    pio_target = target[0].name
+    uri = 'update'
+    do_bin_upload = True
+
+    if pio_target == 'uploadconfirm':
+        uri = 'forceupdate?action=confirm'
+        do_bin_upload = False
+    if pio_target == 'uploadforce':
+        cmd += ["-F", "force=1"]
+    if do_bin_upload:
+        cmd += "-F", "data=@%s" % (elrs_bin_target),
+    if isstm:
+        uri = 'upload'
+        cmd += ["-F", "flash_address=0x%X" % (app_start,)]
+
+    if bootloader_target is not None and isstm:
         cmd_bootloader = ["curl", "--max-time", "60",
             "--retry", "2", "--retry-delay", "1",
             "-F", "data=@%s" % (bootloader_target,), "-F", "flash_address=0x0000"]
-		   
-    if isstm:
-        cmd += ["-F", "flash_address=0x%X" % (app_start,)]
 
     upload_port = env.get('UPLOAD_PORT', None)
     if upload_port is not None:
         upload_addr = [upload_port]
 
+    returncode = ElrsUploadResult.ErrorGeneral
     for addr in upload_addr:
-        addr = "http://%s/%s" % (addr, ['update', 'upload'][isstm])
+        addr = "http://%s/%s" % (addr, uri)
         print(" ** UPLOADING TO: %s" % addr)
         try:
-            if  bootloader_target is not None:  
+            # Flash bootloader first if set
+            if bootloader_target is not None:
                 print("** Flashing Bootloader...")
                 print(cmd_bootloader,cmd)
                 subprocess.check_call(cmd_bootloader + [addr])
                 print("** Bootloader Flashed!")
                 print()
-            subprocess.check_call(cmd + [addr])
-            print()
-            print("** UPLOAD SUCCESS. Flashing in progress.")
-            print("** Please wait for LED to resume blinking before disconnecting power")
-            return
-        except subprocess.CalledProcessError:
-            print("FAILED!")
 
-    raise Exception("WIFI upload FAILED!")
+            # Flash main application binary
+            subprocess.check_call(cmd + [addr])
+            returncode = process_http_result(bin_upload_output)
+        except subprocess.CalledProcessError as e:
+            returncode = e.returncode
+
+    if returncode != ElrsUploadResult.Success:
+        print("WIFI upload FAILED!")
+    return returncode
