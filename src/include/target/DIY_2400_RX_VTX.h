cc6a4583ae4432aa6112e701f8888ea76b396305
Jye <JyeSmith@users.noreply.github.com>
2022-02-22 18:48:57 +1000

    finalise target pinout

--- src/include/target/DIY_2400_RX_VTX.h
+++ src/include/target/DIY_2400_RX_VTX.h
@@ -9,8 +9,8 @@
 #define GPIO_PIN_MOSI           13
 #define GPIO_PIN_MISO           12
 #define GPIO_PIN_SCK            14
-#define GPIO_PIN_RST            2  // Move to GPIO16
-#define GPIO_PIN_LED_RED        16 // Move to GPRIO2 and GPIO_PIN_LED_WS2812.  Add 2 LEDs, one for the Rx and another for VTx status.
+#define GPIO_PIN_RST            16
+#define GPIO_PIN_LED_WS2812     2  // Add 2 LEDs in series, one for the Rx and a second for VTx status.
 
 // VTx pins and Vpd setpoints
 #define GPIO_PIN_SPI_VTX_NSS    9
