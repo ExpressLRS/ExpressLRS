fa10980cf24352e4f7c41898ec756e075ffe0c3c
Jye <JyeSmith@users.noreply.github.com>
2022-02-25 18:43:29 +1000

    replace GPIO_PIN_BUSY with GPIO_PIN_ANTENNA_SELECT

--- src/include/target/DIY_2400_RX_VTX.h
+++ src/include/target/DIY_2400_RX_VTX.h
@@ -4,12 +4,12 @@
 
 // GPIO pin definitions
 #define GPIO_PIN_NSS            15
-#define GPIO_PIN_BUSY           5
 #define GPIO_PIN_DIO1           4
 #define GPIO_PIN_MOSI           13
 #define GPIO_PIN_MISO           12
 #define GPIO_PIN_SCK            14
 #define GPIO_PIN_RST            16
+#define GPIO_PIN_ANTENNA_SELECT 5
 #define GPIO_PIN_LED_WS2812     2  // Add 2 LEDs in series, one for the Rx and a second for VTx status.
 
 // VTx pins and Vpd setpoints
