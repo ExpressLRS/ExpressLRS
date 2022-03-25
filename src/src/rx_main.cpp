bf5b79fe9494ba2ba0002a56d3b990f614ec9dc7
Bryan Mayland <bmayland@capnbry.net>
2022-03-24 23:28:52 -0400

    Add DEBUG_RCVR_LINKSTATS (#1441)
    * Add DEBUG_RCVR_LINKSTATS
    
    * Simplify packet counter in OTA, add FHSS channel
    
    * Add user_define info for DEBUG_RCVR_LINKSTATS
    
    * Add PFD offset to DEBUG_RCVR_LINKSTATS
    
    * Add timing offset to docs

--- src/src/rx_main.cpp
+++ src/src/rx_main.cpp
@@ -120,11 +120,7 @@ uint8_t uplinkLQ;
 uint8_t scanIndex = RATE_DEFAULT;
 uint8_t ExpressLRS_nextAirRateIndex;
 
-int32_t RawOffset;
-int32_t prevRawOffset;
-int32_t Offset;
-int32_t OffsetDx;
-int32_t prevOffset;
+int32_t PfdPrevRawOffset;
 RXtimerState_e RXtimerState;
 uint32_t GotConnectionMillis = 0;
 const uint32_t ConsiderConnGoodMillis = 1000; // minimum time before we can consider a connection to be 'good'
@@ -135,6 +131,7 @@ volatile uint8_t NonceRX = 0; // nonce that we THINK we are up to.
 
 bool alreadyFHSS = false;
 bool alreadyTLMresp = false;
+bool InBindingMode = false;
 
 //////////////////////////////////////////////////////////////
 
@@ -168,7 +165,10 @@ int8_t debug4 = 0;
 ///////////////////////////////////////
 #endif
 
-bool InBindingMode = false;
+#if defined(DEBUG_RCVR_LINKSTATS)
+static bool debugRcvrLinkstatsPending;
+static uint8_t debugRcvrLinkstatsFhssIdx;
+#endif
 
 void reset_into_bootloader(void);
 void EnterBindingMode();
@@ -199,27 +199,31 @@ static uint8_t minLqForChaos()
 
 void ICACHE_RAM_ATTR getRFlinkInfo()
 {
-    int32_t rssiDBM0 = LPF_UplinkRSSI0.SmoothDataINT;
-    int32_t rssiDBM1 = LPF_UplinkRSSI1.SmoothDataINT;
-    switch (antenna) {
-        case 0:
-            rssiDBM0 = LPF_UplinkRSSI0.update(Radio.LastPacketRSSI);
-            break;
-        case 1:
-            rssiDBM1 = LPF_UplinkRSSI1.update(Radio.LastPacketRSSI);
-            break;
+    int32_t rssiDBM = Radio.LastPacketRSSI;
+    if (antenna == 0)
+    {
+        #if !defined(DEBUG_RCVR_LINKSTATS)
+        rssiDBM = LPF_UplinkRSSI0.update(rssiDBM);
+        #endif
+        if (rssiDBM > 0) rssiDBM = 0;
+        // BetaFlight/iNav expect positive values for -dBm (e.g. -80dBm -> sent as 80)
+        crsf.LinkStatistics.uplink_RSSI_1 = -rssiDBM;
+    }
+    else
+    {
+        #if !defined(DEBUG_RCVR_LINKSTATS)
+        rssiDBM = LPF_UplinkRSSI1.update(rssiDBM);
+        #endif
+        if (rssiDBM > 0) rssiDBM = 0;
+        // BetaFlight/iNav expect positive values for -dBm (e.g. -80dBm -> sent as 80)
+        // May be overwritten below if DEBUG_BF_LINK_STATS is set
+        crsf.LinkStatistics.uplink_RSSI_2 = -rssiDBM;
     }
 
-    int32_t rssiDBM = (antenna == 0) ? rssiDBM0 : rssiDBM1;
     crsf.PackedRCdataOut.ch15 = UINT10_to_CRSF(map(constrain(rssiDBM, ExpressLRS_currAirRate_RFperfParams->RXsensitivity, -50),
                                                ExpressLRS_currAirRate_RFperfParams->RXsensitivity, -50, 0, 1023));
     crsf.PackedRCdataOut.ch14 = UINT10_to_CRSF(fmap(uplinkLQ, 0, 100, 0, 1023));
 
-    if (rssiDBM0 > 0) rssiDBM0 = 0;
-    if (rssiDBM1 > 0) rssiDBM1 = 0;
-
-    // BetaFlight/iNav expect positive values for -dBm (e.g. -80dBm -> sent as 80)
-    crsf.LinkStatistics.uplink_RSSI_1 = -rssiDBM0;
     crsf.LinkStatistics.active_antenna = antenna;
     crsf.LinkStatistics.uplink_SNR = Radio.LastPacketSNR;
     //crsf.LinkStatistics.uplink_Link_quality = uplinkLQ; // handled in Tick
@@ -230,11 +234,10 @@ void ICACHE_RAM_ATTR getRFlinkInfo()
     crsf.LinkStatistics.downlink_Link_quality = debug2;
     crsf.LinkStatistics.downlink_SNR = debug3;
     crsf.LinkStatistics.uplink_RSSI_2 = debug4;
-    #else
-    crsf.LinkStatistics.downlink_RSSI = 0;
-    crsf.LinkStatistics.downlink_Link_quality = 0;
-    crsf.LinkStatistics.downlink_SNR = 0;
-    crsf.LinkStatistics.uplink_RSSI_2 = -rssiDBM1;
+    #endif
+
+    #if defined(DEBUG_RCVR_LINKSTATS)
+    debugRcvrLinkstatsFhssIdx = FHSSsequence[FHSSptr];
     #endif
 }
 
@@ -388,9 +391,11 @@ void ICACHE_RAM_ATTR updatePhaseLock()
     {
         PFDloop.calcResult();
         PFDloop.reset();
-        RawOffset = PFDloop.getResult();
-        Offset = LPF_Offset.update(RawOffset);
-        OffsetDx = LPF_OffsetDx.update(RawOffset - prevRawOffset);
+
+        int32_t RawOffset = PFDloop.getResult();
+        int32_t Offset = LPF_Offset.update(RawOffset);
+        int32_t OffsetDx = LPF_OffsetDx.update(RawOffset - PfdPrevRawOffset);
+        PfdPrevRawOffset = RawOffset;
 
         if (RXtimerState == tim_locked && LQCalc.currentIsSet())
         {
@@ -416,11 +421,9 @@ void ICACHE_RAM_ATTR updatePhaseLock()
             hwTimer.phaseShift(Offset >> 2);
         }
 
-        prevOffset = Offset;
-        prevRawOffset = RawOffset;
+        DBGVLN("%d:%d:%d:%d:%d", Offset, RawOffset, OffsetDx, hwTimer.FreqOffset, uplinkLQ);
+        UNUSED(OffsetDx); // complier warning if no debug
     }
-
-    DBGVLN("%d:%d:%d:%d:%d", Offset, RawOffset, OffsetDx, hwTimer.FreqOffset, uplinkLQ);
 }
 
 void ICACHE_RAM_ATTR HWtimerCallbackTick() // this is 180 out of phase with the other callback, occurs mid-packet reception
@@ -471,8 +474,8 @@ static void ICACHE_RAM_ATTR updateDiversity()
         static int32_t prevRSSI;        // saved rssi so that we can compare if switching made things better or worse
         static int32_t antennaLQDropTrigger;
         static int32_t antennaRSSIDropTrigger;
-        int32_t rssi = (antenna == 0) ? LPF_UplinkRSSI0.SmoothDataINT : LPF_UplinkRSSI1.SmoothDataINT;
-        int32_t otherRSSI = (antenna == 0) ? LPF_UplinkRSSI1.SmoothDataINT : LPF_UplinkRSSI0.SmoothDataINT;
+        int32_t rssi = (antenna == 0) ? LPF_UplinkRSSI0.value() : LPF_UplinkRSSI1.value();
+        int32_t otherRSSI = (antenna == 0) ? LPF_UplinkRSSI1.value() : LPF_UplinkRSSI0.value();
 
         //if rssi dropped by the amount of DIVERSITY_ANTENNA_RSSI_TRIGGER
         if ((rssi < (prevRSSI - DIVERSITY_ANTENNA_RSSI_TRIGGER)) && antennaRSSIDropTrigger >= DIVERSITY_ANTENNA_INTERVAL)
@@ -570,10 +573,7 @@ void LostConnection()
     #if defined(RADIO_SX127X)
     Radio.SetPPMoffsetReg(0);
     #endif
-    Offset = 0;
-    OffsetDx = 0;
-    RawOffset = 0;
-    prevOffset = 0;
+    PfdPrevRawOffset = 0;
     GotConnectionMillis = 0;
     uplinkLQ = 0;
     LQCalc.reset();
@@ -599,8 +599,7 @@ void ICACHE_RAM_ATTR TentativeConnection(unsigned long now)
     RXtimerState = tim_disconnected;
     DBGLN("tentative conn");
     FreqCorrection = 0;
-    Offset = 0;
-    prevOffset = 0;
+    PfdPrevRawOffset = 0;
     LPF_Offset.init(0);
     RFmodeLastCycled = now; // give another 3 sec for lock to occur
 
@@ -647,6 +646,9 @@ static void ICACHE_RAM_ATTR ProcessRfPacket_RC()
         newChannelsAvailable = true;
         #else
         crsf.sendRCFrameToFC();
+        #endif
+        #if defined(DEBUG_RCVR_LINKSTATS)
+        debugRcvrLinkstatsPending = true;
         #endif
     }
 }
@@ -1195,6 +1197,34 @@ static void checkSendLinkStatsToFc(uint32_t now)
     }
 }
 
+static void debugRcvrLinkstats()
+{
+#if defined(DEBUG_RCVR_LINKSTATS)
+    if (debugRcvrLinkstatsPending)
+    {
+        debugRcvrLinkstatsPending = false;
+
+        // Copy the data out of the ISR-updating bits ASAP
+        // While YOLOing (const void *) away the volatile
+        crsfLinkStatistics_t ls = *(crsfLinkStatistics_t *)((const void *)&crsf.LinkStatistics);
+        uint32_t packetCounter = debugRcvrLinkstatsPacketId;
+        uint8_t fhss = debugRcvrLinkstatsFhssIdx;
+        // actually the previous packet's offset since the update happens in tick, and this will
+        // fire right after packet reception (a little before tock)
+        int32_t pfd = PfdPrevRawOffset;
+
+        // Use serial instead of DBG() because do not necessarily want all the debug in our logs
+        char buf[50];
+        snprintf(buf, sizeof(buf), "%u,%u,-%u,%u,%d,%u,%u,%d\r\n",
+            packetCounter, ls.active_antenna,
+            ls.active_antenna ? ls.uplink_RSSI_2 : ls.uplink_RSSI_1,
+            ls.uplink_Link_quality, ls.uplink_SNR,
+            ls.uplink_TX_Power, fhss, pfd);
+        Serial.write(buf);
+    }
+#endif
+}
+
 #if defined(PLATFORM_ESP8266)
 // Called from core's user_rf_pre_init() function (which is called by SDK) before setup()
 RF_PRE_INIT()
@@ -1307,14 +1337,14 @@ void loop()
         LostConnection();
     }
 
-    if ((connectionState == tentative) && (abs(OffsetDx) <= 10) && (Offset < 100) && (LQCalc.getLQRaw() > minLqForChaos())) //detects when we are connected
+    if ((connectionState == tentative) && (abs(LPF_OffsetDx.value()) <= 10) && (LPF_Offset.value() < 100) && (LQCalc.getLQRaw() > minLqForChaos())) //detects when we are connected
     {
         GotConnection(now);
     }
 
     checkSendLinkStatsToFc(now);
 
-    if ((RXtimerState == tim_tentative) && ((now - GotConnectionMillis) > ConsiderConnGoodMillis) && (abs(OffsetDx) <= 5))
+    if ((RXtimerState == tim_tentative) && ((now - GotConnectionMillis) > ConsiderConnGoodMillis) && (abs(LPF_OffsetDx.value()) <= 5))
     {
         RXtimerState = tim_locked;
         DBGLN("Timer locked");
@@ -1328,6 +1358,7 @@ void loop()
     }
     updateTelemetryBurst();
     updateBindingMode();
+    debugRcvrLinkstats();
 }
 
 struct bootloader {
