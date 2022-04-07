bc51328352d2e035c9c6cd793dd488e92983d9af
Risto <rotorman@users.noreply.github.com>
2022-04-07 10:10:09 +0200

    Added rudimentary touch support via swiping and tap. Added an exit menu to be able to exit the script via touch.

--- src/lua/elrsV2.lua
+++ src/lua/elrsV2.lua
@@ -34,6 +34,7 @@ local elrsFlags = 0
 local elrsFlagsInfo = ""
 local fields_count = 0
 local backButtonId = 2
+local exitButtonId = 3
 local devicesRefreshTimeout = 50
 local allParamsLoaded = 0
 local folderAccess = nil
@@ -45,6 +46,7 @@ local linkstatTimeout = 100
 local titleShowWarn = nil
 local titleShowWarnTimeout = 100
 local reloadFolder = nil
+local exitscript = 0
 
 local COL2
 local maxLineIndex
@@ -63,6 +65,9 @@ local function allocateFields()
   if folderAccess ~= nil then
     fields[backButtonId].parent = folderAccess
   end
+  fields_count = fields_count + 1
+  exitButtonId = backButtonId + 1
+  fields[exitButtonId] = {id = exitButtonId, name="----EXIT----", parent = nil, type=17}
 end
 
 local function reloadAllField()
@@ -405,13 +410,15 @@ local function fieldCommandLoad(field, data, offset)
 end
 
 local function fieldCommandSave(field)
-  if field.status < 4 then
-    field.status = 1
-    crossfireTelemetryPush(0x2D, { deviceId, handsetId, field.id, field.status })
-    fieldPopup = field
-    fieldPopup.lastStatus = 0
-    commandRunningIndicator = 1
-    fieldTimeout = getTime() + field.timeout
+  if field.status ~= nil then
+    if field.status < 4 then
+      field.status = 1
+      crossfireTelemetryPush(0x2D, { deviceId, handsetId, field.id, field.status })
+      fieldPopup = field
+      fieldPopup.lastStatus = 0
+      commandRunningIndicator = 1
+      fieldTimeout = getTime() + field.timeout
+    end
   end
 end
 
@@ -428,6 +435,10 @@ local function UIbackExec()
   backFld.li = nil
   backFld.po = nil
   folderAccess = nil
+end
+
+local function UIexitExec()
+  exitscript = 1
 end
 
 local function changeDeviceId(devId) --change to selected device ID
@@ -508,6 +519,7 @@ local functions = {
   { load=nil, save=UIbackExec, display=fieldCommandDisplay }, --15 back(14)
   { load=nil, save=fieldDeviceIdSelect, display=fieldCommandDisplay }, --16 device(15)
   { load=nil, save=fieldFolderDeviceOpen, display=fieldFolderDisplay }, --17 deviceFOLDER(16)
+  { load=nil, save=UIexitExec, display=fieldCommandDisplay }, --18 back(17)
 }
 
 local function parseParameterInfoMessage(data)
@@ -737,7 +749,7 @@ local function lcd_warn()
   lcd.drawText(LCD_W/2, textSize*5, "[OK]", BLINK + INVERS + CENTER)
 end
 
-local function handleDevicePageEvent(event)
+local function handleDevicePageEvent(event, touchState)
   if #fields == 0 then --if there is no field yet
     return
   else
@@ -765,7 +777,7 @@ local function handleDevicePageEvent(event)
       end
       UIbackExec()
     end
-  elseif event == EVT_VIRTUAL_ENTER then        -- toggle editing/selecting current field
+  elseif ((event == EVT_VIRTUAL_ENTER) or (event ~= nil and touchState and event == EVT_TOUCH_TAP)) then -- toggle editing/selecting current field
     if elrsFlags > 0x1F then
       elrsFlags = 0
       crossfireTelemetryPush(0x2D, { deviceId, handsetId, 0x2E, 0x00 })
@@ -801,23 +813,77 @@ local function handleDevicePageEvent(event)
       end
     end
   elseif edit then
-    if event == EVT_VIRTUAL_NEXT then
-      incrField(1)
-    elseif event == EVT_VIRTUAL_PREV then
-      incrField(-1)
+    if event ~= nil then
+      if event ~= 0 then
+	if touchState then -- Only touch events come with a touchState
+          if event == EVT_TOUCH_FIRST then
+            -- When the finger first hits the screen
+          elseif event == EVT_TOUCH_BREAK then
+            -- When the finger leaves the screen and did not slide on it
+          elseif event == EVT_TOUCH_TAP then
+            -- A short tap gives TAP instead of BREAK
+            -- touchState.tapCount shows number of taps
+          elseif event == EVT_TOUCH_SLIDE then
+            -- Sliding the finger gives a SLIDE instead of BREAK or TAP
+            if touchState.swipeRight then
+              -- Is true if user swiped right
+              incrField(1)
+            elseif touchState.swipeLeft then
+              incrField(-1)
+            elseif touchState.swipeUp then
+            elseif touchState.swipeDown then
+            else
+              -- Sliding but not swiping
+            end
+		  end
+	    else -- event ~= 0 and touchState == nil: key event
+          if event == EVT_VIRTUAL_NEXT then
+            incrField(1)
+          elseif event == EVT_VIRTUAL_PREV then
+            incrField(-1)
+          end
+        end
+      end
     end
   else
-    if event == EVT_VIRTUAL_NEXT then
-      selectField(1)
-    elseif event == EVT_VIRTUAL_PREV then
-      selectField(-1)
-    end
+    if event ~= nil then
+      if event ~= 0 then
+	    if touchState then -- Only touch events come with a touchState
+          if event == EVT_TOUCH_FIRST then
+            -- When the finger first hits the screen
+          elseif event == EVT_TOUCH_BREAK then
+            -- When the finger leaves the screen and did not slide on it
+          elseif event == EVT_TOUCH_TAP then
+            -- A short tap gives TAP instead of BREAK
+            -- touchState.tapCount shows number of taps
+          elseif event == EVT_TOUCH_SLIDE then
+            -- Sliding the finger gives a SLIDE instead of BREAK or TAP
+            if touchState.swipeRight then
+              -- Is true if user swiped right
+            elseif touchState.swipeLeft then
+            elseif touchState.swipeUp then
+              selectField(-1)
+            elseif touchState.swipeDown then
+              selectField(1)
+            else
+              -- Sliding but not swiping
+            end
+		      end
+	    else -- event ~= 0 and touchState == nil: key event
+          if (event == EVT_VIRTUAL_NEXT) then
+            selectField(1)
+          elseif (event == EVT_VIRTUAL_PREV) then
+            selectField(-1)
+          end
+		    end
+      end
+	end
   end
 end
 
 -- Main
-local function runDevicePage(event)
-  handleDevicePageEvent(event)
+local function runDevicePage(event, touchState)
+  handleDevicePageEvent(event, touchState)
 
   lcd_title()
 
@@ -851,7 +917,7 @@ local function popupCompat(t, m, e)
   return popupConfirmation(t, e)
 end
 
-local function runPopupPage(event)
+local function runPopupPage(event, touchState)
   if event == EVT_VIRTUAL_EXIT then             -- exit script
     crossfireTelemetryPush(0x2D, { deviceId, handsetId, fieldPopup.id, 5 })
     fieldTimeout = getTime() + 200 -- 2s
@@ -956,6 +1022,10 @@ local function setMock()
   deviceIsELRS_TX = true
   allParamsLoaded = 1
   backButtonId = #fields
+  
+  fields_count = fields_count + 1
+  exitButtonId = fields_count + 1
+  fields[exitButtonId] = {id = exitButtonId, name="----EXIT----", parent = nil, type=17}
 end
 
 -- Init
@@ -967,19 +1037,23 @@ local function init()
 end
 
 -- Main
-local function run(event)
+local function run(event, touchState)
   if event == nil then
     error("Cannot be run as a model script!")
     return 2
   end
 
   if fieldPopup ~= nil then
-    runPopupPage(event)
+    runPopupPage(event, touchState)
   else
-    runDevicePage(event)
+    runDevicePage(event, touchState)
   end
 
   refreshNext()
+
+  if exitscript > 0 then
+    return 1
+  end
 
   return 0
 end
