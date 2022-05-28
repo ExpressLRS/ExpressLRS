598cb564fc391ff1103e12952a8af8fe234b1f8e
Bryan Mayland <bmayland@capnbry.net>
2022-05-28 14:59:57 -0400

    Display VTX Admin menus go to Send VTX on save

--- src/lib/SCREEN/menu.cpp
+++ src/lib/SCREEN/menu.cpp
@@ -410,12 +410,28 @@ fsm_state_entry_t const vtx_execute_fsm[] = {
     {STATE_VTX_SEND, nullptr, executeSendVTX, 1000, vtx_execute_events, ARRAY_SIZE(vtx_execute_events)},
 };
 
+// Changing Channel, Band, Power, Pitmode in the VTX Admin menu operate like the value_select_fsm, except
+// After saving they jump to STATE_VTX_SEND to allow an immediate send instead of doing a POP back to the
+// item and requiring a LEFT then PREV/NEXT to get to it
+fsm_state_event_t const vtx_save_events[] = {{EVENT_IMMEDIATE, GOTO(STATE_VTX_SEND)}};
 fsm_state_event_t const vtx_send_events[] = {MENU_EVENTS(vtx_execute_fsm)};
+fsm_state_entry_t const vtx_select_fsm[] = {
+    {STATE_VALUE_INIT, nullptr, setupValueIndex, 0, value_init_events, ARRAY_SIZE(value_init_events)},
+    {STATE_VALUE_SELECT, nullptr, displayValueIndex, 20000, value_select_events, ARRAY_SIZE(value_select_events)},
+    {STATE_VALUE_INC, nullptr, incrementValueIndex, 0, value_increment_events, ARRAY_SIZE(value_increment_events)},
+    {STATE_VALUE_DEC, nullptr, decrementValueIndex, 0, value_decrement_events, ARRAY_SIZE(value_decrement_events)},
+    {STATE_VALUE_SAVE, nullptr, saveValueIndex, 0, vtx_save_events, ARRAY_SIZE(vtx_save_events)},
+
+    {STATE_VTX_SEND, nullptr, displayMenuScreen, 20000, vtx_send_events, ARRAY_SIZE(vtx_send_events)},
+    {STATE_LAST}
+};
+fsm_state_event_t const vtx_item_events[] = {MENU_EVENTS(vtx_select_fsm)};
 fsm_state_entry_t const vtx_menu_fsm[] = {
-    {STATE_VTX_BAND, nullptr, displayMenuScreen, 20000, value_menu_events, ARRAY_SIZE(value_menu_events)},
-    {STATE_VTX_CHANNEL, nullptr, displayMenuScreen, 20000, value_menu_events, ARRAY_SIZE(value_menu_events)},
-    {STATE_VTX_POWER, nullptr, displayMenuScreen, 20000, value_menu_events, ARRAY_SIZE(value_menu_events)},
-    {STATE_VTX_PITMODE, nullptr, displayMenuScreen, 20000, value_menu_events, ARRAY_SIZE(value_menu_events)},
+    // Channel first, the most frequently changed
+    {STATE_VTX_CHANNEL, nullptr, displayMenuScreen, 20000, vtx_item_events, ARRAY_SIZE(vtx_item_events)},
+    {STATE_VTX_BAND, nullptr, displayMenuScreen, 20000, vtx_item_events, ARRAY_SIZE(vtx_item_events)},
+    {STATE_VTX_POWER, nullptr, displayMenuScreen, 20000, vtx_item_events, ARRAY_SIZE(vtx_item_events)},
+    {STATE_VTX_PITMODE, nullptr, displayMenuScreen, 20000, vtx_item_events, ARRAY_SIZE(vtx_item_events)},
     {STATE_VTX_SEND, nullptr, displayMenuScreen, 20000, vtx_send_events, ARRAY_SIZE(vtx_send_events)},
     {STATE_LAST}
 };
@@ -501,10 +517,10 @@ fsm_state_entry_t const main_menu_fsm[] = {
 #ifdef HAS_THERMAL
     {STATE_SMARTFAN, [](){return OPT_HAS_THERMAL;}, displayMenuScreen, 20000, value_menu_events, ARRAY_SIZE(value_menu_events)},
 #endif
-    {STATE_VTX, nullptr, displayMenuScreen, 20000, vtx_menu_events, ARRAY_SIZE(vtx_menu_events)},
     {STATE_JOYSTICK, nullptr, displayMenuScreen, 20000, ble_menu_events, ARRAY_SIZE(ble_menu_events)},
     {STATE_BIND, nullptr, displayMenuScreen, 20000, bind_menu_events, ARRAY_SIZE(bind_menu_events)},
     {STATE_WIFI, nullptr, displayMenuScreen, 20000, wifi_menu_events, ARRAY_SIZE(wifi_menu_events)},
+    {STATE_VTX, nullptr, displayMenuScreen, 20000, vtx_menu_events, ARRAY_SIZE(vtx_menu_events)},
     {STATE_LAST}
 };
 
