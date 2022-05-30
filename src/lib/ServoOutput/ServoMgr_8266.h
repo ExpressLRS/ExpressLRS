3fac8771e42c59ab333a579de2a2a2a4018b45d5
Bryan Mayland <bmayland@capnbry.net>
2022-05-30 07:39:06 -0400

    Refactor servo output to device, add per-servo output rate (#1558)
    * Move servo output to device
    
    * Rejigger of waveform generator for 8266
    
    * Add per-servo refresh rate and convert to channel-based
    
    * Add per-servo PWM mode / narrow to config / webui
    
    * Disable Servo PWM when going to web mode
    
    * Before someone notices!
    
    * waveform_8266 only on 8266
    
    * Leftover debig removed
    
    * Rename Narrow to 750us
    
    * 1016us -> 1006us
    
    * Remove core PWM implementations
    
    * Support 16x PWM channels in config
    
    * Fix DEBUG_LOG for Servos and only initialize has servos
    
    * Change PWMPEX to 6ch only since 7ch don't work
    
    * Use 299 scale for PWM6/7 (33K:1K divider) for 8S
    
    * Switch to use OPT_HAS_SERVO_OUTPUT
    
    * Allow servo ranges down to 881 (CRSF(us)==1)
    
    * Forgot to add ch13-ch16 to webui
    
    * Changes per review
    
    * 988U -> SERVO_FAILSAFE_MIN
    
    Co-authored-by: Paul Kendall <pkendall64@gmail.com>

---
+++ src/lib/ServoOutput/ServoMgr_8266.h
@@ -0,0 +1,37 @@
+#if defined(PLATFORM_ESP8266)
+#pragma once
+
+#include <Arduino.h>
+
+class ServoMgr_8266
+{
+public:
+    ServoMgr_8266(const uint8_t * const pins, const uint8_t outputCnt, uint32_t defaultInterval = 20000U);
+    ~ServoMgr_8266() { delete [] _refreshInterval; }
+
+    // Initialize the pins for output
+    void initialize();
+    // Start/Update PWM
+    void writeMicroseconds(uint8_t ch, uint16_t valueUs);
+    // Stop PWM
+    void stopPwm(uint8_t ch);
+    // Stop any active PWM channels (and set LOW)
+    void stopAllPwm();
+    // Set a pin high/low (will stopPwm first if active)
+    void writeDigital(uint8_t ch, bool value);
+
+    uint16_t getRefreshInterval(uint8_t ch) const { return _refreshInterval[ch]; }
+    void setRefreshInterval(uint8_t ch, uint16_t intervalUs);
+    bool isPwmActive(uint8_t ch) const { return _activePwmChannels & (1 << ch); }
+    bool isAnyPwmActive() const { return _activePwmChannels; }
+    uint8_t getOutputCnt() const { return _outputCnt; }
+
+    const uint8_t PIN_DISCONNECTED = 0xff;
+private:
+    const uint8_t * const _pins;
+    const uint8_t _outputCnt;
+    uint16_t *_refreshInterval;
+    uint32_t _activePwmChannels;
+};
+
+#endif
