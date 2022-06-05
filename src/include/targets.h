dc3aab9520c218115d007a6ced1d52b5295bd6be
Jye <JyeSmith@users.noreply.github.com>
2022-06-05 18:28:17 +1000

    add missing pin config

--- src/include/targets.h
+++ src/include/targets.h
@@ -86,17 +86,26 @@
 #ifndef GPIO_PIN_SCK
 #define GPIO_PIN_SCK UNDEF_PIN
 #endif
+#ifndef GPIO_PIN_NSS_2
+#define GPIO_PIN_NSS_2 UNDEF_PIN
+#endif
 #ifndef GPIO_PIN_RST
 #define GPIO_PIN_RST UNDEF_PIN
 #endif
 #ifndef GPIO_PIN_BUSY
 #define GPIO_PIN_BUSY UNDEF_PIN
 #endif
+#ifndef GPIO_PIN_BUSY_2
+#define GPIO_PIN_BUSY_2 UNDEF_PIN
+#endif
 #ifndef GPIO_PIN_DIO0
 #define GPIO_PIN_DIO0 UNDEF_PIN
 #endif
 #ifndef GPIO_PIN_DIO1
 #define GPIO_PIN_DIO1 UNDEF_PIN
+#endif
+#ifndef GPIO_PIN_DIO1_2
+#define GPIO_PIN_DIO1_2 UNDEF_PIN
 #endif
 #ifndef GPIO_PIN_DIO2
 #define GPIO_PIN_DIO2 UNDEF_PIN
