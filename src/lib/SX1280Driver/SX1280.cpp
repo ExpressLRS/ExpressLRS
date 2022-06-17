e3ffa4a9b89468874610e8a5ec9023c633f176e8
Jye <JyeSmith@users.noreply.github.com>
2022-06-17 17:14:45 +1000

    Set Synch Address Control to zero bit errors permissible

--- src/lib/SX1280Driver/SX1280.cpp
+++ src/lib/SX1280Driver/SX1280.cpp
@@ -326,6 +326,18 @@ void SX1280Driver::SetPacketParamsFLRC(uint8_t HeaderType,
 
     hal.WriteRegister(SX1280_REG_FLRC_SYNC_WORD, buf, 4, SX1280_Radio_All);
 
+    // Set Synch Address Control to zero bit errors permissible 
+    uint8_t syncAddrCtrl = hal.ReadRegister(SX1280_REG_FLRC_SYNC_ADDR_CTRL, SX1280_Radio_1);
+    syncAddrCtrl &= SX1280_REG_FLRC_SYNC_ADDR_CTRL_ZERO_MASK;  // Preserve the upper 4:7 bits as they are an unknown register.
+    hal.WriteRegister(SX1280_REG_FLRC_SYNC_ADDR_CTRL, syncAddrCtrl, SX1280_Radio_1);
+    
+    if (GPIO_PIN_NSS_2 != UNDEF_PIN)
+    {
+        syncAddrCtrl = hal.ReadRegister(SX1280_REG_FLRC_SYNC_ADDR_CTRL, SX1280_Radio_2);
+        syncAddrCtrl &= SX1280_REG_FLRC_SYNC_ADDR_CTRL_ZERO_MASK;  // Preserve the upper 4:7 bits as they are an unknown register.
+        hal.WriteRegister(SX1280_REG_FLRC_SYNC_ADDR_CTRL, syncAddrCtrl, SX1280_Radio_2);
+    }
+
     // FEI only works in Lora and Ranging mode
     modeSupportsFei = false;
 }
