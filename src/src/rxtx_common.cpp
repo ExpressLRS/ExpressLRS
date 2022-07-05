ed7a26a60dc8a28d16d9526effd8d2e89dc78f07
Bryan Mayland <bmayland@capnbry.net>
2022-07-05 01:34:21 -0400

    R9MM RX resetting config on every reboot (#1675)
    * Explicitly set I2C for STM32 which has ambiguity in begin()
    
    * Fix ESP RX compile error

--- src/src/rxtx_common.cpp
+++ src/src/rxtx_common.cpp
@@ -10,14 +10,29 @@ static uint32_t startDeferredTime = 0;
 static uint32_t deferredTimeout = 0;
 static std::function<void()> deferredFunction = nullptr;
 
-void setupTargetCommon()
+static void setupWire()
 {
 #if defined(USE_I2C)
-  if(GPIO_PIN_SDA != UNDEF_PIN && GPIO_PIN_SCL != UNDEF_PIN)
-  {
-    Wire.begin(GPIO_PIN_SDA, GPIO_PIN_SCL);
-  }
+    if(GPIO_PIN_SDA != UNDEF_PIN && GPIO_PIN_SCL != UNDEF_PIN)
+    {
+#if defined(PLATFORM_STM32)
+        // Wire::begin() passing ints is ambiguously overloaded, use the set functions
+        // which themselves might get the PinName overloads
+        Wire.setSCL(GPIO_PIN_SCL);
+        Wire.setSDA(GPIO_PIN_SDA);
+        Wire.begin();
+#else
+        // ESP hopes to get Wire::begin(int, int)
+        // ESP32 hopes to get Wire::begin(int = -1, int = -1, uint32 = 0)
+        Wire.begin((int)GPIO_PIN_SCL, (int)GPIO_PIN_SDA);
 #endif
+    }
+#endif
+}
+
+void setupTargetCommon()
+{
+    setupWire();
 }
 
 void deferExecution(uint32_t ms, std::function<void()> f)
