0e393686e2e24135fe8c767a7160fbe4c41c7cb4
Bryan Mayland <bmayland@capnbry.net>
2022-07-07 18:31:31 -0400

    Use static CRSF for compile error

--- src/lib/Telemetry/telemetry.cpp
+++ src/lib/Telemetry/telemetry.cpp
@@ -255,7 +255,7 @@ bool Telemetry::AppendTelemetryPackage(uint8_t *package)
                 if (wifi2tcp.hasClient() && (header->type == CRSF_FRAMETYPE_MSP_RESP || header->type == CRSF_FRAMETYPE_MSP_REQ)) // if we have a client we probs wanna talk to it
                 {
                     DBGLN("Got MSP frame, forwarding to client, len: %d", currentTelemetryByte);
-                    crsf.crsf2msp.parse(package);
+                    CRSF::crsf2msp.parse(package);
                 }
                 else // if no TCP client we just want to forward MSP over the link
             #endif
