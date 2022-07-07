a69e63927a0870dab765bb99cec30a675f095ab4
Bryan Mayland <bmayland@capnbry.net>
2022-07-07 19:52:52 -0400

    Allow button to reset RX config (#1681)
    * 9x Long Button now resets config, works on STM32
    
    * Clear SPIFFS as well when resetting

--- src/lib/BUTTON/devButton.h
+++ src/lib/BUTTON/devButton.h
@@ -1,11 +1,9 @@
 #pragma once
 
 #include "device.h"
+#include "config.h"
 
 #if defined(GPIO_PIN_BUTTON)
-    #if defined(TARGET_TX) || \
-        (defined(TARGET_RX) && (defined(PLATFORM_ESP32) || defined(PLATFORM_ESP8266)))
-        extern device_t Button_device;
-        #define HAS_BUTTON
-    #endif
+    extern device_t Button_device;
+    #define HAS_BUTTON
 #endif
