c3fa86db5a370396589373ff61783d84ba05a218
Jose Carmelo O. Almadrones <almadrones.jose.carmelo@gmail.com>
2022-08-04 23:53:09 +0800

    EU CE Regulatory Domain display text

--- src/lib/FHSS/FHSS.cpp
+++ src/lib/FHSS/FHSS.cpp
@@ -18,7 +18,11 @@ const fhss_config_t domains[] = {
 #include "SX1280Driver.h"
 
 const fhss_config_t domains[] = {
-    {"ISM2G4", FREQ_HZ_TO_REG_VAL(2400400000), FREQ_HZ_TO_REG_VAL(2479400000), 80}
+    #if defined(Regulatory_Domain_EU_CE_2400)
+        {"CE_LBT", FREQ_HZ_TO_REG_VAL(2400400000), FREQ_HZ_TO_REG_VAL(2479400000), 80}
+    #elif defined(Regulatory_Domain_ISM_2400)
+        {"ISM2G4", FREQ_HZ_TO_REG_VAL(2400400000), FREQ_HZ_TO_REG_VAL(2479400000), 80}
+    #endif
 };
 #endif
 
