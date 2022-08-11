4a5d201df5cb986ac0cfb53ecad78c9ea9768d85
Paul Kendall <pkendall64@gmail.com>
2022-08-12 08:20:23 +1200

    RX as TX with RGB

--- src/lib/CRSF/devCRSF_tx.cpp
+++ src/lib/CRSF/devCRSF_tx.cpp
@@ -10,8 +10,7 @@ static int start()
     CRSF::Begin();
 #if defined(DEBUG_TX_FREERUN)
     CRSF::CRSFstate = true;
-    extern void UARTconnected();
-    UARTconnected();
+    CRSF::connected();
 #endif
     return DURATION_IMMEDIATELY;
 }
