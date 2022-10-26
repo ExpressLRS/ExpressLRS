9098881aa6bc510299cf78ed7fa1ea4d9581c459
Paul Kendall <pkendall64@gmail.com>
2022-10-26 16:50:20 +1300

    The actions in the FIFOs need to be atomic
    So wrap them in critical sections

--- src/lib/FIFO/FIFO.cpp
+++ src/lib/FIFO/FIFO.cpp
@@ -30,6 +30,20 @@
 #include "FIFO.h"
 #include "logging.h"
 
+#if defined(PLATFORM_ESP32)
+#define ENTER_CRITICAL  portENTER_CRITICAL(&mux)
+#define EXIT_CRITICAL   portEXIT_CRITICAL(&mux)
+#elif defined(PLATFORM_ESP8266)
+#define ENTER_CRITICAL  noInterrupts()
+#define EXIT_CRITICAL   interrupts()
+#elif defined(PLATFORM_STM32)
+#define ENTER_CRITICAL  noInterrupts()
+#define EXIT_CRITICAL   interrupts()
+#else
+#define ENTER_CRITICAL
+#define EXIT_CRITICAL
+#endif
+
 FIFO::FIFO()
 {
     head = 0;
@@ -51,9 +65,11 @@ void ICACHE_RAM_ATTR FIFO::push(const uint8_t data)
     }
     else
     {
+        ENTER_CRITICAL;
         numElements++;
         buffer[tail] = data;
         tail = (tail + 1) % FIFO_SIZE;
+        EXIT_CRITICAL;
     }
 }
 
@@ -65,12 +81,14 @@ void ICACHE_RAM_ATTR FIFO::pushBytes(const uint8_t *data, uint8_t len)
         flush();
         return;
     }
+    ENTER_CRITICAL;
     for (int i = 0; i < len; i++)
     {
         buffer[tail] = data[i];
         tail = (tail + 1) % FIFO_SIZE;
     }
     numElements += len;
+    EXIT_CRITICAL;
 }
 
 uint8_t ICACHE_RAM_ATTR FIFO::pop()
@@ -80,13 +98,12 @@ uint8_t ICACHE_RAM_ATTR FIFO::pop()
         // DBGLN(F("Buffer empty"));
         return 0;
     }
-    else
-    {
-        numElements--;
-        uint8_t data = buffer[head];
-        head = (head + 1) % FIFO_SIZE;
-        return data;
-    }
+    ENTER_CRITICAL;
+    numElements--;
+    uint8_t data = buffer[head];
+    head = (head + 1) % FIFO_SIZE;
+    EXIT_CRITICAL;
+    return data;
 }
 
 void ICACHE_RAM_ATTR FIFO::popBytes(uint8_t *data, uint8_t len)
@@ -95,16 +112,16 @@ void ICACHE_RAM_ATTR FIFO::popBytes(uint8_t *data, uint8_t len)
     {
         // DBGLN(F("Buffer underrun"));
         flush();
+        return;
     }
-    else
+    ENTER_CRITICAL;
+    numElements -= len;
+    for (int i = 0; i < len; i++)
     {
-        numElements -= len;
-        for (int i = 0; i < len; i++)
-        {
-            data[i] = buffer[head];
-            head = (head + 1) % FIFO_SIZE;
-        }
+        data[i] = buffer[head];
+        head = (head + 1) % FIFO_SIZE;
     }
+    EXIT_CRITICAL;
 }
 
 uint8_t ICACHE_RAM_ATTR FIFO::peek()
@@ -114,11 +131,8 @@ uint8_t ICACHE_RAM_ATTR FIFO::peek()
         // DBGLN(F("Buffer empty"));
         return 0;
     }
-    else
-    {
-        uint8_t data = buffer[head];
-        return data;
-    }
+    uint8_t data = buffer[head];
+    return data;
 }
 
 uint16_t ICACHE_RAM_ATTR FIFO::size()
@@ -128,19 +142,26 @@ uint16_t ICACHE_RAM_ATTR FIFO::size()
 
 void ICACHE_RAM_ATTR FIFO::flush()
 {
+    ENTER_CRITICAL;
     head = 0;
     tail = 0;
     numElements = 0;
+    EXIT_CRITICAL;
 }
 
 bool ICACHE_RAM_ATTR FIFO::ensure(uint8_t requiredSize)
 {
     if(requiredSize > FIFO_SIZE)
+    {
         return false;
-    while(!available(requiredSize)) {
+    }
+    ENTER_CRITICAL;
+    while(!available(requiredSize))
+    {
         uint8_t len = pop();
         head = (head + len) % FIFO_SIZE;
         numElements -= len;
     }
+    EXIT_CRITICAL;
     return true;
 }
