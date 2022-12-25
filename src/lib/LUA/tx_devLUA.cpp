84b6dbd45666c47304ff2e2f4fdb205677221a2d
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2022-12-25 19:43:41 +0200

    removed Ble Telemetry option from lua menu on non-esp32 bases platforms

--- src/lib/LUA/tx_devLUA.cpp
+++ src/lib/LUA/tx_devLUA.cpp
@@ -98,12 +98,14 @@ static struct luaItem_selection luaModelMatch = {
     modelMatchUnit
 };
 
+#if defined(PLATFORM_ESP32)
 static struct luaItem_selection luaBLETelemetry = {
     {"BleTelemetry", CRSF_TEXT_SELECTION},
     0, // value
     "Off;On",
     STR_EMPTYSPACE
 };
+#endif
 
 static struct luaItem_command luaBind = {
     {"Bind", CRSF_COMMAND},
@@ -572,10 +574,12 @@ static void registerLuaParameters()
       luadevUpdateModelID();
     });
 
+#if defined(PLATFORM_ESP32)
     registerLUAParameter(&luaBLETelemetry, [](struct luaPropertiesCommon *item, uint8_t arg) {
       bool newBLETelemetry = arg;
       config.SetBLETelemetry(newBLETelemetry);      
     });
+#endif
 
     // POWER folder
     registerLUAParameter(&luaPowerFolder);
@@ -693,7 +697,9 @@ static int event()
   }
   luadevUpdateModelID();
   setLuaTextSelectionValue(&luaModelMatch, (uint8_t)config.GetModelMatch());
+#if defined(PLATFORM_ESP32)
   setLuaTextSelectionValue(&luaBLETelemetry, (uint8_t)config.GetBLETelemetry());
+#endif  
   setLuaTextSelectionValue(&luaPower, config.GetPower() - MinPower);
   if (GPIO_PIN_FAN_EN != UNDEF_PIN || GPIO_PIN_FAN_PWM != UNDEF_PIN)
   {
