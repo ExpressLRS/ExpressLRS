0a32a7fb4ba32c70a517cf906468c4180dc28d97
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2022-12-28 22:27:42 +0200

    Update src/lib/BLE/devBLETele.h
    Co-authored-by: Janez Troha <239513+dz0ny@users.noreply.github.com>

--- src/lib/BLE/devBLETele.h
+++ src/lib/BLE/devBLETele.h
@@ -4,7 +4,7 @@
 
 #if defined(PLATFORM_ESP32)
 extern device_t BLET_device;
-void ICACHE_RAM_ATTR BluetoothTelemetryUpdateValues(uint8_t *data);
+void BluetoothTelemetryUpdateValues(uint8_t *data);
 void BluetoothTelemetryShutdown();
 #define HAS_BLET
 #endif
