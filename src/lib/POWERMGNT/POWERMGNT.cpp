ec15f4f07b8df79f5ee54abb76a8e33f9ab5f3f1
Jye <JyeSmith@users.noreply.github.com>
2023-05-09 19:14:04 +1000

    Revert "fix 8285 builds"
    This reverts commit dd2e7db935ce8175d70a95031caa38063a00ca0c.

--- src/lib/POWERMGNT/POWERMGNT.cpp
+++ src/lib/POWERMGNT/POWERMGNT.cpp
@@ -270,7 +270,7 @@ void POWERMGNT::setPower(PowerLevels_e Power)
     //Set DACs PA5 & PA4
     analogWrite(GPIO_PIN_RFamp_APC1, 3350); //0-4095 2.7V
     analogWrite(GPIO_PIN_RFamp_APC2, powerValues[Power - MinPower]);
-#elif defined(POWER_OUTPUT_DACWRITE) && defined(PLATFORM_ESP32)
+#elif defined(POWER_OUTPUT_DACWRITE)
     Radio.SetOutputPower(0b0000);
     dacWrite(GPIO_PIN_RFamp_APC2, powerValues[Power - MinPower]);
 #else
