f0c7967b6b69f805a43ac2c3d8c727d3bbdeebe7
Paul Kendall <pkendall64@gmail.com>
2023-05-10 08:00:53 +1200

    Respect model match on all protocols

--- src/src/rx-serial/devSerialIO.cpp
+++ src/src/rx-serial/devSerialIO.cpp
@@ -31,15 +31,22 @@ static int event()
 
 static int timeout()
 {
-    if (connectionState != serialUpdate)
+    if (connectionState == serialUpdate)
     {
-        uint32_t duration = serialIO->sendRCFrameToFC(frameAvailable, ChannelData);
-        frameAvailable = false;
-        serialIO->handleUARTout();
-        serialIO->handleUARTin();
-        return duration;
+        return DURATION_NEVER;  // stop callbacks when doing serial update
     }
-    return DURATION_IMMEDIATELY;
+
+    uint32_t duration = 10; // 10ms callback (i.e. when no theres no model match)
+    // only send frames if we have a model match
+    if (connectionHasModelMatch)
+    {
+        duration = serialIO->sendRCFrameToFC(frameAvailable, ChannelData);
+    }
+    frameAvailable = false;
+    // still get telemetry and send link stats if theres no model match
+    serialIO->handleUARTout();
+    serialIO->handleUARTin();
+    return duration;
 }
 
 device_t Serial_device = {
