1ab8ef3866892839106eb607c45f2546c627d606
Senk0 <103913375+Senk02@users.noreply.github.com>
2023-05-13 05:53:57 -0500

    Fix for airport to enable CRSF communication (#2215)
    * Remove lines 10-13 in /src/lib/CRSF/devCRSF_tx.cpp

--- src/lib/CRSF/devCRSF_tx.cpp
+++ src/lib/CRSF/devCRSF_tx.cpp
@@ -7,10 +7,6 @@
 
 static int start()
 {
-    if (firmwareOptions.is_airport)
-    {
-        return DURATION_NEVER;
-    }
     CRSF::Begin();
 #if defined(DEBUG_TX_FREERUN)
     CRSF::CRSFstate = true;
