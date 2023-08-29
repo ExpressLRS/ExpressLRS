30ffc19836048de5b1ca6baee1e944e20c01c6cd
Johannes Kasberger <schugabe@gmx.at>
2023-08-29 23:29:14 +0200

    add udp wifi joystick

---
+++ src/lib/WIFI/wifiJoystick.cpp
@@ -0,0 +1,121 @@
+#include "device.h"
+
+#include "wifiJoystick.h"
+
+#if defined(TARGET_TX) && defined(PLATFORM_ESP32)
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include "common.h"
+#include "CRSF.h"
+#include "POWERMGNT.h"
+#include "hwTimer.h"
+#include "logging.h"
+#include "options.h"
+
+
+#if defined(RADIO_SX127X)
+extern SX127xDriver Radio;
+#elif defined(RADIO_SX128X)
+extern SX1280Driver Radio;
+#endif
+
+WiFiUDP *WifiJoystick::udp = NULL;
+IPAddress WifiJoystick::remoteIP;
+uint8_t WifiJoystick::channelCount = JOYSTICK_DEFAULT_CHANNEL_COUNT;
+bool WifiJoystick::active = false;
+
+void WifiJoystick::StartJoystickService()
+{
+    if (!udp)
+    {
+        udp = new WiFiUDP();
+        udp->begin(JOYSTICK_PORT);
+    }
+}
+
+void WifiJoystick::StopJoystickService()
+{
+    active = false;
+    udp->stop();
+    delete udp;
+    udp = NULL;
+}
+
+void WifiJoystick::StartSending(IPAddress ip, uint32_t updateInterval, uint8_t newChannelCount)
+{
+    if (!udp)
+    {
+        return;
+    }
+    remoteIP = ip;
+
+    hwTimer::updateInterval(updateInterval);
+    CRSF::setSyncParams(updateInterval);
+    POWERMGNT::setPower(MinPower);
+    Radio.End();
+
+    CRSF::RCdataCallback = UpdateValues;
+    channelCount = newChannelCount;
+
+    if (channelCount > 16)
+    {
+        channelCount = 16;
+    }
+    active = true;
+}
+
+
+void WifiJoystick::Loop(unsigned long now)
+{
+   static unsigned long millis = 0;
+   if(!udp || active)
+   {
+       return;
+   }
+
+   // broadcast wifi joystick options
+   if(now >= millis + 5000)
+   {
+      // DBGLN("SSID: %s", WiFi.SSID());
+
+       const char header[] = "ELRS_JOYSTICK";
+
+       udp->beginPacket("255.255.255.255", JOYSTICK_PORT);
+       udp->write((uint8_t*)header, strlen(header));
+       udp->write(JOYSTICK_VERSION);
+       udp->write((uint8_t*)device_name, strlen(device_name));
+       udp->endPacket();
+
+       millis = now;
+   }
+
+   udp->flush();
+
+}
+
+/*
+Frame format:
+1 byte frame type
+1 byte channel count
+channel count * 2 bytes channel data in range -992 to 992
+*/
+void WifiJoystick::UpdateValues()
+{
+    if (!udp || !active)
+    {
+        return;
+    }
+
+    udp->beginPacket(remoteIP, JOYSTICK_PORT);
+    udp->write(JOYSTICK_CHANNEL_FRAME);
+    udp->write(channelCount);
+    for (uint8_t i = 0; i < channelCount; i++)
+    {
+        uint16_t channel = ChannelData[i] - CRSF_CHANNEL_VALUE_MID;
+        udp->write((uint8_t*)&channel, 2);
+    }
+    udp->endPacket();
+}
+
+#endif
---
+++ src/lib/WIFI/wifiJoystick.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#if defined(TARGET_TX) && defined(PLATFORM_ESP32)
+
+#include <WiFiUdp.h>
+#include <AsyncUDP.h>
+
+#define HAS_WIFI_JOYSTICK 1
+#define JOYSTICK_PORT 11000
+#define JOYSTICK_DEFAULT_UPDATE_INTERVAL 10000
+#define JOYSTICK_DEFAULT_CHANNEL_COUNT 8
+#define JOYSTICK_VERSION 1
+#define JOYSTICK_CHANNEL_FRAME 1
+
+class WifiJoystick
+{
+public:
+    static void StartJoystickService();
+    static void StopJoystickService();
+    static void UpdateValues();
+    static void StartSending(IPAddress ip, uint32_t updateInterval, uint8_t newChannelCount);
+    static void Loop(unsigned long now);
+private:
+    static WiFiUDP *udp;
+    static IPAddress remoteIP;
+    static uint8_t channelCount;
+    static bool active;
+};
+
+#endif
