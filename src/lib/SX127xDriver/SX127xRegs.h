e18eb4ca5d9c03f9deac604a075f1907667316da
Paul Kendall <pkendall64@gmail.com>
2023-09-17 15:30:44 +1200

    Ability to set Semtech output power when using DAC for PA power control (#2388)
    * Ability to set Semtech output power when using DAC for PA power control
    
    * Single setOutputPower with MaxOutput & OutputPower for SX127x
    
    * Remove unnecessary defines
    
    * Create a define for the magic number

--- src/lib/SX127xDriver/SX127xRegs.h
+++ src/lib/SX127xDriver/SX127xRegs.h
@@ -125,10 +125,7 @@ typedef enum
 // SX127X_REG_PA_CONFIG
 #define SX127X_PA_SELECT_RFO 0b00000000    //  7     7     RFO pin output, power limited to +14 dBm
 #define SX127X_PA_SELECT_BOOST 0b10000000  //  7     7     PA_BOOST pin output, power limited to +20 dBm
-#define SX127X_OUTPUT_POWER 0b00001111     //  3     0     output power: P_out = 17 - (15 - OUTPUT_POWER) [dBm] for PA_SELECT_BOOST
-#define SX127X_MAX_OUTPUT_POWER_RFO_HF 0b00000000 //       Max output power when using RFO_HF
-#define SX127X_MAX_OUTPUT_POWER 0b01110000 //              Enable max output power
-#define SX127X_MAX_OUTPUT_POWER_INVALID 0b00010000 //      A value for the MaxPower field that is not used by ExpressLRS
+#define SX127X_PA_POWER_MASK 0b01111111    //  6     0     PA MAX_POWER and OUTPUT_POWER combined bit mask
 // SX127X_REG_OCP
 #define SX127X_OCP_OFF 0b00000000   //  5     5     PA overload current protection disabled
 #define SX127X_OCP_ON 0b00100000    //  5     5     PA overload current protection enabled
