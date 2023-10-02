604ee07037f89db2f0576e24d50663127bd88dd5
Johannes Kasberger <schugabe@gmx.at>
2023-10-02 23:54:26 +0200

    add usage documentation

--- src/lib/WIFI/wifiJoystick.h
+++ src/lib/WIFI/wifiJoystick.h
@@ -10,8 +10,38 @@
 #define JOYSTICK_DEFAULT_UPDATE_INTERVAL 10000
 #define JOYSTICK_DEFAULT_CHANNEL_COUNT 8
 #define JOYSTICK_VERSION 1
+#define JOYSTICK_VERSION_STRING "1"
 #define JOYSTICK_CHANNEL_FRAME 1
 
+/**
+ * Class to send stick data via udp
+ * Version 1
+ *
+ * Usage for simulator or driver on PC:
+ *
+ * Step 1: Find device. There are two options:
+ *   Recommended: Use MDNS to query for a service called elrs-joystick to find out IP+Port, Protocol version can be found in service txt version
+ *   Alternative: Listen for UDP broadcasts that contains a frame in the structure of
+ *     13 bytes: ELRS_JOYSTICK
+ *     1 byte: Protocol Version
+ *     uint 2 bytes: PORT
+ *     ASCII Text: Device Name
+ *
+ * Step 2:
+ *   Start udp socket that listens on configured PORT
+ *
+ * Step 3:
+ *   Send HTTP GET request to device in the form of http://<IP>/wifi_joystick?start=1&updateInterval=10000&channels=8
+ *   updateInterval is in us
+ *   channels defines how many channels are sent in each frame
+ *
+ * Step 3:
+ *   receive frames in the format of:
+ *   1 byte: Frame type = 1
+ *   1 byte: channel count
+ *   <channel count>*2 bytes: channel data each channel 2 bytes in range 0 to 32767
+ *
+ */
 class WifiJoystick
 {
 public:
