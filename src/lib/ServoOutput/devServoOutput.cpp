5b8a8073de3d6b8ce205158d3a3d40ba3a369688
Paul Kendall <pkendall64@gmail.com>
2023-10-11 16:31:18 +1300

    Set failsafe on startup

--- src/lib/ServoOutput/devServoOutput.cpp
+++ src/lib/ServoOutput/devServoOutput.cpp
@@ -219,6 +219,8 @@ static int start()
         }
 #endif
     }
+    // set servo outputs to failsafe position on start in case they want to play silly buggers!
+    servosFailsafe();
     return DURATION_NEVER;
 }
 
