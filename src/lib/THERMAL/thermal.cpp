5b7941eb4abf929fe514e59aab29324def335c73
Paul Kendall <pkendall64@gmail.com>
2023-10-30 14:46:08 +1300

    Get thermal info from S3 if no LM75A

--- src/lib/THERMAL/thermal.cpp
+++ src/lib/THERMAL/thermal.cpp
@@ -6,6 +6,9 @@
 #include "lm75a.h"
 LM75A lm75a;
 #endif
+#if defined(PLATFORM_ESP32_S3)
+#include "driver/temp_sensor.h"
+#endif
 
 uint8_t thermal_threshold_data[] = {
     THERMAL_FAN_DEFAULT_HIGH_THRESHOLD,
@@ -17,18 +20,32 @@ uint8_t thermal_threshold_data[] = {
 };
 
 int thermal_status = THERMAL_STATUS_FAIL;
+static bool use_internal_sensor = false;
 
 void Thermal::init()
 {
     int status = -1;
-#ifdef HAS_THERMAL_LM75A
-    status = lm75a.init();
-#endif
+    if (OPT_HAS_THERMAL_LM75A)
+    {
+        status = lm75a.init();
+    }
+#if defined(PLATFORM_ESP32_S3)
+    if (status == -1)
+    {
+        temp_sensor_config_t temp_sensor = TSENS_CONFIG_DEFAULT();
+        temp_sensor.dac_offset = TSENS_DAC_L2;  //TSENS_DAC_L2 is default   L4(-40℃ ~ 20℃), L2(-10℃ ~ 80℃) L1(20℃ ~ 100℃) L0(50℃ ~ 125℃)
+        temp_sensor_set_config(temp_sensor);
+        temp_sensor_start();
+        use_internal_sensor = true;
+    }
+#else
     if(status == -1)
     {
         ERRLN("Thermal failed!");
+        return;
     }
     else
+#endif
     {
         DBGLN("Thermal OK!");
         temp_value = 0;
@@ -49,8 +66,15 @@ uint8_t Thermal::read_temp()
         ERRLN("thermal not ready!");
         return 0;
     }
-#ifdef HAS_THERMAL_LM75A
-    return lm75a.read_lm75a();
+    if (OPT_HAS_THERMAL_LM75A)
+    {
+        return lm75a.read_lm75a();
+    }
+
+#if defined(PLATFORM_ESP32_S3)
+    float result = 0;
+    temp_sensor_read_celsius(&result);
+    return result;
 #else
     return 0;
 #endif
@@ -77,9 +101,10 @@ void Thermal::update_threshold(int index)
     }
     uint8_t high = thermal_threshold_data[2*index];
     uint8_t low = thermal_threshold_data[2*index+1];
-#ifdef HAS_THERMAL_LM75A
-    lm75a.update_lm75a_threshold(high, low);
-#endif
+    if (OPT_HAS_THERMAL_LM75A)
+    {
+        lm75a.update_lm75a_threshold(high, low);
+    }
 }
 
 #endif
