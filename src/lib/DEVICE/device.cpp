59301fc2402f1dd0d697822edb77159e96044f6e
Bryan Mayland <bmayland@capnbry.net>
2024-03-20 16:33:58 -0400

    Remove those turnery's for readability

--- src/lib/DEVICE/device.cpp
+++ src/lib/DEVICE/device.cpp
@@ -117,13 +117,14 @@ void devicesTriggerEvent()
 
 static int _devicesUpdate(unsigned long now)
 {
-    int32_t core = CURRENT_CORE;
+    const int32_t core = CURRENT_CORE;
+    const int32_t coreMulti = (core == -1) ? 0 : core;
 
     bool newModelMatch = connectionHasModelMatch && teamraceHasModelMatch;
-    bool handleEvents = eventFired[core==-1?0:core] || lastConnectionState[core==-1?0:core] != connectionState || lastModelMatch[core==-1?0:core] != newModelMatch;
-    eventFired[core==-1?0:core] = false;
-    lastConnectionState[core==-1?0:core] = connectionState;
-    lastModelMatch[core==-1?0:core] = newModelMatch;
+    bool handleEvents = eventFired[coreMulti] || lastConnectionState[coreMulti] != connectionState || lastModelMatch[coreMulti] != newModelMatch;
+    eventFired[coreMulti] = false;
+    lastConnectionState[coreMulti] = connectionState;
+    lastModelMatch[coreMulti] = newModelMatch;
 
     for(size_t i=0 ; i<deviceCount ; i++)
     {
