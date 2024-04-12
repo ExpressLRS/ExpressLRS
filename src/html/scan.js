1578c668582a29829cf7996d24055dcc6668f9c2
Michael <101.37584@germanynet.de>
2024-04-12 17:13:53 +0200

    removed duplicate line of code

--- src/html/scan.js
+++ src/html/scan.js
@@ -212,8 +212,6 @@ function updatePwmSettings(arPwm) {
   
   modeSelectionInit = false;
 
-  modeSelectionInit = false;
-
   // put some constraints on pinRx/Tx mode selects
   if (pinRxIndex !== undefined && pinTxIndex !== undefined) {
     const pinRxMode = _(`pwm_${pinRxIndex}_mode`);
