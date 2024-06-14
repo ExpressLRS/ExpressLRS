2b48718fa9f7455e110c54a42e0b3a47e0a8b255
MUSTARDTIGERFPV <mustardtiger@thetacoduck.com>
2024-06-13 17:49:42 -0700

    Move MAVLink to CRSF into separate file; fix likely math bug

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -23,7 +23,7 @@
 #include "devPDET.h"
 #include "devBackpack.h"
 
-#include "common/mavlink.h"
+#include "MAVLink.h"
 
 #if defined(PLATFORM_ESP32_S3)
 #include "USB.h"
@@ -1490,74 +1490,8 @@ void loop()
         {
           // raw mavlink data - forward to USB rather than handset
           uint8_t count = CRSFinBuffer[1];
-          for (uint8_t i = 0; i < count; i++)
-          {
-            mavlink_message_t msg;
-            mavlink_status_t status;
-            bool have_message = mavlink_parse_char(MAVLINK_COMM_0, CRSFinBuffer[CRSF_FRAME_NOT_COUNTED_BYTES + i], &msg, &status);
-            // convert mavlink messages to CRSF messages
-            if (have_message) {
-              switch (msg.msgid)
-              {
-                case MAVLINK_MSG_ID_BATTERY_STATUS:
-                {
-                  mavlink_battery_status_t battery_status;
-                  mavlink_msg_battery_status_decode(&msg, &battery_status);
-                  CRSF_MK_FRAME_T(crsf_sensor_battery_t) crsfbatt = { 0 };
-                  // mV -> mv*100
-                  crsfbatt.p.voltage = htobe16(battery_status.voltages[0] / 100);
-                  // cA -> mA*100
-                  crsfbatt.p.current = htobe16(battery_status.current_battery / 10);
-                  crsfbatt.p.capacity = htobe32(battery_status.current_consumed) & 0x0FFF;
-                  crsfbatt.p.remaining = battery_status.battery_remaining;
-                  CRSF::SetHeaderAndCrc((uint8_t *)&crsfbatt, CRSF_FRAMETYPE_BATTERY_SENSOR, CRSF_FRAME_SIZE(sizeof(crsf_sensor_battery_t)), CRSF_ADDRESS_CRSF_TRANSMITTER);
-                  handset->sendTelemetryToTX((uint8_t *)&crsfbatt);
-                  break;
-                }
-                case MAVLINK_MSG_ID_GPS_RAW_INT:
-                {
-                  mavlink_gps_raw_int_t gps_int;
-                  mavlink_msg_gps_raw_int_decode(&msg, &gps_int);
-                  CRSF_MK_FRAME_T(crsf_sensor_gps_t) crsfgps = { 0 };
-                  // mm -> meters + 1000
-                  crsfgps.p.altitude = htobe16(gps_int.alt / 1000 + 1000);
-                  // cm/s -> km/h / 10
-                  crsfgps.p.groundspeed = htobe16(gps_int.vel * 36 / 100);
-                  crsfgps.p.latitude = htobe32(gps_int.lat);
-                  crsfgps.p.longitude = htobe32(gps_int.lon);
-                  crsfgps.p.gps_heading = htobe16(gps_int.cog);
-                  crsfgps.p.satellites_in_use = gps_int.satellites_visible;
-                  CRSF::SetHeaderAndCrc((uint8_t *)&crsfgps, CRSF_FRAMETYPE_GPS, CRSF_FRAME_SIZE(sizeof(crsf_sensor_gps_t)), CRSF_ADDRESS_CRSF_TRANSMITTER);
-                  handset->sendTelemetryToTX((uint8_t *)&crsfgps);
-                  break;
-                }
-                case MAVLINK_MSG_ID_GLOBAL_POSITION_INT:
-                {
-                  mavlink_global_position_int_t global_pos;
-                  mavlink_msg_global_position_int_decode(&msg, &global_pos);
-                  CRSF_MK_FRAME_T(crsf_sensor_baro_vario_t) crsfbaro = { 0 };
-                  crsfbaro.p.altitude = htobe16(global_pos.alt / 100 + 10000);
-                  // force the top bit of altitude low to represent that it's in decimeters
-                  crsfbaro.p.altitude &= 0x7FFF;
-                  crsfbaro.p.verticalspd = htobe16(global_pos.vz);
-                  CRSF::SetHeaderAndCrc((uint8_t *)&crsfbaro, CRSF_FRAMETYPE_BARO_ALTITUDE, CRSF_FRAME_SIZE(sizeof(crsf_sensor_baro_vario_t)), CRSF_ADDRESS_CRSF_TRANSMITTER);
-                  handset->sendTelemetryToTX((uint8_t *)&crsfbaro);
-                  break;
-                }
-                case MAVLINK_MSG_ID_ATTITUDE:
-                {
-                  mavlink_attitude_t attitude;
-                  mavlink_msg_attitude_decode(&msg, &attitude);
-                  CRSF_MK_FRAME_T(crsf_sensor_attitude_t) crsfatt = { 0 };
-                  crsfatt.p.pitch = htobe16(attitude.pitch * 10000);
-                  crsfatt.p.roll = htobe16(attitude.roll * 10000);
-                  crsfatt.p.yaw = htobe16(attitude.yaw * 10000);
-                  CRSF::SetHeaderAndCrc((uint8_t *)&crsfatt, CRSF_FRAMETYPE_ATTITUDE, CRSF_FRAME_SIZE(sizeof(crsf_sensor_attitude_t)), CRSF_ADDRESS_CRSF_TRANSMITTER);
-                  handset->sendTelemetryToTX((uint8_t *)&crsfatt);
-                }
-              }
-            }
-          }
+          // Convert to CRSF telemetry where we can
+          convert_mavlink_to_crsf_telem(CRSFinBuffer, count, handset);
           // If we have a backpack 
           TxUSB->write(CRSFinBuffer + CRSF_FRAME_NOT_COUNTED_BYTES, count);
           if (TxUSB != TxBackpack)
