e32e36135fc0fb2708db6998764943585d13d1fc
MUSTARDTIGERFPV <mustardtiger@thetacoduck.com>
2024-06-17 13:04:52 -0700

    Only send 1 Alt CRSF telemetry

--- src/lib/MAVLink/MAVLink.cpp
+++ src/lib/MAVLink/MAVLink.cpp
@@ -6,6 +6,9 @@
 void convert_mavlink_to_crsf_telem(uint8_t *CRSFinBuffer, uint8_t count, Handset *handset)
 {
 #if !defined(PLATFORM_STM32)
+    // Store the relative altitude for GPS altitude
+    static int32_t relative_alt = 0;
+
     for (uint8_t i = 0; i < count; i++)
     {
         mavlink_message_t msg;
@@ -41,8 +44,20 @@ void convert_mavlink_to_crsf_telem(uint8_t *CRSFinBuffer, uint8_t count, Handset
                 mavlink_msg_gps_raw_int_decode(&msg, &gps_int);
                 CRSF_MK_FRAME_T(crsf_sensor_gps_t)
                 crsfgps = {0};
+// We use altitude relative to home for GPS altitude, by default, but we can also use GPS altitude if USE_MAVLINK_GPS_ALTITUDE is defined
+#if defined(USE_MAVLINK_GPS_ALTITUDE)
                 // mm -> meters + 1000
                 crsfgps.p.altitude = htobe16(gps_int.alt / 1000 + 1000);
+#else
+                // mm -> meters + 1000 (uint16_t)
+                // int16_t relative_alt_16le = relative_alt;
+                // relative_alt_16le = (relative_alt_16le / 1000) + 1000;
+                // crsfgps.p.altitude = htobe16(relative_alt_16le);
+
+                crsfgps.p.altitude = htobe16(((int16_t)relative_alt) / 1000 + 1000);
+
+
+#endif
                 // cm/s -> km/h / 10
                 crsfgps.p.groundspeed = htobe16(gps_int.vel * 36 / 100);
                 crsfgps.p.latitude = htobe32(gps_int.lat);
@@ -56,14 +71,13 @@ void convert_mavlink_to_crsf_telem(uint8_t *CRSFinBuffer, uint8_t count, Handset
             case MAVLINK_MSG_ID_GLOBAL_POSITION_INT: {
                 mavlink_global_position_int_t global_pos;
                 mavlink_msg_global_position_int_decode(&msg, &global_pos);
-                CRSF_MK_FRAME_T(crsf_sensor_baro_vario_t)
-                crsfbaro = {0};
-                crsfbaro.p.altitude = htobe16(global_pos.relative_alt / 100 + 10000);
-                // force the top bit of altitude low to represent that it's in decimeters
-                crsfbaro.p.altitude &= 0x7FFF;
-                crsfbaro.p.verticalspd = htobe16(global_pos.vz);
-                CRSF::SetHeaderAndCrc((uint8_t *)&crsfbaro, CRSF_FRAMETYPE_BARO_ALTITUDE, CRSF_FRAME_SIZE(sizeof(crsf_sensor_baro_vario_t)), CRSF_ADDRESS_CRSF_TRANSMITTER);
-                handset->sendTelemetryToTX((uint8_t *)&crsfbaro);
+                CRSF_MK_FRAME_T(crsf_sensor_vario_t)
+                crsfvario = {0};
+                // store relative altitude for GPS Alt so we don't have 2 Alt sensors
+                relative_alt = global_pos.relative_alt;
+                crsfvario.p.verticalspd = htobe16(global_pos.vz);
+                CRSF::SetHeaderAndCrc((uint8_t *)&crsfvario, CRSF_FRAMETYPE_VARIO, CRSF_FRAME_SIZE(sizeof(crsf_sensor_vario_t)), CRSF_ADDRESS_CRSF_TRANSMITTER);
+                handset->sendTelemetryToTX((uint8_t *)&crsfvario);
                 break;
             }
             case MAVLINK_MSG_ID_ATTITUDE: {
