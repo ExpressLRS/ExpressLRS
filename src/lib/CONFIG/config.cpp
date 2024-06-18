725f288e54c66b90c0b134cb6c080428c2a4df50
Michael <101.37584@germanynet.de>
2024-06-18 11:43:41 +0200

    cleanup

--- src/lib/CONFIG/config.cpp
+++ src/lib/CONFIG/config.cpp
@@ -1180,6 +1180,7 @@ void RxConfig::SetSerialProtocol(eSerialProtocol serialProtocol)
     }
 }
 
+#if defined(PLATFORM_ESP32)
 void RxConfig::SetSerial1Protocol(eSerial1Protocol serialProtocol)
 {
     if (m_config.serial1Protocol != serialProtocol)
@@ -1188,6 +1189,7 @@ void RxConfig::SetSerial1Protocol(eSerial1Protocol serialProtocol)
         m_modified = true;
     }
 }
+#endif
 
 void RxConfig::SetTeamraceChannel(uint8_t teamraceChannel)
 {
