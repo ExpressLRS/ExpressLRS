a03fdfdbd83983d3181ad866240b968d911c48b2
Jye <14170229+JyeSmith@users.noreply.github.com>
2024-06-29 12:58:00 +1000

    U0RXD_GPIO_NUM and U0TXD_GPIO_NUM

--- src/src/tx_main.cpp
+++ src/src/tx_main.cpp
@@ -1218,7 +1218,7 @@ static void setupSerial()
     // Set TxUSB to TxBackpack so that data goes to the same place
     TxUSB = TxBackpack;
   }
-  else if (GPIO_PIN_RCSIGNAL_RX == 3 && GPIO_PIN_RCSIGNAL_TX == 1)
+  else if (GPIO_PIN_RCSIGNAL_RX == U0RXD_GPIO_NUM && GPIO_PIN_RCSIGNAL_TX == U0TXD_GPIO_NUM)
   {
     // This is an internal module, or an external module configured with a relay.  Do not setup TxUSB.
     TxUSB = new NullStream();
