7510caecfab5605f98ab6ac114f17a95e104cec9
Paul Kendall <pkendall64@gmail.com>
2024-09-20 11:02:18 +1200

    Disable backpack in wifi and BLE modes

--- src/lib/Backpack/devBackpack.cpp
+++ src/lib/Backpack/devBackpack.cpp
@@ -363,7 +363,7 @@ static int event()
     if (OPT_USE_TX_BACKPACK && GPIO_PIN_BACKPACK_EN != UNDEF_PIN)
     {
         // EN should be HIGH to be active
-        digitalWrite(GPIO_PIN_BACKPACK_EN, config.GetBackpackDisable() ? LOW : HIGH);
+        digitalWrite(GPIO_PIN_BACKPACK_EN, (config.GetBackpackDisable() || connectionState == bleJoystick || connectionState == wifiUpdate) ? LOW : HIGH);
     }
 #endif
 #if !defined(PLATFORM_STM32)
