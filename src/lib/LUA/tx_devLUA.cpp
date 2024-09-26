67ee6802b0868423e86620596172b7a2080a43ea
Jye <14170229+JyeSmith@users.noreply.github.com>
2024-09-26 18:44:29 +1000

    Fix allowed rf modes in tx lua (#2954)
    * remove jank
    
    * fix allowed rates for low bauds

--- src/lib/LUA/tx_devLUA.cpp
+++ src/lib/LUA/tx_devLUA.cpp
@@ -589,11 +589,8 @@ static void recalculatePacketRateOptions(int minInterval)
     for (int i=0 ; i < RATE_MAX ; i++)
     {
         uint8_t rate = i;
-#if defined(RADIO_LR1121) // Janky fix to order menu correctly
-        rate = (rate + 4) % RATE_MAX;
-#endif
         rate = RATE_MAX - 1 - rate;
-        bool rateAllowed = get_elrs_airRateConfig(rate)->interval >= minInterval;
+        bool rateAllowed = (get_elrs_airRateConfig(rate)->interval * get_elrs_airRateConfig(rate)->numOfSends) >= minInterval;
         const char *semi = strchrnul(pos, ';');
         if (rateAllowed)
         {
