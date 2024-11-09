7fada1935a06f6ba81179b0870adb86ee5cafa73
Jye <14170229+JyeSmith@users.noreply.github.com>
2024-11-09 13:20:18 +1000

    update after master merge

--- src/lib/OTA/OTA.h
+++ src/lib/OTA/OTA.h
@@ -29,7 +29,7 @@ typedef struct {
     uint8_t rfRateEnum;
     uint8_t switchEncMode:1,
             newTlmRatio:3,
-            free:1;
+            free:1,
             otaProtocol:3;
     uint8_t UID4;
     uint8_t UID5;
