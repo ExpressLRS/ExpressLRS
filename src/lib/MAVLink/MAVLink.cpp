e5857b4da5afdbfc96aa6b69543eea87b272ca44
Patrick Menschel <menschel.p@posteo.de>
2024-12-31 23:20:30 +0100

    WIP: rework trigonometry to cordic

--- src/lib/MAVLink/MAVLink.cpp
+++ src/lib/MAVLink/MAVLink.cpp
@@ -1,82 +1,59 @@
 #include "MAVLink.h"
 #include "ardupilot_protocol.h"
 #include <math.h>
-#include <cmath>
-#include <endian.h>
+
 /*
  * Known Issues:
  * - Battery Capacity is currently not available via Mavlink BATTERY_INFO message is not implemented on Ardupilot side.
  *   This can be worked around by setting the battery capacity via Yaapu Config option.
  */
 
-
-/*
- * Adapted from Ardupilot Common and Math Lib
- */
-#define DEG_TO_RAD      (M_PI / 180.0f)
-#define RAD_TO_DEG      (180.0f / M_PI)
-
-// Centi-degrees to radians
-#define DEGX100 5729.57795f
-
-#define LATLON_TO_M     0.011131884502145034
-
 #define MIN(a,b) ((a)<(b)?(a):(b))
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
-typedef double ftype;
 
-int32_t diff_longitude(int32_t lon1, int32_t lon2);
-int32_t diff_longitude(int32_t lon1, int32_t lon2)
+#define NUMBER_OF_CORDIC_ITERATIONS (6U)
+/**
+ * static lookup for the angle corresponding tan(2^-i) * 1000
+ * essentially the degree that corresponds to a tan(degree) = [1, 0.5, 0.25, 0.125 0.0625, ...]
+ */
+static uint16_t lookup_theta[NUMBER_OF_CORDIC_ITERATIONS] = {45000, 26565, 14036, 7125, 3576, 1789};
+
+/**
+ * COS(degree) of phi [0.7071067811865476, 0.8944271909999159, 0.9701425001453319, 0.9922778767136676, 0.9980525784828885, 0.9995120760870788]
+ * product of all factors 0.607351770141296 * 1000
+ */
+#define KFACTOR1000 (607)
+
+/**
+ * Cordic Algo to transform cartesian coordinates into polar coordinates
+ */
+static void cordic_cartesion_to_polar(int32_t north, int32_t east, int32_t *phi, int32_t *radius);
+static void cordic_cartesion_to_polar(int32_t north, int32_t east, int32_t *phi, int32_t *radius)
 {
-    if ((lon1 & 0x80000000) == (lon2 & 0x80000000)) {
-        // common case of same sign
-        return lon1 - lon2;
+    int32_t x = north;
+    int32_t y = east;
+    int32_t offset = 0;
+    // compared to the academic examples, by swapping x and y, we have mirrored the coordinate system along the main diagonale
+    // this also makes the angle and the sign of the angle correct for the typical heading where North is 0 and positive is clockwise.
+    if (x < 0){
+        // the algo requires x to be positive
+        x = -x;
+        y = -y;
+        offset = 180;
     }
-    int64_t dlon = int64_t(lon1)-int64_t(lon2);
-    if (dlon > 1800000000LL) {
-        dlon -= 3600000000LL;
-    } else if (dlon < -1800000000LL) {
-        dlon += 3600000000LL;
+    int8_t sign = 1;
+    int32_t theta = 0;
+    
+    for (uint8_t index=0; index < NUMBER_OF_CORDIC_ITERATIONS; index++){
+        sign = (y >= 0) ? 1 : -1;
+        x -= (abs(y) >> index) * sign;
+        y += (abs(x) >> index) * sign;
+        theta += lookup_theta[index] * sign;
     }
-    return int32_t(dlon);
-}
-
-ftype longitude_scale(int32_t lat);
-ftype longitude_scale(int32_t lat)
-{
-    ftype scale = cos(lat * (1.0e-7 * DEG_TO_RAD));
-    return MAX(scale, 0.01);
-}
-
-// return horizontal distance in meters between two locations
-ftype get_distance(const int32_t lat1, const int32_t lon1, const int32_t lat2, const int32_t lon2);
-ftype get_distance(const int32_t lat1, const int32_t lon1, const int32_t lat2, const int32_t lon2)
-{
-    ftype dlat = (ftype)(lat2 - lat1);
-    ftype dlng = ((ftype)diff_longitude(lon2, lon1)) * longitude_scale((lat1+lat2)/2);
-    return sqrt(dlat*dlat + dlng*dlng) * LATLON_TO_M;
-}
-
-
-// return bearing in radians from location to loc2, return is 0 to 2*Pi
-ftype get_bearing(const int32_t lat1, const int32_t lon1, const int32_t lat2, const int32_t lon2);
-ftype get_bearing(const int32_t lat1, const int32_t lon1, const int32_t lat2, const int32_t lon2)
-{
-    const int32_t off_x = diff_longitude(lon2,lon1);
-    const int32_t off_y = (lat2 - lat1) / longitude_scale((lat1+lat2)/2);
-    ftype bearing = (M_PI*0.5) + atan2(-off_y, off_x);
-    if (bearing < 0) {
-        bearing += 2*M_PI;
-    }
-    return bearing;
-}
-
-// return bearing in centi-degrees from location to loc2, return is 0 to 35999
-int32_t get_bearing_to(const int32_t lat1, const int32_t lon1, const int32_t lat2, const int32_t lon2);
-int32_t get_bearing_to(const int32_t lat1, const int32_t lon1, const int32_t lat2, const int32_t lon2)
-{
-    return int32_t(get_bearing(lat1, lon1, lat2, lon2) * DEGX100 + 0.5);
+    *phi = (theta/1000) + offset;
+    *radius = (x * KFACTOR1000) / 1000;
+    return;
 }
 
 
@@ -100,7 +77,7 @@ static uint16_t prep_number(int32_t number, uint8_t digits, uint8_t power)
         if (abs_number < 100) {
             res = abs_number<<1;
         } else if (abs_number < 1270) {
-            res = ((uint8_t)roundf(abs_number * 0.1f)<<1)|0x1;
+            res = ((abs_number / 10)<<1)|0x1;
         } else { // transmit max possible value (0x7F x 10^1 = 1270)
             res = 0xFF;
         }
@@ -111,11 +88,11 @@ static uint16_t prep_number(int32_t number, uint8_t digits, uint8_t power)
         if (abs_number < 100) {
             res = abs_number<<2;
         } else if (abs_number < 1000) {
-            res = ((uint8_t)roundf(abs_number * 0.1f)<<2)|0x1;
+            res = ((abs_number / 10)<<2)|0x1;
         } else if (abs_number < 10000) {
-            res = ((uint8_t)roundf(abs_number * 0.01f)<<2)|0x2;
+            res = ((abs_number / 100)<<2)|0x2;
         } else if (abs_number < 127000) {
-            res = ((uint8_t)roundf(abs_number * 0.001f)<<2)|0x3;
+            res = ((abs_number / 1000)<<2)|0x3;
         } else { // transmit max possible value (0x7F x 10^3 = 127000)
             res = 0x1FF;
         }
@@ -126,7 +103,7 @@ static uint16_t prep_number(int32_t number, uint8_t digits, uint8_t power)
         if (abs_number < 1000) {
             res = abs_number<<1;
         } else if (abs_number < 10240) {
-            res = ((uint16_t)roundf(abs_number * 0.1f)<<1)|0x1;
+            res = ((abs_number / 1000)<<1)|0x1;
         } else { // transmit max possible value (0x3FF x 10^1 = 10230)
             res = 0x7FF;
         }
@@ -137,11 +114,11 @@ static uint16_t prep_number(int32_t number, uint8_t digits, uint8_t power)
         if (abs_number < 1000) {
             res = abs_number<<2;
         } else if (abs_number < 10000) {
-            res = ((uint16_t)roundf(abs_number * 0.1f)<<2)|0x1;
+            res = ((abs_number / 10)<<2)|0x1;
         } else if (abs_number < 100000) {
-            res = ((uint16_t)roundf(abs_number * 0.01f)<<2)|0x2;
+            res = ((abs_number / 100)<<2)|0x2;
         } else if (abs_number < 1024000) {
-            res = ((uint16_t)roundf(abs_number * 0.001f)<<2)|0x3;
+            res = ((abs_number / 1000)<<2)|0x3;
         } else { // transmit max possible value (0x3FF x 10^3 = 1023000)
             res = 0xFFF;
         }
@@ -186,7 +163,7 @@ static uint32_t format_ap_status(uint8_t base_mode, uint32_t custom_mode, uint8_
         ap_status |= (1 << AP_FS_OFFSET);
     }
     // signed throttle [-100,100] scaled down to [-63,63] on 7 bits, MSB for sign + 6 bits for 0-63
-    ap_status |= prep_number(throttle*0.63, 2, 0)<<AP_THROTTLE_OFFSET;
+    ap_status |= prep_number(throttle*63/100, 2, 0)<<AP_THROTTLE_OFFSET;
     return ap_status;
 }
 
@@ -209,11 +186,11 @@ static uint32_t format_gps_status(uint8_t fix_type, int32_t alt_msl_mm, uint16_t
     // GPS receiver status (limit to 0-3 (0x3) since the value is stored on 2 bits: NO_GPS = 0, NO_FIX = 1, GPS_OK_FIX_2D = 2, GPS_OK_FIX_3D or GPS_OK_FIX_3D_DGPS or GPS_OK_FIX_3D_RTK_FLOAT or GPS_OK_FIX_3D_RTK_FIXED = 3)
     gps_status |= ((fix_type < GPS_STATUS_LIMIT) ? fix_type : GPS_STATUS_LIMIT)<<GPS_STATUS_OFFSET;
     // GPS horizontal dilution of precision in dm
-    gps_status |= prep_number(roundf(eph * 0.1f),2,1)<<GPS_HDOP_OFFSET;
+    gps_status |= prep_number(eph / 10,2,1)<<GPS_HDOP_OFFSET;
     // GPS receiver advanced status (0: no advanced fix, 1: GPS_OK_FIX_3D_DGPS, 2: GPS_OK_FIX_3D_RTK_FLOAT, 3: GPS_OK_FIX_3D_RTK_FIXED)
     gps_status |= ((fix_type > GPS_STATUS_LIMIT) ? fix_type-GPS_STATUS_LIMIT : 0)<<GPS_ADVSTATUS_OFFSET;
     // Altitude MSL in dm
-    gps_status |= prep_number(roundf(alt_msl_mm * 0.01f),2,2)<<GPS_ALTMSL_OFFSET;
+    gps_status |= prep_number(alt_msl_mm / 100,2,2)<<GPS_ALTMSL_OFFSET;
     return gps_status;
 }
 
@@ -230,9 +207,9 @@ static uint32_t format_batt1(uint16_t voltage_mv, uint16_t current_ca, uint32_t 
 #define BATT_TOTALMAH_LIMIT         0x7FFF
 #define BATT_TOTALMAH_OFFSET        17
     // battery voltage in decivolts, can have up to a 12S battery (4.25Vx12S = 51.0V)
-    uint32_t batt = (((uint16_t)roundf(voltage_mv * 0.01f)) & BATT_VOLTAGE_LIMIT);
+    uint32_t batt = ((voltage_mv / 100) & BATT_VOLTAGE_LIMIT);
     // battery current draw in deciamps
-    batt |= prep_number(roundf(current_ca * 0.1f), 2, 1)<<BATT_CURRENT_OFFSET;
+    batt |= prep_number(current_ca / 10, 2, 1)<<BATT_CURRENT_OFFSET;
     // battery current drawn since power on in mAh (limit to 32767 (0x7FFF) since value is stored on 15 bits)
     batt |= ((current_consumed < BATT_TOTALMAH_LIMIT) ? (current_consumed & BATT_TOTALMAH_LIMIT) : BATT_TOTALMAH_LIMIT)<<BATT_TOTALMAH_OFFSET;
     return batt;
@@ -243,18 +220,18 @@ static uint32_t format_batt1(uint16_t voltage_mv, uint16_t current_ca, uint32_t 
  * Adapted from Ardupilot's AP_Frsky_SPort_Passthrough::calc_home()
  * This is the content of the 0x5004 HOME value.
  */
-static uint32_t format_home(float distance_to_home_m, float altitude_above_home_m, float bearing_to_home_centideg);
-static uint32_t format_home(float distance_to_home_m, float altitude_above_home_m, float bearing_to_home_centideg)
+static uint32_t format_home(uint32_t distance_to_home_m, uint32_t altitude_above_home_dm, uint8_t bearing_to_home_deg);
+static uint32_t format_home(uint32_t distance_to_home_m, uint32_t altitude_above_home_dm, uint8_t bearing_to_home_deg)
 {
 #define HOME_ALT_OFFSET             12
 #define HOME_BEARING_LIMIT          0x7F
 #define HOME_BEARING_OFFSET         25
     // distance between vehicle and home_loc in meters
-    uint32_t home = prep_number(roundf(distance_to_home_m), 3, 2);
+    uint32_t home = prep_number(distance_to_home_m, 3, 2);
     // angle from front of vehicle to the direction of home_loc in 3 degree increments (just in case, limit to 127 (0x7F) since the value is stored on 7 bits)
-    home |= (((uint8_t)roundf(bearing_to_home_centideg * 0.00333f)) & HOME_BEARING_LIMIT)<<HOME_BEARING_OFFSET;
+    home |= ((bearing_to_home_deg/3) & HOME_BEARING_LIMIT)<<HOME_BEARING_OFFSET;
     // altitude between vehicle and home_loc in 0.1 meters.
-    home |= prep_number(roundf(altitude_above_home_m*10), 3, 2)<<HOME_ALT_OFFSET;
+    home |= prep_number(altitude_above_home_dm, 3, 2)<<HOME_ALT_OFFSET;
     return home;
 }
 
@@ -270,10 +247,10 @@ static uint32_t format_velandyaw(float climb_mps, float groundspeed_mps, int16_t
 #define VELANDYAW_YAW_LIMIT         0x7FF
 #define VELANDYAW_YAW_OFFSET        17
     // vertical velocity in dm/s
-    uint32_t velandyaw = prep_number(roundf(climb_mps * 10), 2, 1);
+    uint32_t velandyaw = prep_number(climb_mps * 10, 2, 1);
 
     // horizontal velocity in dm/s
-    velandyaw |= prep_number(roundf(groundspeed_mps * 10), 2, 1)<<VELANDYAW_XYVEL_OFFSET;
+    velandyaw |= prep_number(groundspeed_mps * 10, 2, 1)<<VELANDYAW_XYVEL_OFFSET;
 
     // idiotic scaling from int to int*5
     velandyaw |= ((heading * 5) & VELANDYAW_YAW_LIMIT)<<VELANDYAW_YAW_OFFSET;
@@ -293,8 +270,8 @@ static uint32_t format_attiandrng(float pitch_rad, float roll_rad)
 #define ATTIANDRNG_PITCH_LIMIT      0x3FF
 #define ATTIANDRNG_PITCH_OFFSET     11
 #define ATTIANDRNG_RNGFND_OFFSET    21
-    uint32_t attiandrng = ((uint16_t)roundf((roll_rad * RAD_TO_DEG * 100 + 18000) * 0.05f) & ATTIANDRNG_ROLL_LIMIT);
-    attiandrng |= ((uint16_t)roundf((pitch_rad * RAD_TO_DEG * 100 + 9000) * 0.05f) & ATTIANDRNG_PITCH_LIMIT)<<ATTIANDRNG_PITCH_OFFSET;
+    uint32_t attiandrng = ((((uint16_t)(roll_rad * 286.0f)) + 900) & ATTIANDRNG_ROLL_LIMIT);
+    attiandrng |= ((((uint16_t)(pitch_rad * 286.0f)) + 450) & ATTIANDRNG_PITCH_LIMIT)<<ATTIANDRNG_PITCH_OFFSET;
     return attiandrng;
 }
 
@@ -302,14 +279,10 @@ static uint32_t format_attiandrng(float pitch_rad, float roll_rad)
  * Adapted from Ardupilot's AP_Frsky_SPort_Passthrough::calc_terrain()
  * This is the content of the 0x500B terrain.
  */
-static uint32_t format_terrain(float altitude_terrain);
-static uint32_t format_terrain(float altitude_terrain)
+static uint32_t format_terrain(uint32_t altitude_terrain);
+static uint32_t format_terrain(uint32_t altitude_terrain)
 {
-    float terrain_alt_m = 0;
-    if (!isnan(altitude_terrain)){
-        terrain_alt_m = altitude_terrain;
-    }
-    uint32_t value = prep_number(roundf(terrain_alt_m * 10), 3, 2);
+    uint32_t value = prep_number(altitude_terrain * 10, 3, 2);
     return value;
 }
 
@@ -329,7 +302,7 @@ static uint32_t format_waypoint(uint8_t heading, uint16_t distance, uint16_t num
     // distance to next waypoint
     value |= prep_number(distance, 3, 2) << WP_DISTANCE_OFFSET;
     // bearing encoded in 3 degrees increments
-    value |= ((uint8_t)roundf(heading * 0.666f)) << WP_BEARING_OFFSET;
+    value |= (heading * 2 / 3) << WP_BEARING_OFFSET;
     return value;
 }
 
@@ -349,8 +322,8 @@ static void ap_send_crsf_passthrough_single(uint16_t appid, uint32_t data)
     CRSF_MK_FRAME_T(ap_crsf_passthrough_single_t)
     crsfpassthrough = {0};
     crsfpassthrough.p.sub_type = CRSF_AP_CUSTOM_TELEM_SINGLE_PACKET_PASSTHROUGH;
-    crsfpassthrough.p.appid = htole16(appid);
-    crsfpassthrough.p.data = htole32(data);
+    crsfpassthrough.p.appid = appid;
+    crsfpassthrough.p.data = data;
 
     CRSF::SetHeaderAndCrc((uint8_t *)&crsfpassthrough, CRSF_FRAMETYPE_ARDUPILOT_RESP, CRSF_FRAME_SIZE(sizeof(crsfpassthrough)), CRSF_ADDRESS_FLIGHT_CONTROLLER);
     handset->sendTelemetryToTX((uint8_t *)&crsfpassthrough);
@@ -468,13 +441,14 @@ void convert_mavlink_to_crsf_telem(uint8_t *CRSFinBuffer, uint8_t count, Handset
 
 
                 // send the home message to Yaapu Telemetry Script
-                float bearing = 0;
-                float distance_to_home = 0;
-                if (home_latitude != 0){
-                    bearing = get_bearing_to(gps_int.lat, gps_int.lon, home_latitude, home_longitude);
-                    distance_to_home = get_distance(gps_int.lat, gps_int.lon, home_latitude, home_longitude);
+                int32_t bearing = 0;
+                int32_t distance_to_home = 0;
+                if ((home_latitude != 0) && (home_longitude != 0)){
+                    int32_t north_m = (home_latitude - gps_int.lat) * 111318;
+                    int32_t east_m = (home_longitude - gps_int.lon) * 111318;
+                    cordic_cartesion_to_polar(north_m, east_m, &bearing, &distance_to_home);
                 }
-                ap_send_crsf_passthrough_single(0x5004, format_home(distance_to_home, relative_alt/1000, bearing));
+                ap_send_crsf_passthrough_single(0x5004, format_home(distance_to_home, relative_alt/100, bearing));
                 break;
             }
             case MAVLINK_MSG_ID_GLOBAL_POSITION_INT: {
