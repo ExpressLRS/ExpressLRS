d4e8b0ebc4edc9d49c863377148c621c7044522a
Michael <101.37584@germanynet.de>
2025-07-01 19:43:48 +0200

    initial commit

--- src/src/rx-serial/SerialHoTT_TLM.h
+++ src/src/rx-serial/SerialHoTT_TLM.h
@@ -31,6 +31,15 @@
 #define SENSOR_ID_VARIO_B 0x89 // device ID binary mode VARIO module
 #define SENSOR_ID_VARIO_T 0x90 // device ID for text mode adressing
 
+#define HOTT_TEMP_OFFSET 20    // HoTT delivers temperature with +20 offset
+#define HOTT_TEMP_SCALE 10
+#define HOTT_RPM_SCALE 10
+#define HOTT_VSPD_OFFSET 30000
+#define HOTT_CELL_SCALE 20
+#define HOTT_VOLT_SCALE 100
+#define HOTT_SPEED_SCALE_EAM 10
+#define HOTT_SPEED_SCALE_GAM 20
+
 //
 // GAM data frame data structure
 //
@@ -320,6 +329,11 @@ private:
     void sendCRSFvario(uint32_t now);
     void sendCRSFgps(uint32_t now);
     void sendCRSFbattery(uint32_t now);
+    void sendCRSFrpm(uint32_t now, HoTTDevices device);
+    void sendCRSFtemp(uint32_t now, HoTTDevices device);
+    void sendCRSFcells(uint32_t now, HoTTDevices device);
+    void sendCRSFvolt(uint32_t now, HoTTDevices device);
+    void sendCRSFairspeed(uint32_t now, HoTTDevices device);
 
     uint16_t getHoTTvoltage();
     uint16_t getHoTTcurrent();
@@ -370,6 +384,16 @@ private:
     uint32_t lastGPSCRC = 0;
     uint32_t lastBatterySent = 0;
     uint32_t lastBatteryCRC = 0;
+    uint32_t lastRpmSent = 0;
+    uint32_t lastRpmCRC = 0;
+    uint32_t lastTempSent = 0;
+    uint32_t lastTempCRC = 0;
+    uint32_t lastCellsSent = 0;
+    uint32_t lastCellsCRC = 0;
+    uint32_t lastVoltSent = 0;
+    uint32_t lastVoltCRC = 0;
+    uint32_t lastAirspeedSent = 0;
+    uint32_t lastAirspeedCRC = 0;
 
     const uint8_t DegMinScale = 100;
     const uint8_t SecScale = 100;
