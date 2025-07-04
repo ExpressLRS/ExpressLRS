ExpressLRS/ExpressLRS

commit a103c27d350402f2df398e70d53cc627ca42df99

Change MSP TcpSocket to dynamically allocate its buffers (#3240)
* Dynamically allocate tcp-crsf buffers, remove client leak

* Move crsf2msp into tcpsocket

* Move msp2crsf to tcpsocket

src/lib/Telemetry/telemetry.cpp
@@ -13,7 +13,6 @@ extern TCPSOCKET wifi2tcp;
 #include "devMSPVTX.h"
 #endif
 
-#include "crsf2msp.h"
 #include "helpers.h"
 
 // Size byte in FIFO contains bit to indicate if the frame is deleted
@@ -274,10 +273,9 @@ void Telemetry::AppendTelemetryPackage(uint8_t *package)
         {
 #if defined(USE_MSP_WIFI)
             // this probably needs refactoring in the future, I think we should have this telemetry class inside the crsf module
-            if (wifi2tcp.hasClient() && (header->type == CRSF_FRAMETYPE_MSP_RESP || header->type == CRSF_FRAMETYPE_MSP_REQ)) // if we have a client we probs wanna talk to it
+            if (header->type == CRSF_FRAMETYPE_MSP_RESP || header->type == CRSF_FRAMETYPE_MSP_REQ) // if we have a client we probs wanna talk to it
             {
-                DBGLN("Got MSP frame, forwarding to client, len: %d", currentTelemetryByte);
-                crsf2msp.parse(package);
+                wifi2tcp.crsfMspIn(package);
             }
 #endif
 #if defined(HAS_MSP_VTX)

src/lib/CRSF2MSP/crsf2msp.h
@@ -42,5 +42,3 @@ public:
     uint8_t getSrc();
     uint8_t getDest();
 };
-
-extern CROSSFIRE2MSP crsf2msp;

src/lib/CRSF2MSP/msp2crsf.h
@@ -32,5 +32,3 @@ public:
     void parse(const uint8_t *data, uint32_t frameLen, uint8_t src = CRSF_ADDRESS_CRSF_RECEIVER, uint8_t dest = CRSF_ADDRESS_FLIGHT_CONTROLLER);
     bool validate(const uint8_t *data, uint32_t expectLen);
 };
-
-extern MSP2CROSSFIRE msp2crsf;

src/lib/TCPSOCKET/tcpsocket.cpp
@@ -3,140 +3,215 @@
 #include "tcpsocket.h"
 #include "logging.h"
 
-TCPSOCKET *TCPSOCKET::instance = NULL;
+#define TCP_PORT_BETAFLIGHT 5761 //port 5761 as used by BF configurator
 
-TCPSOCKET::TCPSOCKET(const uint32_t port)
-{
-    instance = this;
-    TCPport = port;
-}
+TCPSOCKET *TCPSOCKET::instance = NULL;
 
 void TCPSOCKET::begin()
 {
-    TCPserver = new AsyncServer(TCPport);
+    instance = this;
+
+    TCPserver = new AsyncServer(TCP_PORT_BETAFLIGHT);
     TCPserver->onClient(handleNewClient, TCPserver);
     TCPserver->begin();
 }
 
+void TCPSOCKET::pumpData()
+{
+    // Data going from FC -> MSP2CRSF -> socket
+    if (crsf2msp->FIFOout.peekSize() > 0)
+    {
+        const uint16_t len = crsf2msp->FIFOout.popSize();
+        uint8_t data[len];
+        crsf2msp->FIFOout.popBytes(data, len);
+        write(data, len);
+    }
+
+    // Data going from socket -> MSP2CRSF -> FC
+    if (FIFOin->peekSize() > 0)
+    {
+        const uint16_t len = FIFOin->popSize();
+        uint8_t data[len];
+        FIFOin->popBytes(data, len);
+        msp2crsf->parse(data, len);
+    }
+}
+
+/***
+ * @brief Add a new MSP-in-CRSF data packet read from serial to start its journey to the socket
+ */
+void TCPSOCKET::crsfMspIn(uint8_t *data)
+{
+    if (!hasClient())
+    {
+        return;
+    }
+
+    crsf2msp->parse(data);
+}
+
+/***
+ * @brief Count of bytes waiting to send to FC in CRSF form
+ * @param maxLen maximum length the caller is willing to accept
+ */
+uint8_t TCPSOCKET::crsfCrsfOutAvailable(uint32_t maxLen)
+{
+    if (!hasClient())
+    {
+        return 0;
+    }
+
+    uint8_t len = msp2crsf->FIFOout.peek();
+    if (len >= msp2crsf->FIFOout.size() || len > maxLen)
+    {
+        return 0;
+    }
+
+    return len;
+}
+
+/***
+ * @brief Pop the next packet to send to the FC. There is no size passed
+ * as this function expects the caller to have at least crsfCrsfOutAvailable() bytes
+ * of space available in the data buffer
+ */
+void TCPSOCKET::crsfCrsfOutPop(uint8_t *data)
+{
+    msp2crsf->FIFOout.lock();
+    uint8_t OutPktLen = msp2crsf->FIFOout.pop();
+    msp2crsf->FIFOout.popBytes(data, OutPktLen);
+    msp2crsf->FIFOout.unlock();
+}
+
 void TCPSOCKET::handle()
 {
-    if (TCPclient == NULL)
+    if (!hasClient())
     {
-        //DBGLN("no client");
-        return; // nothing to do
+        return;
     }
 
     // check timeout
     if (millis() - clientTimeoutLastData > clientTimeoutPeriod)
     {
-        TCPclient = NULL;
         DBGLN("TCP client timeout");
+        clientDisconnect(TCPclient);
         return;
     }
 
+    pumpData();
+
     // check if there is data to send out the TCP port
-    if ((FIFOout.size()) > 0)
+    if ((FIFOout->size()) > 0)
     {
-        const uint16_t len = FIFOout.peekSize();
+        const uint16_t len = FIFOout->peekSize();
         if (TCPclient->canSend() && TCPclient->space() > len)
         {
-            FIFOout.popSize();
+            FIFOout->popSize();
             uint8_t data[len];
-            FIFOout.popBytes(data, len);
+            FIFOout->popBytes(data, len);
             TCPclient->write((const char *)data, len);
             TCPclient->send();
-            DBGLN("TCP OUT SENT: Sent!: len: %d", len);
+            DBGLN("TCP OUT SENT: Sent!: len: %u", len);
         }
         else
         {
-            DBGLN("TCP OUT SENT: Have data but TCP not ready!: len: %d", len);
+            DBGLN("TCP OUT SENT: Have data but TCP not ready!: len: %u", len);
         }
     }
 }
 
-bool TCPSOCKET::write(uint8_t *data, uint16_t len) // doesn't send, just ques it up.
+void TCPSOCKET::write(uint8_t *data, uint16_t len) // doesn't send, just ques it up.
 {
-    if (TCPclient == NULL)
+    if (!hasClient())
     {
-        return false; // nothing to do
+        return;
     }
 
-    if (FIFOout.available(len + 2))
+    if (FIFOout->available(len + 2))
     {
-        FIFOout.pushSize(len);
-        FIFOout.pushBytes(data, len);
-        DBGLN("TCP OUT QUE: queued %d bytes", len);
-        return true;
+        FIFOout->pushSize(len);
+        FIFOout->pushBytes(data, len);
+        DBGLN("TCP OUT QUE: queued %u bytes", len);
     }
     else
     {
-        DBGLN("TCP OUT QUE: No space in FIFOout! len: %d", len);
-        return false;
+        DBGLN("TCP OUT QUE: No space in FIFOout! len: %u", len);
     }
-}
-
-void TCPSOCKET::read(uint8_t *data)
-{
-    // assume we have already checked that there is data to receive and we know how much
-    // we always recieve a single chunk so no need to give len parameter
-    uint16_t len = FIFOin.popSize();
-    FIFOin.popBytes(data, len);
-}
-
-uint16_t TCPSOCKET::bytesReady()
-{
-    return FIFOin.peekSize();
 }
 
 void TCPSOCKET::handleDataIn(void *arg, AsyncClient *client, void *data, size_t len)
 {
-    instance->TCPclient = client;
     instance->clientTimeoutLastData = millis();
+    instance->TCPclient = client;
 
-    if (instance->FIFOin.available(len + 2)) // +2 because it takes 2 bytes to store the size of the FIFO chunk
+    if (instance->FIFOin->available(len + 2)) // +2 because it takes 2 bytes to store the size of the FIFO chunk
     {
-        instance->FIFOin.pushSize(len);
-        instance->FIFOin.pushBytes((uint8_t *)data, len);
-        DBGLN("TCP IN: queued %d bytes", len);
+        instance->FIFOin->pushSize(len);
+        instance->FIFOin->pushBytes((uint8_t *)data, len);
+        DBGLN("TCP IN: queued %u bytes", len);
     }
     else
     {
-        DBGLN("TCP IN: buffer full! wanted: %d, free: %d", (len + 2), instance->FIFOin.free());
+        DBGLN("TCP IN: buffer full! wanted: %u, free: %u", (len + 2), instance->FIFOin->free());
     }
 }
 
 void TCPSOCKET::handleError(void *arg, AsyncClient *client, int8_t error)
 {
-    DBGLN("\n connection error %s from client %s \n", client->errorToString(error), client->remoteIP().toString().c_str());
+    DBGLN("\nclient %x connection error %s", client, client->errorToString(error));
+    instance->clientDisconnect(client);
 }
 
 void TCPSOCKET::handleDisconnect(void *arg, AsyncClient *client)
 {
-    DBGLN("\n client %s disconnected \n", client->remoteIP().toString().c_str());
-    instance->TCPclient = NULL;
-    instance->FIFOin.flush();
-    instance->FIFOout.flush();
+    DBGLN("\nclient %x disconnected", client);
+    instance->clientDisconnect(client);
 }
 
 void TCPSOCKET::handleTimeOut(void *arg, AsyncClient *client, uint32_t time)
 {
-    DBGLN("\n client ACK timeout ip: %s \n", client->remoteIP().toString().c_str());
+    DBGLN("\nclient ACK timeout ip: %s", client->remoteIP().toString().c_str());
 }
 
-bool TCPSOCKET::hasClient()
+void TCPSOCKET::clientConnect(AsyncClient *client)
 {
-    return (TCPclient == NULL) ? false : true;
+    if (!FIFOin)
+    {
+        FIFOin = new FIFO<BUFFER_INPUT_SIZE>();
+    }
+    if (!FIFOout)
+    {
+        FIFOout = new FIFO<BUFFER_OUTPUT_SIZE>();
+    }
+    if (!crsf2msp)
+    {
+        crsf2msp = new CROSSFIRE2MSP();
+    }
+    if (!msp2crsf)
+    {
+        msp2crsf = new MSP2CROSSFIRE();
+    }
+
+    clientTimeoutLastData = millis();
+    TCPclient = client;
+}
+
+void TCPSOCKET::clientDisconnect(AsyncClient *client)
+{
+    // NOTE: FIFO buffers / crsf2msp / msp2crsf stick around because the code doesn't account for multiple clients
+    if (client == TCPclient)
+    {
+      TCPclient = nullptr;
+    }
+    client->close();
+    delete client;
 }
 
 void TCPSOCKET::handleNewClient(void *arg, AsyncClient *client)
 {
-    DBGLN("\n new client has been connected to server, ip: %s", client->remoteIP().toString().c_str());
+    DBGLN("\nTCPSOCKET client (%x) connected ip: %s", client, client->remoteIP().toString().c_str());
 
-    instance->TCPclient = client;
-    instance->clientTimeoutLastData = millis();
-
-    // add to list
-    //instance->clients.push_back(client); // not using right now
+    instance->clientConnect(client);
 
     // register events
     client->onData(handleDataIn, NULL);

src/lib/TCPSOCKET/tcpsocket.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <cstring>
 #include "ESPAsyncWebServer.h"
+#include "crsf2msp.h"
+#include "msp2crsf.h"
 #include "FIFO.h"
 
 #define BUFFER_OUTPUT_SIZE 1024
@@ -17,11 +19,10 @@ private:
     static TCPSOCKET *instance;
     //std::vector<AsyncClient *> clients; // a list to hold all clients for MSP to WIFI bridge nut not using multi-client for now
 
-    AsyncServer *TCPserver;
-    AsyncClient *TCPclient = NULL;
-    uint32_t TCPport;
+    AsyncServer *TCPserver = nullptr;
+    AsyncClient *TCPclient = nullptr;
     const uint32_t clientTimeoutPeriod = 2000;
-    uint32_t clientTimeoutLastData;
+    uint32_t clientTimeoutLastData = 0;
 
     static void handleNewClient(void *arg, AsyncClient *client);
     static void handleDataIn(void *arg, AsyncClient *client, void *data, size_t len);
@@ -29,17 +30,23 @@ private:
     static void handleTimeOut(void *arg, AsyncClient *client, uint32_t time);
     static void handleError(void *arg, AsyncClient *client, int8_t error);
 
-    FIFO<BUFFER_OUTPUT_SIZE> FIFOout;
-    FIFO<BUFFER_INPUT_SIZE> FIFOin;
+    bool hasClient() const { return (TCPclient != nullptr); }
+    void pumpData();
+    void write(uint8_t *data, uint16_t len);
+    void clientConnect(AsyncClient *client);
+    void clientDisconnect(AsyncClient *client);
+
+    FIFO<BUFFER_OUTPUT_SIZE> *FIFOout = nullptr;
+    FIFO<BUFFER_INPUT_SIZE> *FIFOin = nullptr;
+    CROSSFIRE2MSP *crsf2msp = nullptr;
+    MSP2CROSSFIRE *msp2crsf = nullptr;
 
 public:
-    TCPSOCKET(const uint32_t port);
     void begin();
     void handle();
-    bool hasClient();
-    uint16_t bytesReady(); // has x bytes in the input buffer ready
-    bool write(uint8_t *data, uint16_t len);
-    void read(uint8_t *data);
+    void crsfMspIn(uint8_t *data);
+    uint8_t crsfCrsfOutAvailable(uint32_t maxLen);
+    void crsfCrsfOutPop(uint8_t *data);
 };
 
 #endif

src/lib/WIFI/devWIFI.cpp
@@ -79,12 +79,9 @@ static IPAddress netMsk(255, 255, 255, 0);
 static DNSServer dnsServer;
 static IPAddress ipAddress;
 
-#if defined(USE_MSP_WIFI) && defined(TARGET_RX)  //MSP2WIFI in enabled only for RX only at the moment
-#include "crsf2msp.h"
-#include "msp2crsf.h"
-
+#if defined(USE_MSP_WIFI) && defined(TARGET_RX)
 #include "tcpsocket.h"
-TCPSOCKET wifi2tcp(5761); //port 5761 as used by BF configurator
+TCPSOCKET wifi2tcp;
 #endif
 
 #if defined(PLATFORM_ESP8266)
@@ -1243,24 +1240,6 @@ static void HandleWebUpdate()
 void HandleMSP2WIFI()
 {
   #if defined(USE_MSP_WIFI) && defined(TARGET_RX)
-  // check is there is any data to write out
-  if (crsf2msp.FIFOout.peekSize() > 0)
-  {
-    const uint16_t len = crsf2msp.FIFOout.popSize();
-    uint8_t data[len];
-    crsf2msp.FIFOout.popBytes(data, len);
-    wifi2tcp.write(data, len);
-  }
-
-  // check if there is any data to read in
-  const uint16_t bytesReady = wifi2tcp.bytesReady();
-  if (bytesReady > 0)
-  {
-    uint8_t data[bytesReady];
-    wifi2tcp.read(data);
-    msp2crsf.parse(data, bytesReady);
-  }
-
   wifi2tcp.handle();
   #endif
 }

src/src/rx-serial/SerialCRSF.cpp
@@ -4,9 +4,8 @@
 #include "device.h"
 #include "telemetry.h"
 #if defined(USE_MSP_WIFI)
-#include "msp2crsf.h"
-
-extern MSP2CROSSFIRE msp2crsf;
+#include "tcpsocket.h"
+extern TCPSOCKET wifi2tcp;
 #endif
 
 extern Telemetry telemetry;
@@ -17,13 +16,11 @@ void SerialCRSF::sendQueuedData(uint32_t maxBytesToSend)
 {
     uint32_t bytesWritten = 0;
     #if defined(USE_MSP_WIFI)
-    while (msp2crsf.FIFOout.size() > msp2crsf.FIFOout.peek() && (bytesWritten + msp2crsf.FIFOout.peek()) < maxBytesToSend)
+    uint8_t OutPktLen;
+    while (OutPktLen = wifi2tcp.crsfCrsfOutAvailable(maxBytesToSend - bytesWritten))
     {
-        msp2crsf.FIFOout.lock();
-        uint8_t OutPktLen = msp2crsf.FIFOout.pop();
         uint8_t OutData[OutPktLen];
-        msp2crsf.FIFOout.popBytes(OutData, OutPktLen);
-        msp2crsf.FIFOout.unlock();
+        wifi2tcp.crsfCrsfOutPop(OutData);
         this->_outputPort->write(OutData, OutPktLen); // write the packet out
         bytesWritten += OutPktLen;
     }

src/src/rx_main.cpp
@@ -127,14 +127,6 @@ Telemetry telemetry;
 Stream *SerialLogger;
 bool hardwareConfigured = true;
 
-#if defined(USE_MSP_WIFI)
-#include "crsf2msp.h"
-#include "msp2crsf.h"
-
-CROSSFIRE2MSP crsf2msp;
-MSP2CROSSFIRE msp2crsf;
-#endif
-
 #if defined(PLATFORM_ESP8266) || defined(PLATFORM_ESP32)
 unsigned long rebootTime = 0;
 extern bool webserverPreventAutoStart;

commit 97f4c1922c945f25d14268b2b6c49598636d1ca7

Fix VTX commands not being processed
Broken by #3240

src/lib/Telemetry/telemetry.cpp
@@ -279,7 +279,7 @@ void Telemetry::AppendTelemetryPackage(uint8_t *package)
             }
 #endif
 #if defined(HAS_MSP_VTX)
-            else if (header->type == CRSF_FRAMETYPE_MSP_RESP)
+            if (header->type == CRSF_FRAMETYPE_MSP_RESP)
             {
                 mspVtxProcessPacket(package);
             }