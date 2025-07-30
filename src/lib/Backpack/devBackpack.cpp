05f825e7aa65f899d92091e823a2015773a50e4f
Christina Sauer <christina@radiomistress.com>
2025-07-30 10:50:40 -0500

    Channel comments updated, tilt fixed for real this time.

--- src/lib/Backpack/devBackpack.cpp
+++ src/lib/Backpack/devBackpack.cpp
@@ -277,9 +277,9 @@ static void injectBackpackPanTiltRollData()
         // If enabled and this packet is less than 1 second old then use it
         if (millis() - lastPTRValidTimeMs < 1000)
         {
-            ChannelData[0] = ptrChannelData[0]; // Pan on channel 0
-            ChannelData[2] = CRSF_CHANNEL_VALUE_MAX - ptrChannelData[1]; // Tilt on channel 1
-            ChannelData[3] = ptrChannelData[2]; // Roll on channel 2
+            ChannelData[0] = ptrChannelData[0]; // Pan on channel 1
+            ChannelData[1] = CRSF_CHANNEL_VALUE_MAX - ptrChannelData[1]; // Tilt on channel 2
+            ChannelData[3] = ptrChannelData[2]; // Roll on channel 4
         }
     }
     else
