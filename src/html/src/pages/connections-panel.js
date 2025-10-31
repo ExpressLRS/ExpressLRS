e0d081af788b04068e1c7bdfd5a57cf25cb60123
Paul Kendall <pkendall64@gmail.com>
2025-10-31 14:13:48 +1300

    Handle mobile devices better on the connections screen

--- src/html/src/pages/connections-panel.js
+++ src/html/src/pages/connections-panel.js
@@ -16,59 +16,75 @@ class ConnectionsPanel extends LitElement {
 
     render() {
         return html`
-            <div class="mui-panel mui--text-title">PWM Pin Functions</div>
-            <div class="mui-panel">
-                Set PWM output mode and failsafe positions.
-                <ul>
-                    <li><b>Output:</b> Receiver output pin</li>
-                    <li><b>Features:</b> If an output is capable of supporting another function, that is indicated
-                        here
-                    </li>
-                    <li><b>Mode:</b> Output frequency, 10KHz 0-100% duty cycle, binary On/Off, DShot, Serial, or I2C
-                        (some options are pin dependant)
-                    </li>
+            <style>
+                .connections-panel-root .connections-mobile-warning { display: none; }
+                @media (max-width: 768px) and (orientation: portrait) {
+                    .connections-panel-root .connections-mobile-warning { display: block; }
+                    .connections-panel-root .connections-panel { display: none !important; }
+                }
+            </style>
+            <div class="connections-panel-root">
+                <div class="mui-panel mui--text-title">PWM Pin Functions</div>
+                <div class="mui-panel warning-bg connections-mobile-warning">
+                    <div class="mui--text-title" style="margin-bottom: 8px;">Rotate to landscape</div>
+                    <p>
+                        The connections panel is too wide for small screens in portrait mode. Please rotate your device to
+                        landscape mode to view and edit the settings.
+                    </p>
+                </div>
+                <div class="mui-panel connections-panel">
+                    Set PWM output mode and failsafe positions.
+                    <form>
+                        <div class="mui-panel pwmpnl">
+                            <table class="pwmtbl mui-table">
+                                <thead>
+                                <tr>
+                                    <th class="fixed-column">Output</th><th class="mui--text-center fixed-column">Features</th><th>Mode</th><th>Input</th><th class="mui--text-center fixed-column">Invert?</th><th class="mui--text-center fixed-column">750us?</th><th class="mui--text-center fixed-column pwmitm">Failsafe Mode</th><th class="mui--text-center fixed-column pwmitm">Failsafe Pos</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    ${this._renderConnectionPins()}
+                                </tbody>
+                            </table>
+                        </div>
+                        <button class="mui-btn mui-btn--small mui-btn--primary" @click="${this._savePwmConfig}">Save</button>
+                    </form>
                     <ul>
-                        <li>When enabling serial pins, be sure to select the <b>Serial Protocol</b> below and <b>UART
-                            baud</b> on the <b><a href="#serial">Serial</a></b> page in the menu
+                        <li><b>Output:</b> Receiver output pin</li>
+                        <li><b>Features:</b> If an output is capable of supporting another function, that is indicated
+                            here
+                        </li>
+                        <li><b>Mode:</b> Output frequency, 10KHz 0-100% duty cycle, binary On/Off, DShot, Serial, or I2C
+                            (some options are pin dependant)
+                        </li>
+                        <ul>
+                            <li>When enabling serial pins, be sure to select the <b>Serial Protocol</b> below and <b>UART
+                                baud</b> on the <b><a href="#serial">Serial</a></b> page in the menu
+                            </li>
+                        </ul>
+                        <li><b>Input:</b> Input channel from the handset</li>
+                        <li><b>Invert:</b> Invert input channel position</li>
+                        <li><b>750us:</b> Use half pulse width (494-1006us) with center 750us instead of 988-2012us</li>
+                        <li><b>Failsafe</b>
+                            <ul>
+                                <li>"Set Position" sets the servo to an absolute "Failsafe Pos"
+                                    <ul>
+                                        <li>Does not use "Invert" flag</li>
+                                        <li>Value will be halved if "750us" flag is set</li>
+                                        <li>Will be converted to binary for "On/Off" mode (>1500us = HIGH)</li>
+                                    </ul>
+                                </li>
+                                <li>"No Pulses" stops sending pulses
+                                    <ul>
+                                        <li>Unpowers servos</li>
+                                        <li>May disarm ESCs</li>
+                                    </ul>
+                                </li>
+                                <li>"Last Position" continues sending last received channel position</li>
+                            </ul>
                         </li>
                     </ul>
-                    <li><b>Input:</b> Input channel from the handset</li>
-                    <li><b>Invert:</b> Invert input channel position</li>
-                    <li><b>750us:</b> Use half pulse width (494-1006us) with center 750us instead of 988-2012us</li>
-                    <li><b>Failsafe</b>
-                        <ul>
-                            <li>"Set Position" sets the servo to an absolute "Failsafe Pos"
-                                <ul>
-                                    <li>Does not use "Invert" flag</li>
-                                    <li>Value will be halved if "750us" flag is set</li>
-                                    <li>Will be converted to binary for "On/Off" mode (>1500us = HIGH)</li>
-                                </ul>
-                            </li>
-                            <li>"No Pulses" stops sending pulses
-                                <ul>
-                                    <li>Unpowers servos</li>
-                                    <li>May disarm ESCs</li>
-                                </ul>
-                            </li>
-                            <li>"Last Position" continues sending last received channel position</li>
-                        </ul>
-                    </li>
-                </ul>
-                <form>
-                    <div class="mui-panel pwmpnl">
-                        <table class="pwmtbl mui-table">
-                            <thead>
-                            <tr>
-                                <th class="fixed-column">Output</th><th class="mui--text-center fixed-column">Features</th><th>Mode</th><th>Input</th><th class="mui--text-center fixed-column">Invert?</th><th class="mui--text-center fixed-column">750us?</th><th class="mui--text-center fixed-column pwmitm">Failsafe Mode</th><th class="mui--text-center fixed-column pwmitm">Failsafe Pos</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                ${this._renderConnectionPins()}
-                            </tbody>
-                        </table>
-                    </div>
-                    <button class="mui-btn mui-btn--small mui-btn--primary" @click="${this._savePwmConfig}">Save</button>
-                </form>
+                </div>
             </div>
         `;
     }
