6698d7a5ef7ca5d054091e2f6091d2f38c0747f3
Paul Kendall <pkendall64@gmail.com>
2025-11-14 07:56:35 +1300

    Extended PWM failsafe values

--- src/html/src/pages/connections-panel.js
+++ src/html/src/pages/connections-panel.js
@@ -156,15 +156,14 @@ class ConnectionsPanel extends LitElement {
         this.pinRxIndex = undefined
         this.pinTxIndex = undefined
         this.pinModes = []
-        // arPwm is an array of raw integers [49664,50688,51200]. 10 bits of failsafe position, 4 bits of input channel, 1 bit invert, 4 bits mode, 1 bit for narrow/750us
         const htmlFields = []
         elrsState.config.pwm.forEach((item, index) => {
-            const failsafe = (item.config & 1023) + 988 // 10 bits
-            const failsafeMode = (item.config >> 20) & 3 // 2 bits
-            const ch = (item.config >> 10) & 15 // 4 bits
-            const inv = (item.config >> 14) & 1
-            const mode = (item.config >> 15) & 15 // 4 bits
-            const narrow = (item.config >> 19) & 1
+            const failsafe = (item.config & 2047) + 476; // 11 bits
+            const ch = (item.config >> 11) & 15; // 4 bits
+            const inv = (item.config >> 15) & 1;
+            const mode = (item.config >> 16) & 15; // 4 bits
+            const narrow = (item.config >> 21) & 1;
+            const failsafeMode = (item.config >> 22) & 3; // 2 bits
             const features = item.features
             const modes = ['50Hz', '60Hz', '100Hz', '160Hz', '333Hz', '400Hz', '10KHzDuty', 'On/Off']
             if (features & 16) {
@@ -315,12 +314,12 @@ class ConnectionsPanel extends LitElement {
             const failsafeField = _(`pwm_${ch}_fs`)
             const failsafeModeField = _(`pwm_${ch}_fsmode`)
             let failsafe = failsafeField.value
-            if (failsafe > 2011) failsafe = 2011
-            if (failsafe < 988) failsafe = 988
+            if (failsafe > 2115) failsafe = 2115; // for max range change to 2523
+            if (failsafe < 885) failsafe = 885;   // for min range change to 476
             failsafeField.value = failsafe
             let failsafeMode = failsafeModeField.value
 
-            const raw = (narrow << 19) | (mode << 15) | (invert << 14) | (inChannel << 10) | (failsafeMode << 20) | (failsafe - 988)
+            const raw = (failsafeMode << 22) | (narrow << 21) | (mode << 16) | (invert << 15) | (inChannel << 11) | (failsafe - 476)
             // console.log(`PWM ${ch} mode=${mode} input=${inChannel} fs=${failsafe} fsmode=${failsafeMode} inv=${invert} nar=${narrow} raw=${raw}`)
             outData.push(raw)
             ++ch
