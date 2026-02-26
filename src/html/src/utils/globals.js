e8459ffdce6fe126856a8c82cd4825721b4069cc
Ruixi Zhou <zhouruixi@gmail.com>
2026-02-26 22:19:23 +0800

    Add Rlaarlo light control (RLLLC) output protocol
    Signed-off-by: Ruixi Zhou <zhouruixi@gmail.com>

--- src/html/src/utils/globals.js
+++ src/html/src/utils/globals.js
@@ -1,2 +1,2 @@
-export const SERIAL_OPTIONS1 = ["CRSF", "Inverted CRSF", "SBUS", "Inverted SBUS", "SUMD", "DJI RS Pro", "HoTT Telemetry", "MAVLink", "DisplayPort", "GPS", "AirPort"]
-export const SERIAL_OPTIONS2 = ["Off", "CRSF", "Inverted CRSF", "SBUS", "Inverted SBUS", "SUMD", "DJI RS Pro", "HoTT Telemetry", "IRC Tramp", "TBS SmartAudio", "DisplayPort", "GPS"]
+export const SERIAL_OPTIONS1 = ["CRSF", "Inverted CRSF", "SBUS", "Inverted SBUS", "SUMD", "DJI RS Pro", "HoTT Telemetry", "MAVLink", "DisplayPort", "GPS", "RLLLC", "AirPort"]
+export const SERIAL_OPTIONS2 = ["Off", "CRSF", "Inverted CRSF", "SBUS", "Inverted SBUS", "SUMD", "DJI RS Pro", "HoTT Telemetry", "IRC Tramp", "TBS SmartAudio", "DisplayPort", "GPS", "RLLLC"]
