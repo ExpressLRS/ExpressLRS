25ac5c97995374b6f1fc9b40dc09155b1ec0c405
Bryan Mayland <bmayland@capnbry.net>
2026-04-07 16:00:16 -0400

    More build fixes for windows tests

--- src/include/native.h
+++ src/include/native.h
@@ -103,26 +103,78 @@ inline char *utoa(uint32_t value, char *str, int base) { sprintf(str, "%u", valu
 
 #if defined(_WIN32) || defined(__MINGW32__)
 // Flippin BSD functions, not available on windows for tests!
-static inline size_t strlcpy(char *dst, const char *src, size_t siz)
+/*
+ * Copyright (c) 1998, 2015 Todd C. Miller <millert@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+static inline size_t strlcpy(char *dst, const char *src, size_t dsize)
 {
-    size_t src_len = strlen(src);
-    if (siz) {
-        size_t copy = (src_len >= siz) ? siz - 1 : src_len;
-        memcpy(dst, src, copy);
-        dst[copy] = '\0';
-    }
-    return src_len;
+	const char *osrc = src;
+	size_t nleft = dsize;
+
+	/* Copy as many bytes as will fit. */
+	if (nleft != 0) {
+		while (--nleft != 0) {
+			if ((*dst++ = *src++) == '\0')
+				break;
+		}
+	}
+
+	/* Not enough room in dst, add NUL and traverse rest of src. */
+	if (nleft == 0) {
+		if (dsize != 0)
+			*dst = '\0';		/* NUL-terminate dst */
+		while (*src++)
+			;
+	}
+
+	return(src - osrc - 1);	/* count does not include NUL */
 }
 
 static inline char *stpcpy(char *dst, const char *src)
 {
-    size_t len = strlen(src);
-    memcpy(dst, src, len + 1); // include null terminator
-    return dst + len;
+	for (; (*dst = *src) != '\0'; ++src, ++dst);
+	return(dst);
 }
-#endif
 
-#ifdef _WIN32
+static inline size_t strlcat(char *dst, const char *src, size_t dsize)
+{
+	const char *odst = dst;
+	const char *osrc = src;
+	size_t n = dsize;
+	size_t dlen;
+
+	/* Find the end of dst and adjust bytes left but don't go past end. */
+	while (n-- != 0 && *dst != '\0')
+		dst++;
+	dlen = dst - odst;
+	n = dsize - dlen;
+
+	if (n-- == 0)
+		return(dlen + strlen(src));
+	while (*src != '\0') {
+		if (n != 0) {
+			*dst++ = *src;
+			n--;
+		}
+		src++;
+	}
+	*dst = '\0';
+
+	return(dlen + (src - osrc));	/* count does not include NUL */
+}
+
 #define random rand
 #define srandom srand
 #endif
