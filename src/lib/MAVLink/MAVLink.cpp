090ac3337a3d79cd9eb390f290219e3aed5fae93
JLP <jlpoltrack@gmail.com>
2026-04-28 14:31:56 -0400

    MAVLink Capa Fix

--- src/lib/MAVLink/MAVLink.cpp
+++ src/lib/MAVLink/MAVLink.cpp
@@ -124,7 +124,7 @@ void convert_mavlink_to_crsf_telem(crsf_addr_e destination, uint8_t *CRSFinBuffe
                 // mAh
                 crsfbatt.p.capacity = 0;
                 if (battery_status.current_consumed > 0){ // int32_t, -1 means invalid
-                    crsfbatt.p.capacity = htobe32(std::min(((uint32_t) battery_status.current_consumed), (uint32_t) 0xFFFFFFU)); // 24bit value
+                    crsfbatt.p.capacity = htobe32(std::min(((uint32_t) battery_status.current_consumed), (uint32_t) 0xFFFFFFU)) >> 8; // 24bit value
                 }
                 // 0-100%
                 crsfbatt.p.remaining = 0;
