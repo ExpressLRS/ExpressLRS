a432ec6930bc43f604550f279e12b796fe352b1d
Bryan Mayland <bmayland@capnbry.net>
2026-06-24 11:22:29 -0400

    Only process/forward MAVLink messages which pass frame check

--- src/src/rx-serial/SerialMavlink.cpp
+++ src/src/rx-serial/SerialMavlink.cpp
@@ -127,7 +127,7 @@ void SerialMavlink::sendQueuedData(uint32_t maxBytesToSend)
         mavlink_status_t status;
 
         // Try parse a mavlink message
-        if (mavlink_frame_char(MAVLINK_COMM_0, c, &msg, &status))
+        if (mavlink_frame_char(MAVLINK_COMM_0, c, &msg, &status) == MAVLINK_FRAMING_OK)
         {
             // Message decoded successfully
 
